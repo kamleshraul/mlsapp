4d6ba3d42a6a6bd43fe1e9b43eaa815e633f348e
amitd <amitd@348444cb-318d-0e48-9875-12270ded7a77>
2011-09-08 04:51:41 +0000

    ELS-90 Master Entry Screen: Role of a Member -- Controller

--- src/main/java/org/mkcl/els/controller/AssemblyRoleController.java
+++ src/main/java/org/mkcl/els/controller/AssemblyRoleController.java
@@ -1,7 +1,7 @@
 /*
 ******************************************************************
 File: org.mkcl.els.controller.AssemblyRoleController.java
-Copyright (c) 2011, sandeeps, MKCL
+Copyright (c) 2011, amitd, MKCL
 All rights reserved.
 
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
@@ -21,26 +21,158 @@ POSSIBILITY OF SUCH DAMAGE.
  */
 package org.mkcl.els.controller;
 
+import javax.validation.Valid;
+
+import org.mkcl.els.domain.AssemblyRole;
+import org.mkcl.els.domain.Grid;
+import org.mkcl.els.service.IAssemblyRoleService;
+import org.mkcl.els.service.IGridService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * The Class AssemblyRoleController.
  *
- * @author sandeeps
+ * @author amitd
  * @version v1.0.0
  */
+@Controller
+@RequestMapping("/assembly_roles")
 public class AssemblyRoleController extends BaseController{
 
+	/** The grid service. */
+	@Autowired
+	IGridService gridService;
+	
+	/** The assembly role service. */
+	@Autowired
+	IAssemblyRoleService assemblyRoleService;
+	
 	/**
-	 * Index.
+	 * List.
 	 *
 	 * @param model the model
 	 * @return the string
 	 */
 	@RequestMapping(method = RequestMethod.GET)
-	public String index(ModelMap model){
-		return "assemblyroles/list";
+	public String list(ModelMap model) {
+		Grid grid = gridService.findByName("ASSEMBLY_ROLE_GRID");
+		model.addAttribute("gridId", grid.getId());
+		return "assembly_roles/list";
+	}
+	
+	/**
+	 * _new.
+	 *
+	 * @param model the model
+	 * @return the string
+	 */
+	@RequestMapping(value = "new", method = RequestMethod.GET)
+	public String _new(ModelMap model){
+		AssemblyRole assemblyRole = new AssemblyRole();
+		model.addAttribute(assemblyRole);
+		return "assembly_roles/new";
+	}
+	
+	/**
+	 * Edits the.
+	 *
+	 * @param id the id
+	 * @param model the model
+	 * @return the string
+	 */
+	@RequestMapping(value = "{id}", method = RequestMethod.GET)
+	public String edit(@PathVariable Long id, ModelMap model){
+		AssemblyRole assemblyRole = assemblyRoleService.findById(id);
+		model.addAttribute(assemblyRole);
+		return "assembly_roles/edit";
+	}
+	
+	/**
+	 * Creates the.
+	 *
+	 * @param assemblyRole the assembly role
+	 * @param result the result
+	 * @param model the model
+	 * @return the string
+	 */
+	@RequestMapping(method = RequestMethod.POST)
+	public String create(@Valid 
+			@ModelAttribute("assemblyRole") AssemblyRole assemblyRole, 
+			BindingResult result, ModelMap model){
+		this.validate(assemblyRole, result);
+		
+		if(result.hasErrors()){
+			model.addAttribute("isvalid", false);
+			return "redirect:assembly_roles/new?type=error&msg=create_failed";
+		}
+		
+		assemblyRoleService.create(assemblyRole);		
+		return "redirect:assembly_roles/"+assemblyRole.getId()+"?type=success&msg=create_success";
+	}
+	
+	/**
+	 * Update.
+	 *
+	 * @param assemblyRole the assembly role
+	 * @param result the result
+	 * @param model the model
+	 * @return the string
+	 */
+	@RequestMapping(method = RequestMethod.PUT)
+	public String update(@Valid 
+			@ModelAttribute("assemblyRole")AssemblyRole assemblyRole, 
+			BindingResult result, ModelMap model){
+		this.validate(assemblyRole, result);
+		
+		if(result.hasErrors()){
+			model.addAttribute("isvalid", false);
+			return "redirect:assembly_roles/"+assemblyRole.getId()+"?type=error&msg=update_failed";
+		}
+		 
+		assemblyRoleService.update(assemblyRole);		
+		return "redirect:assembly_roles/"+assemblyRole.getId()+"?type=success&msg=update_success";
+	}
+	
+	/**
+	 * Delete.
+	 *
+	 * @param id the id
+	 * @param model the model
+	 * @return the string
+	 */
+	@RequestMapping(value="{id}", method=RequestMethod.DELETE)
+    public String delete(@PathVariable Long id, ModelMap model){
+		assemblyRoleService.removeById(id);	
+		return "info";
+	}
+	
+	/**
+	 * Validate.
+	 *
+	 * @param role the role
+	 * @param errors the errors
+	 */
+	private void validate(AssemblyRole role, Errors errors){
+		AssemblyRole duplicateRole = assemblyRoleService.findByName(role.getName());
+		if(duplicateRole!=null){
+			if(!duplicateRole.getId().equals(role.getId())){
+				// name attribute of AssemblyRole object must be unique
+				errors.rejectValue("name","NonUnique");
+			}	
+		}
+		//Check if the version matches
+		if(role.getId()!=null){
+			if(!role.getVersion().equals(assemblyRoleService.findById(role.getId()).getVersion())){
+				errors.reject("Version_Mismatch");
+			}
+		}
 	}
 }
