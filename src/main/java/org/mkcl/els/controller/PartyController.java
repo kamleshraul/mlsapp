ccd7276987fb3462464801534e086f81b5e74ed4
amitd <amitd@348444cb-318d-0e48-9875-12270ded7a77>
2011-09-08 05:01:34 +0000

    ELS-85 Master Entry Screen: Party Names

--- src/main/java/org/mkcl/els/controller/PartyController.java
+++ src/main/java/org/mkcl/els/controller/PartyController.java
@@ -1,7 +1,7 @@
 /*
 ******************************************************************
 File: org.mkcl.els.controller.PartyController.java
-Copyright (c) 2011, sandeeps, MKCL
+Copyright (c) 2011, amitd, MKCL
 All rights reserved.
 
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
@@ -21,26 +21,158 @@ POSSIBILITY OF SUCH DAMAGE.
  */
 package org.mkcl.els.controller;
 
+import javax.validation.Valid;
+
+import org.mkcl.els.domain.Grid;
+import org.mkcl.els.domain.Party;
+import org.mkcl.els.service.IGridService;
+import org.mkcl.els.service.IPartyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * The Class PartyController.
  *
- * @author sandeeps
+ * @author amitd
  * @version v1.0.0
  */
+@Controller
+@RequestMapping("/party")
 public class PartyController extends BaseController{
 
+	/** The grid service. */
+	@Autowired
+	IGridService gridService;
+	
+	/** The party service. */
+	@Autowired
+	IPartyService partyService;
+	
 	/**
-	 * Index.
+	 * List.
 	 *
 	 * @param model the model
 	 * @return the string
 	 */
 	@RequestMapping(method = RequestMethod.GET)
-	public String index(ModelMap model){
-		return "parties/list";
+	public String list(ModelMap model) {
+		Grid grid = gridService.findByName("PARTY_GRID");
+		model.addAttribute("gridId", grid.getId());
+		return "party/list";
+	}
+	
+	/**
+	 * _new.
+	 *
+	 * @param model the model
+	 * @return the string
+	 */
+	@RequestMapping(value = "new", method = RequestMethod.GET)
+	public String _new(ModelMap model){
+		Party party = new Party();
+		model.addAttribute(party);
+		return "party/new";
+	}
+	
+	/**
+	 * Edits the.
+	 *
+	 * @param id the id
+	 * @param model the model
+	 * @return the string
+	 */
+	@RequestMapping(value = "{id}", method = RequestMethod.GET)
+	public String edit(@PathVariable Long id, ModelMap model){
+		Party party = partyService.findById(id);
+		model.addAttribute(party);
+		return "party/edit";
+	}
+	
+	/**
+	 * Creates the.
+	 *
+	 * @param party the party
+	 * @param result the result
+	 * @param model the model
+	 * @return the string
+	 */
+	@RequestMapping(method = RequestMethod.POST)
+	public String create(@Valid 
+			@ModelAttribute("party") Party party, 
+			BindingResult result, ModelMap model){
+		this.validate(party, result);
+		
+		if(result.hasErrors()){
+			model.addAttribute("isvalid", false);
+			return "redirect:party/new?type=error&msg=create_failed";
+		}
+		
+		partyService.create(party);		
+		return "redirect:party/"+party.getId()+"?type=success&msg=create_success";
+	}
+	
+	/**
+	 * Update.
+	 *
+	 * @param party the party
+	 * @param result the result
+	 * @param model the model
+	 * @return the string
+	 */
+	@RequestMapping(method = RequestMethod.PUT)
+	public String update(@Valid 
+			@ModelAttribute("party")Party party, 
+			BindingResult result, ModelMap model){
+		this.validate(party, result);
+		
+		if(result.hasErrors()){
+			model.addAttribute("isvalid", false);
+			return "redirect:party/"+party.getId()+"?type=error&msg=update_failed";
+		}
+		 
+		partyService.update(party);		
+		return "redirect:party/"+party.getId()+"?type=success&msg=update_success";
+	}
+	
+	/**
+	 * Delete.
+	 *
+	 * @param id the id
+	 * @param model the model
+	 * @return the string
+	 */
+	@RequestMapping(value="{id}", method=RequestMethod.DELETE)
+    public String delete(@PathVariable Long id, ModelMap model){
+		partyService.removeById(id);	
+		return "info";
+	}
+	
+	/**
+	 * Validate.
+	 *
+	 * @param party the party
+	 * @param errors the errors
+	 */
+	private void validate(Party party, Errors errors){
+		Party duplicateParty = partyService.findByName(party.getName());
+		if(duplicateParty!=null){
+			if(!duplicateParty.getId().equals(party.getId())){
+				// name attribute of Party object must be unique
+				errors.rejectValue("name","NonUnique");
+			}	
+		}
+		//Check if the version matches
+		if(party.getId()!=null){
+			if(!party.getVersion().equals(partyService.findById(party.getId()).getVersion())){
+				errors.reject("Version_Mismatch");
+			}
+		}
 	}
 }
