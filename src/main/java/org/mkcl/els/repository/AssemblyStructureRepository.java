260c0e416a15b66fc79eb446019512f217ee08d3
amitd <amitd@348444cb-318d-0e48-9875-12270ded7a77>
2011-09-09 10:56:32 +0000

    ELS-98 Master Entry Screen: Assembly Structure -- Repository

---
+++ src/main/java/org/mkcl/els/repository/AssemblyStructureRepository.java
@@ -0,0 +1,51 @@
+/*
+******************************************************************
+File: org.mkcl.els.repository.AssemblyStructureRepository.java
+Copyright (c) 2011, amitd, ${company}
+All rights reserved.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+
+******************************************************************
+ */
+package org.mkcl.els.repository;
+
+import org.mkcl.els.domain.AssemblyStructure;
+import org.springframework.stereotype.Repository;
+
+import com.trg.search.Search;
+
+/**
+ * The Class AssemblyStructureRepository.
+ *
+ * @author amitd
+ * @version v1.0.0
+ */
+@Repository
+public class AssemblyStructureRepository 
+	extends BaseRepository<AssemblyStructure, Long>{
+	
+	/**
+	 * Find by name.
+	 *
+	 * @param name the name
+	 * @return the assembly structure
+	 */
+	public AssemblyStructure findByName(String name){
+		Search search = new Search();
+		search.addFilterEqual("name", name);
+		AssemblyStructure structure = this.searchUnique(search);
+		return structure;
+	}
+}
