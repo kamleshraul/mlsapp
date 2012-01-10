37ce9fd6e317440339bae54179fdbcaa410d08d9
meenalw <meenalw@348444cb-318d-0e48-9875-12270ded7a77>
2012-01-10 11:55:00 +0000

    changed the test case method

--- src/test/java/org/mkcl/els/AssemblyRoleTest.java
+++ src/test/java/org/mkcl/els/AssemblyRoleTest.java
@@ -9,12 +9,20 @@
  */
 package org.mkcl.els;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import junit.framework.Assert;
 
 import org.junit.Test;
 import org.mkcl.els.domain.AssemblyRole;
+import org.mkcl.els.domain.Constituency;
+import org.mkcl.els.domain.CustomParameter;
+import org.mkcl.els.domain.District;
+import org.mkcl.els.domain.MemberDetails;
+import org.mkcl.els.domain.Party;
+import org.mkcl.els.domain.State;
+import org.mkcl.els.domain.Tehsil;
 import org.springframework.transaction.annotation.Transactional;
 
 
@@ -115,7 +123,13 @@ public class AssemblyRoleTest extends AbstractTest {
     @Test
     @Transactional
     public final void findUnassignedRoles() {
-        List<AssemblyRole> listAssemblyRole = AssemblyRole.findUnassignedRoles("en", 1L);
+        MemberDetails memberDetails = intializeData();
+        CustomParameter parameter = new CustomParameter("MEMBERROLE_ASSIGNED",
+                "ASSIGNED", true, "");
+        parameter.persist();
+
+        List<AssemblyRole> listAssemblyRole = AssemblyRole.findUnassignedRoles(
+                "en", memberDetails.getId());
         Assert.assertNotNull("findUnassignedRoles AssemblyRole Data ", listAssemblyRole);
     }
 
@@ -149,5 +163,54 @@ public class AssemblyRoleTest extends AbstractTest {
         assemblyRole.persist();
         AssemblyRole assemblyRole1 = AssemblyRole.findById(assemblyRole.getId());
         Assert.assertNotNull("findById AssemblyRole Data ", assemblyRole1);
+    }
+    
+
+    /**
+     * Intialize data.
+     *
+     * @return the member details
+     * @author meenalw
+     * @since v1.0.0
+     */
+    @Transactional
+    public MemberDetails intializeData() {
+        // persist State object
+        final State state = new State("testState");
+        state.persist();
+
+        // persist District object
+        final District district = new District("testDistrict", state);
+        district.persist();
+
+        final List<District> lstDistricts = new ArrayList<District>();
+        lstDistricts.add(district);
+
+        // persist Tehsil object
+        final Tehsil tehsil = new Tehsil("testTehsil", district, "en", 1L);
+        tehsil.persist();
+
+        // persist Constituency object
+        final Constituency constituency = new Constituency("testConstituency",
+                "1", lstDistricts, false, 1L, "en");
+        constituency.persist();
+
+        // persist Party object
+        final Party party = new Party("testParty", "PA1", 1L, "en", null);
+        party.persist();
+
+        return new MemberDetails("Photo", "Mr.", "firstName", "middleName",
+                "lastName", "alias", true, "Male", constituency, party, "Mr.",
+                "fatherName", "Mrs", "motherName", "birthDate", "placeOfBirth",
+                false, "marriageDate", "spouseName", 1, 1,
+                "educationalQualification", "profession", "test@test.org",
+                "presentAddress", state, district, tehsil, "presentCity",
+                "400703", "022-23456757", "022-23456757", "9003455434", false,
+                "permanentAddress", state, district, tehsil, "permanentCity",
+                "400001", "022-23456758", "022-23456758", "9889767876", 5,
+                null, "socioCulturalActivities",
+                "literaryArtisticScAccomplishment", "booksPublished",
+                "specialInterests", "pastimeRecreation", "sportsClubs",
+                "countriesVisited", "experience", "otherInfo", 1L, "en");
     }
 }
