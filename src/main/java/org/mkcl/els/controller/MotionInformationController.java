8edb337215b273136bfd1af660fa8bdd09d3be79
nileshp <nileshp@348444cb-318d-0e48-9875-12270ded7a77>
2012-01-11 10:35:56 +0000

    Added new for MotionInformation prototype

---
+++ src/main/java/org/mkcl/els/controller/MotionInformationController.java
@@ -0,0 +1,43 @@
+/**
+ * See the file LICENSE for redistribution information.
+ *
+ * Copyright (c) 2012 MKCL.  All rights reserved.
+ *
+ * Project: e-Legislature
+ * File: org.mkcl.els.controller.MotionInformationController.java
+ * Created On: Jan 11, 2012
+ */
+package org.mkcl.els.controller;
+
+import org.mkcl.els.domain.Grid;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * The Class MotionInformationController.
+ *
+ * @author nileshp
+ * @since v1.0.0
+ */
+@Controller
+@RequestMapping("/motion_information")
+public class MotionInformationController extends BaseController {
+
+    /**
+     * Index.
+     *
+     * @param model the model
+     * @return the string
+     * @author nileshp
+     * @since v1.0.0
+     */
+    @RequestMapping(value = "list", method = RequestMethod.GET)
+    public String index(final ModelMap model) {
+        Grid grid = Grid.findByName("MEMBER_DETAIL_GRID");
+        model.addAttribute("gridId", grid.getId());
+        return "motion_information/assembly/list";
+    }
+
+}
