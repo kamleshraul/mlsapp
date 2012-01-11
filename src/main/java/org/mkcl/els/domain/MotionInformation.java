65378c1effd4db54a1ce0ec4fa416fd7185b3fb2
nileshp <nileshp@348444cb-318d-0e48-9875-12270ded7a77>
2012-01-11 10:35:40 +0000

    Added new for MotionInformation prototype

---
+++ src/main/java/org/mkcl/els/domain/MotionInformation.java
@@ -0,0 +1,137 @@
+/**
+ * See the file LICENSE for redistribution information.
+ *
+ * Copyright (c) 2012 MKCL.  All rights reserved.
+ *
+ * Project: e-Legislature
+ * File: org.mkcl.els.domain.MotionInformation.java
+ * Created On: Jan 11, 2012
+ */
+package org.mkcl.els.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.springframework.beans.factory.annotation.Configurable;
+
+/**
+ * The Class MotionInformation.
+ *
+ * @author nileshp
+ */
+@Configurable
+@Entity
+@Table(name = "motion_information")
+public class MotionInformation {
+
+    /** The id. */
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    /** The year. */
+    private String year;
+
+    /** The motion type. */
+    private String motionType;
+
+    /** The assembly. */
+    private String assembly;
+
+    /** The assembly date. */
+    private String assemblyDate;
+
+    /**
+     * Gets the assembly.
+     *
+     * @return the assembly
+     */
+    public String getAssembly() {
+        return assembly;
+    }
+
+    /**
+     * Sets the assembly.
+     *
+     * @param assembly the new assembly
+     */
+    public void setAssembly(final String assembly) {
+        this.assembly = assembly;
+    }
+
+    /**
+     * Gets the assembly date.
+     *
+     * @return the assembly date
+     */
+    public String getAssemblyDate() {
+        return assemblyDate;
+    }
+
+    /**
+     * Sets the assembly date.
+     *
+     * @param assemblyDate the new assembly date
+     */
+    public void setAssemblyDate(final String assemblyDate) {
+        this.assemblyDate = assemblyDate;
+    }
+
+    /**
+     * Gets the id.
+     *
+     * @return the id
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * Sets the id.
+     *
+     * @param id the new id
+     */
+    public void setId(final Long id) {
+        this.id = id;
+    }
+
+    /**
+     * Gets the year.
+     *
+     * @return the year
+     */
+    public String getYear() {
+        return year;
+    }
+
+    /**
+     * Sets the year.
+     *
+     * @param year the new year
+     */
+    public void setYear(final String year) {
+        this.year = year;
+    }
+
+    /**
+     * Gets the motion type.
+     *
+     * @return the motion type
+     */
+    public String getMotionType() {
+        return motionType;
+    }
+
+    /**
+     * Sets the motion type.
+     *
+     * @param motionType the new motion type
+     */
+    public void setMotionType(final String motionType) {
+        this.motionType = motionType;
+    }
+
+}
