3bd2572945ccec9507aa030b9a72372316f03c6b
sujitas <sujitas@348444cb-318d-0e48-9875-12270ded7a77>
2012-01-12 09:05:00 +0000

    Modified for motion information prototype

--- src/main/java/org/mkcl/els/controller/MotionInformationController.java
+++ src/main/java/org/mkcl/els/controller/MotionInformationController.java
@@ -11,14 +11,18 @@ package org.mkcl.els.controller;
 
 import java.util.Locale;
 
-import org.mkcl.els.domain.Grid;
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+
+import org.mkcl.els.domain.CustomParameter;
 import org.mkcl.els.domain.MotionInformation;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-// TODO: Auto-generated Javadoc
 /**
  * The Class MotionInformationController.
  *
@@ -29,40 +33,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping("/motion_information")
 public class MotionInformationController extends BaseController {
 
-    /**
-     * Index.
-     *
-     * @param model the model
-     * @return the string
-     * @author nileshp
-     * @since v1.0.0
-     */
-    @RequestMapping(value = "list", method = RequestMethod.GET)
-    public String index(final ModelMap model) {
-        Grid grid = Grid.findByName("MOIS_GRID");
-        model.addAttribute("gridId", grid.getId());
-        return "motion_information/assembly/list";
-    }
 
-    /**
-     * New form.
-     *
-     * @param model the model
-     * @param errors the errors
-     * @param locale the locale
-     * @return the string
-     * @author nileshp
-     * @since v1.0.0
-     */
-    @RequestMapping(value = "new", method = RequestMethod.GET)
-    public String newForm( final ModelMap model,
-                          final Error errors,
-                          final Locale locale) {
-        MotionInformation motionInformation = new MotionInformation();
-        model.addAttribute("motionInformation", motionInformation);
-
-       return "motion_information/assembly/new";
-    }
 
     /**
      * Edits the form.
@@ -75,12 +46,34 @@ public class MotionInformationController extends BaseController {
      * @since v1.0.0
      */
     @RequestMapping(value = "{id}/edit", method = RequestMethod.GET)
-    public String editForm( final ModelMap model,
+    public String editMotionForm( final ModelMap model,
                           final Error errors,
                           final Locale locale) {
         MotionInformation motionInformation = new MotionInformation();
         model.addAttribute("motionInformation", motionInformation);
 
        return "motion_information/motion/edit";
+    }
+
+
+    @RequestMapping(method = RequestMethod.POST)
+    public String create(@Valid @ModelAttribute("motionInformation")
+    final MotionInformation motionInformation,
+                         final BindingResult result,
+                         final ModelMap model,
+                         final HttpServletRequest request) {
+        motionInformation.setId(1L);
+        model.addAttribute("motionInformation", motionInformation);
+
+        if (CustomParameter.findByName("MOIS_PROGRESSIVE_DISPLAY").getValue()
+                .toLowerCase().equals("progressive")) {
+            return "redirect:/motion_assembly/"
+                    + motionInformation.getId()
+                    + "/edit?type=success&msg=update_success";
+        } else {
+            return "redirect:motion_information/"
+                    + motionInformation.getId()
+                    + "/edit?type=success&msg=update_success";
+        }
     }
 }
