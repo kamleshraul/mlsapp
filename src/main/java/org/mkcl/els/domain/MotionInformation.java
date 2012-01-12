36b36e8a1d9961554a2f7237c174449650f17101
sujitas <sujitas@348444cb-318d-0e48-9875-12270ded7a77>
2012-01-12 05:10:57 +0000

    Modified for motion information prototype

--- src/main/java/org/mkcl/els/domain/MotionInformation.java
+++ src/main/java/org/mkcl/els/domain/MotionInformation.java
@@ -15,11 +15,11 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.Version;
 
 import org.hibernate.validator.constraints.NotEmpty;
 import org.springframework.beans.factory.annotation.Configurable;
 
-// TODO: Auto-generated Javadoc
 /**
  * The Class MotionInformation.
  *
@@ -47,11 +47,41 @@ public class MotionInformation {
     /** The assembly date. */
     private String assemblyDate;
 
+    /** The submission date. */
+    private String submissionDate;
+
+    /** The department. */
+    private String department;
+
+    /** The submission time. */
+    private String submissionTime;
+
+    /** The motion subject. */
+    private String motionSubject;
+
+    /** The motion text. */
+    private String motionText;
+
+    /** The supporting members. */
+    private String supportingMembers;
+
+    /** The is admitted. */
+    private boolean isAdmitted;
+
+    /** The is discussed. */
+    private boolean isDiscussed;
+
+    /** The date of discussion. */
+    private String dateOfDiscussion;
+
     /** The locale. */
     @Column(length = 50)
     @NotEmpty
     private String locale;
 
+    /** The version. */
+    @Version
+    private Long version;
 
     /**
      * Gets the locale.
@@ -61,7 +91,6 @@ public class MotionInformation {
     public String getLocale() {
         return locale;
     }
-
 
     /**
      * Sets the locale.
@@ -160,6 +189,206 @@ public class MotionInformation {
      */
     public void setMotionType(final String motionType) {
         this.motionType = motionType;
+    }
+
+
+    /**
+     * Gets the submission date.
+     *
+     * @return the submission date
+     */
+    public String getSubmissionDate() {
+        return submissionDate;
+    }
+
+
+    /**
+     * Sets the submission date.
+     *
+     * @param submissionDate the new submission date
+     */
+    public void setSubmissionDate(final String submissionDate) {
+        this.submissionDate = submissionDate;
+    }
+
+
+    /**
+     * Gets the department.
+     *
+     * @return the department
+     */
+    public String getDepartment() {
+        return department;
+    }
+
+
+    /**
+     * Sets the department.
+     *
+     * @param department the new department
+     */
+    public void setDepartment(final String department) {
+        this.department = department;
+    }
+
+
+    /**
+     * Gets the submission time.
+     *
+     * @return the submission time
+     */
+    public String getSubmissionTime() {
+        return submissionTime;
+    }
+
+
+    /**
+     * Sets the submission time.
+     *
+     * @param submissionTime the new submission time
+     */
+    public void setSubmissionTime(final String submissionTime) {
+        this.submissionTime = submissionTime;
+    }
+
+
+    /**
+     * Gets the motion subject.
+     *
+     * @return the motion subject
+     */
+    public String getMotionSubject() {
+        return motionSubject;
+    }
+
+
+    /**
+     * Sets the motion subject.
+     *
+     * @param motionSubject the new motion subject
+     */
+    public void setMotionSubject(final String motionSubject) {
+        this.motionSubject = motionSubject;
+    }
+
+
+    /**
+     * Gets the motion text.
+     *
+     * @return the motion text
+     */
+    public String getMotionText() {
+        return motionText;
+    }
+
+
+    /**
+     * Sets the motion text.
+     *
+     * @param motionText the new motion text
+     */
+    public void setMotionText(final String motionText) {
+        this.motionText = motionText;
+    }
+
+
+    /**
+     * Gets the supporting members.
+     *
+     * @return the supporting members
+     */
+    public String getSupportingMembers() {
+        return supportingMembers;
+    }
+
+
+    /**
+     * Sets the supporting members.
+     *
+     * @param supportingMembers the new supporting members
+     */
+    public void setSupportingMembers(final String supportingMembers) {
+        this.supportingMembers = supportingMembers;
+    }
+
+
+    /**
+     * Checks if is admitted.
+     *
+     * @return true, if is admitted
+     */
+    public boolean getIsAdmitted() {
+        return isAdmitted;
+    }
+
+
+    /**
+     * Sets the admitted.
+     *
+     * @param isAdmitted the new admitted
+     */
+    public void setIsAdmitted(final boolean isAdmitted) {
+        this.isAdmitted = isAdmitted;
+    }
+
+
+    /**
+     * Checks if is discussed.
+     *
+     * @return true, if is discussed
+     */
+    public boolean getIsDiscussed() {
+        return isDiscussed;
+    }
+
+
+    /**
+     * Sets the discussed.
+     *
+     * @param isDiscussed the new discussed
+     */
+    public void setIsDiscussed(final boolean isDiscussed) {
+        this.isDiscussed = isDiscussed;
+    }
+
+
+    /**
+     * Gets the date of discussion.
+     *
+     * @return the date of discussion
+     */
+    public String getDateOfDiscussion() {
+        return dateOfDiscussion;
+    }
+
+
+    /**
+     * Sets the date of discussion.
+     *
+     * @param dateOfDiscussion the new date of discussion
+     */
+    public void setDateOfDiscussion(final String dateOfDiscussion) {
+        this.dateOfDiscussion = dateOfDiscussion;
+    }
+
+
+    /**
+     * Gets the version.
+     *
+     * @return the version
+     */
+    public Long getVersion() {
+        return version;
+    }
+
+
+    /**
+     * Sets the version.
+     *
+     * @param version the new version
+     */
+    public void setVersion(final Long version) {
+        this.version = version;
     }
 
 }
