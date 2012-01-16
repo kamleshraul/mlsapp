5c1f90da374824e0f81f82a9fd77162251bfe7b6
meenalw <meenalw@348444cb-318d-0e48-9875-12270ded7a77>
2012-01-16 12:37:51 +0000

    updated for prototype

---
+++ src/main/java/org/mkcl/els/controller/MyTaskController.java
@@ -0,0 +1,88 @@
+/**
+ * See the file LICENSE for redistribution information.
+ *
+ * Copyright (c) 2012 MKCL.  All rights reserved.
+ *
+ * Project: e-Legislature
+ * File: org.mkcl.els.controller.MyTaskController.java
+ * Created On: Jan 16, 2012
+ */
+package org.mkcl.els.controller;
+
+import java.util.Locale;
+
+import org.mkcl.els.domain.Grid;
+import org.mkcl.els.domain.MotionApproval;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Class MyTaskController.
+ *
+ * @author meenalw
+ * @since v1.0.0
+ */
+@Controller
+@RequestMapping("/mytask")
+public class MyTaskController extends BaseController {
+
+
+    /**
+     * Index.
+     *
+     * @param model the model
+     * @return the string
+     * @author meenalw
+     * @since v1.0.0
+     */
+    @RequestMapping(value = "list", method = RequestMethod.GET)
+    public String index(final ModelMap model) {
+        Grid grid = Grid.findByName("MYTASK_GRID");
+        model.addAttribute("gridId", grid.getId());
+        return "motion_information/approval/myTask";
+    }
+
+    /**
+     * New form.
+     *
+     * @param model the model
+     * @param errors the errors
+     * @param locale the locale
+     * @return the string
+     * @author meenalw
+     * @since v1.0.0
+     */
+    @RequestMapping(value = "new", method = RequestMethod.GET)
+    public String newForm(final ModelMap model,
+                          final Error errors,
+                          final Locale locale) {
+        MotionApproval motionApproval = new MotionApproval();
+        model.addAttribute("motionApproval", motionApproval);
+
+        return "motion_information/approval/process";
+    }
+
+    
+    /**
+     * Edits the form.
+     *
+     * @param model the model
+     * @param errors the errors
+     * @param locale the locale
+     * @return the string
+     * @author meenalw
+     * @since v1.0.0
+     */
+    @RequestMapping(value = "{id}/edit", method = RequestMethod.GET)
+    public String editForm(final ModelMap model,
+                           final Error errors,
+                           final Locale locale) {
+        
+
+        return "motion_information/approval/process";
+    }
+}
