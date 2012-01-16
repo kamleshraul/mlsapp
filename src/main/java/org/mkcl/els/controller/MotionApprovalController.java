b3b54d6759d2b0a7006cca62c38460781f521c84
meenalw <meenalw@348444cb-318d-0e48-9875-12270ded7a77>
2012-01-16 12:37:37 +0000

    updated for prototype

--- src/main/java/org/mkcl/els/controller/MotionApprovalController.java
+++ src/main/java/org/mkcl/els/controller/MotionApprovalController.java
@@ -5,22 +5,29 @@
  *
  * Project: e-Legislature
  * File: org.mkcl.els.controller.MotionApprovalController.java
- * Created On: Jan 12, 2012
+ * Created On: Jan 16, 2012
  */
 package org.mkcl.els.controller;
 
 import java.util.Locale;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+
 import org.mkcl.els.domain.Grid;
+import org.mkcl.els.domain.MotionApproval;
 import org.mkcl.els.domain.MotionInformation;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+// TODO: Auto-generated Javadoc
 /**
  * The Class MotionApprovalController.
- *
+ * 
  * @author samiksham
  * @since v1.0.0
  */
@@ -30,7 +37,7 @@ public class MotionApprovalController extends BaseController {
 
     /**
      * Index.
-     *
+     * 
      * @param model the model
      * @return the string
      * @author samiksham
@@ -45,7 +52,7 @@ public class MotionApprovalController extends BaseController {
 
     /**
      * New form.
-     *
+     * 
      * @param model the model
      * @param errors the errors
      * @param locale the locale
@@ -57,10 +64,61 @@ public class MotionApprovalController extends BaseController {
     public String newForm(final ModelMap model,
                           final Error errors,
                           final Locale locale) {
-        MotionInformation motionInformation = new MotionInformation();
-        model.addAttribute("motionInformation", motionInformation);
+        // MotionInformation motionInformation = new MotionInformation();
+        // model.addAttribute("motionInformation", motionInformation);
+        MotionApproval motionApproval = new MotionApproval();
+        model.addAttribute("motionApproval", motionApproval);
 
         return "motion_information/approval/process";
+    }
+
+    /**
+     * Edits the form.
+     * 
+     * @param model the model
+     * @param errors the errors
+     * @param locale the locale
+     * @return the string
+     * @author meenalw
+     * @since v1.0.0
+     */
+    @RequestMapping(value = "{id}/edit", method = RequestMethod.GET)
+    public String editForm(final ModelMap model,
+                           final Error errors,
+                           final Locale locale) {
+        // MotionInformation motionInformation = new MotionInformation();
+        // motionInformation.setId(1L);
+        // model.addAttribute("motionApproval", motionApproval);
+        MotionApproval motionApproval = new MotionApproval();
+        motionApproval.setId(1L);
+        model.addAttribute("motionApproval", motionApproval);
+
+        return "motion_information/approval/edit";
+    }
+
+    /**
+     * Creates the.
+     * 
+     * @param motionInformation the motion information
+     * @param result the result
+     * @param model the model
+     * @param request the request
+     * @return the string
+     * @author meenalw
+     * @since v1.0.0
+     */
+    @RequestMapping(method = RequestMethod.POST)
+    public String create(@Valid @ModelAttribute("motionApproval") final MotionApproval motionApproval,
+                         final BindingResult result,
+                         final ModelMap model,
+                         final HttpServletRequest request) {
+
+        motionApproval.setId(1L);
+        model.addAttribute("motionApproval", motionApproval);
+
+        String message = "Motion sent for approval";
+        return "redirect:motion_approval/" + motionApproval.getId()
+                + "/edit?type=success&msg=create_success";
     }
 
 }
