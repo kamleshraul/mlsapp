2f4addd34e44b080213643d53cf3ef5e33f88b56
sandeeps <sandeeps@348444cb-318d-0e48-9875-12270ded7a77>
2012-03-12 10:27:56 +0000

    adding new source els-mois

--- src/main/java/org/mkcl/els/service/IErrorNotificationService.java
+++
@@ -1,8 +0,0 @@
-package org.mkcl.els.service;
-
-import java.util.Map;
-
-public interface IErrorNotificationService {
-	  public void sendNotification(String message, Exception exception);	  
-	  public void sendNotification(String message, String AuthUser, Map<String,String> requestParams, Exception exception);	
-}
