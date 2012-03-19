e4392b044776a3b69de039d6187889d6cff70d91
anandk <anandk@348444cb-318d-0e48-9875-12270ded7a77>
2012-03-19 11:29:00 +0000

    ELS-159 ELS-160 ELS-161 ELS-162 the Domain for Legislative house type is created

---
+++ src/main/java/org/mkcl/els/domain/HouseType.java
@@ -0,0 +1,81 @@
+/**
+ * See the file LICENSE for redistribution information.
+ *
+ * Copyright (c) 2012 MKCL.  All rights reserved.
+ *
+ * Project: e-Legislature
+ * File: org.mkcl.els.domain.AssemblyCouncilType.java
+ * Created On: Mar 15, 2012
+ */
+package org.mkcl.els.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import org.hibernate.validator.constraints.NotEmpty;
+import org.springframework.beans.factory.annotation.Configurable;
+
+/**
+ * The Class AssemblyCouncilType.
+ *
+ * @author Anand
+ * @since v1.0.0
+ */
+@Configurable
+@Entity
+@Table(name = "masters_HouseType")
+public class HouseType extends BaseDomain implements Serializable{
+  // ---------------------------------Attributes------------------------------------------
+    /** The Constant serialVersionUID. */
+    private static final long serialVersionUID = 1L;
+    /** The type. */
+    @Column(length = 150, nullable = false)
+    @NotEmpty
+    private String type;
+
+ // ---------------------------------Constructors----------------------------------------------
+  /**
+  * Instantiates a new assembly council type.
+  */
+ public HouseType() {
+    super();
+    }
+
+	/**
+	 * Instantiates a new assembly council type.
+	 *
+	 * @param type the type
+	 */
+	public HouseType(String type) {
+		super();
+		this.type = type;
+	}
+
+	 // -------------------------------Domain_Methods----------------------------------------------
+	
+	 // ------------------------------------------Getters/Setters-----------------------------------
+	/**
+ 	 * Gets the type.
+ 	 *
+ 	 * @return the type
+ 	 */
+ 	public String getType() {
+		return type;
+	}
+
+	/**
+	 * Sets the type.
+	 *
+	 * @param type the new type
+	 */
+	public void setType(String type) {
+		this.type = type;
+	}
+    
+    
+    
+
+}
