5018d2fdd00b9903da1b552ad571ca191fc6ce82
anandk <anandk@348444cb-318d-0e48-9875-12270ded7a77>
2012-03-19 11:51:26 +0000

    ELS-147 ELS-148 ELS-149 ELS-150 The Domain for Language master is created

---
+++ src/main/java/org/mkcl/els/domain/Language.java
@@ -0,0 +1,83 @@
+/**
+ * See the file LICENSE for redistribution information.
+ *
+ * Copyright (c) 2012 MKCL.  All rights reserved.
+ *
+ * Project: e-Legislature
+ * File: org.mkcl.els.domain.Language.java
+ * Created On: Mar 13, 2012
+ */
+package org.mkcl.els.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import org.hibernate.validator.constraints.NotEmpty;
+import org.springframework.beans.factory.annotation.Configurable;
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Class Language.
+ * 
+ * @author Anand
+ * @since v1.0.0
+ */
+@Configurable
+@Entity
+@Table(name = "masters_languages")
+public class Language extends BaseDomain implements Serializable {
+
+	// ---------------------------------Attributes------------------------------------------
+	/** The Constant serialVersionUID. */
+	private static final long serialVersionUID = 1L;
+
+	/** The language. */
+	@Column(length = 150)
+	@NotEmpty
+	private String name;
+
+	// ---------------------------------Constructors----------------------------------------------
+	/**
+	 * Instantiates a new language.
+	 */
+	public Language() {
+		super();
+	}
+
+	/**
+	 * Instantiates a new language.
+	 * 
+	 * @param language
+	 *            the language
+	 */
+	public Language(final String name) {
+		super();
+		this.name = name;
+	}
+
+	// -------------------------------Domain_Methods----------------------------------------------
+
+	// ------------------------------------------Getters/Setters-----------------------------------
+	/**
+	 * Gets the language.
+	 * 
+	 * @return the language
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * Sets the language.
+	 * 
+	 * @param language
+	 *            the new language
+	 */
+	public void setName(final String name) {
+		this.name = name;
+	}
+
+}
