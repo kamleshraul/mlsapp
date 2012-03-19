b784b6cfabb7d456715a8fab4e84aa97b5c3ae47
anandk <anandk@348444cb-318d-0e48-9875-12270ded7a77>
2012-03-19 10:43:19 +0000

    ELS-204 ELS-205 ELS-207 The uni testcase for Degree is implemented

---
+++ src/test/java/org/mkcl/els/DegreeTest.java
@@ -0,0 +1,76 @@
+package org.mkcl.els;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+import org.mkcl.els.domain.Degree;
+import org.springframework.transaction.annotation.Transactional;
+
+public class DegreeTest extends AbstractTest{
+
+	@Test
+	@Transactional
+	public void testPersist() {
+		Degree degree=new Degree("TestDegree");
+		degree.persist();
+		Assert.assertNotNull("Saved Degree Data ", degree);
+	}
+
+	@Test
+	@Transactional
+	public void testMerge() {
+		Degree degree=new Degree("testDegree");
+		degree.persist();
+		degree.setDegree("new Degree");
+		degree.merge();
+		Assert.assertNotNull("Updated Degree Data ", degree);
+
+	}
+
+	@Test
+	@Transactional
+	public void testRemove() {
+		Degree degree=new Degree("testDegree");
+		degree.persist();
+		degree.remove();
+		Assert.assertNotNull("Deleted Degree Data ", degree);
+
+	}
+
+	@Test
+	@Transactional
+	public void testFindById() {
+		Degree degree=new Degree("testDegree");
+		degree.persist();
+		Degree degree2=Degree.findById(Degree.class,degree.getId());
+		Assert.assertNotNull("Getting Degree Data by ID ", degree);
+
+				
+	}
+
+	@Test
+	@Transactional
+	public void testFindByFieldName() {
+		Degree degree=new Degree("testDegree");
+		degree.persist();
+		Degree degree2=Degree.findByFieldName(Degree.class, "degree", "testDegree", degree.getLocale());
+		Assert.assertNotNull("Getting Degree Data by Field Name ", degree2);
+
+	}
+
+	@Test
+	@Transactional
+	public void testFindAll() {
+		Degree degree=new Degree("testDegree");
+		degree.persist();
+	    List<Degree> listdegree = Degree.findAll(Degree.class,"degree", "desc", "en");
+		Assert.assertNotNull("Getting All Degree Data ", listdegree);
+
+
+	}
+
+}
