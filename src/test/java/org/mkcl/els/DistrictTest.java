359640e1704f7a889c41dddeca3bee8a9a4a3b91
dhananjayb <dhananjayb@348444cb-318d-0e48-9875-12270ded7a77>
2012-03-20 12:01:18 +0000

    ELS-143 ELS-144 ELS-145 ELS-146

--- src/test/java/org/mkcl/els/DistrictTest.java
+++ src/test/java/org/mkcl/els/DistrictTest.java
@@ -1,214 +1,159 @@
-///**
-// * See the file LICENSE for redistribution information.
-// *
-// * Copyright (c) 2011 MKCL.  All rights reserved.
-// *
-// * Project: e-Legislature
-// * File: org.mkcl.els.DistrictTest.java
-// * Created On: Dec 20, 2011
-// */
-//package org.mkcl.els;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//import junit.framework.Assert;
-//
-//import org.junit.Test;
-//import org.mkcl.els.domain.Constituency;
-//import org.mkcl.els.domain.District;
-//import org.mkcl.els.domain.State;
-//import org.springframework.transaction.annotation.Transactional;
-//
-//
-///**
-// * The Class DistrictTest.
-// *
-// * @author nileshp
-// */
-//public class DistrictTest extends AbstractTest {
-//
-//
-//   /**
-//     * Test persist.
-//     *
-//     * @author nileshp
-//     * @since v1.0.0
-//     * * Test persist.
-//     */
-//    @Test
-//    @Transactional
-//    public final void testPersist() {
-//        State state = new State("testState");
-//        state.persist();
-//        final District district = new District("Nasik", state);
-//        district.persist();
-//        Assert.assertNotNull("Saved District Data ", district);
-//    }
-//
-//
-//    /**
-//     * Test find by name.
-//     *
-//     * @author nileshp
-//     * @since v1.0.0
-//     * * Test find by name.
-//     */
-//    @Test
-//    @Transactional
-//    public final void testFindByName() {
-//        final State statePersist = new State("testState");
-//        statePersist.persist();
-//        final District districtPersist = new District("Nasik", statePersist);
-//        districtPersist.persist();
-//
-//        final District district = District.findByName(districtPersist.getName());
-//        Assert.assertNotNull(district);
-//    }
-//
-//    /**
-//     * Test find by id.
-//     *
-//     * @author nileshp
-//     * @since v1.0.0
-//     * * Test find by id.
-//     */
-//    @Test
-//    @Transactional
-//    public final void testFindById() {
-//        final State statePersist = new State("testState");
-//        statePersist.persist();
-//        final District districtPersist = new District("Nasik", statePersist);
-//        districtPersist.persist();
-//
-//        final District district = District.findById(districtPersist.getId());
-//        Assert.assertEquals("Nasik", district.getName());
-//    }
-//
-//    /**
-//     * Test update state.
-//     *
-//     * @author nileshp
-//     * @since v1.0.0
-//     * * Test update district.
-//     */
-//    @Test
-//    @Transactional
-//    public final void testUpdateDistrict() {
-//        final State statePersist = new State("testState");
-//        statePersist.persist();
-//        final District districtPersist = new District("Nasik", statePersist);
-//        districtPersist.persist();
-//
-//        final District district = District.findById(districtPersist.getId());
-//        district.setName("Mumbaia");
-//        district.update();
-//        Assert.assertNotNull("updated District data is :-  ", District.findByName("Mumbaia"));
-//    }
-//
-//    /**
-//     * Test remove state.
-//     *
-//     * @author nileshp
-//     * @since v1.0.0
-//     * * Test remove district.
-//     */
-//    @Test
-//    @Transactional
-//    public final void testRemoveDistrict() {
-//        final State statePersist = new State("testState");
-//        statePersist.persist();
-//        final District districtPersist = new District("Nasik", statePersist);
-//        districtPersist.persist();
-//
-//        District district = District.findByName(districtPersist.getName());
-//        district.remove();
-//        Assert.assertNotNull("removed district Data ", district);
-//    }
-//
-//
-//    /**
-//     * Test find district by state id.
-//     *
-//     * @author nileshp
-//     * @since v1.0.0
-//     * * Test find district by state id.
-//     */
-//    @Test
-//    @Transactional
-//    public final void testFindDistrictByStateId() {
-//        final State statePersist = new State("testState");
-//        statePersist.persist();
-//        final District districtPersist = new District("Nasik", statePersist);
-//        districtPersist.persist();
-//
-//        final List<District> districtList = District.findDistrictsByStateId(districtPersist.getState().getId());
-//        Assert.assertNotNull("District List is :- ", districtList);
-//    }
-//
-//
-//    /**
-//     * Test find sort districts by state id.
-//     *
-//     * @author nileshp
-//     * @since v1.0.0
-//     * * Test find sort districts by state id.
-//     */
-//    @Test
-//    @Transactional
-//    public final void testFindSortDistrictsByStateId() {
-//        final State statePersist = new State("testState");
-//        statePersist.persist();
-//        final District districtPersist = new District("Nasik", statePersist);
-//        districtPersist.persist();
-//
-//        final List<District> districtList = District.findDistrictsByStateId(districtPersist.getState().getId(), "name", false);
-//        Assert.assertNotNull("District List is :- ", districtList);
-//    }
-//
-//
-//    /**
-//     * Test find sort districts by state name.
-//     *
-//     * @author nileshp
-//     * @since v1.0.0
-//     * * Test find sort districts by state name.
-//     */
-//    @Test
-//    @Transactional
-//    public final void testFindSortDistrictsByStateName() {
-//        final State statePersist = new State("testState");
-//        statePersist.persist();
-//        final District districtPersist = new District("Nasik", statePersist);
-//        districtPersist.persist();
-//
-//        final List<District> districtList = District.findDistrictsByStateName(districtPersist.getState().getName());
-//        Assert.assertNotNull("District List is :- ", districtList);
-//    }
-//
-//    /**
-//     * Test find districts by constituency id.
-//     *
-//     * @author nileshp
-//     * @since v1.0.0
-//     * * Test find districts by constituency id.
-//     */
-//    @Test
-//    @Transactional
-//    public final void testFindDistrictsByConstituencyId() {
-//        final State statePersist = new State("testState");
-//        statePersist.persist();
-//        final District districtPersist = new District("Nasik", statePersist);
-//        districtPersist.persist();
-//
-//        List<District> list = new ArrayList<District>();
-//        list.add(districtPersist);
-//
-//        Constituency c = new Constituency("Mumbai", "MH01", list, true, 2L , "en");
-//        c.persist();
-//
-//        final List<District> districtList =
-//                District.findDistrictsByConstituencyId(c.getId(), "name", false);
-//        Assert.assertNotNull("District List is :- ", districtList);
-//    }
-//}
+package org.mkcl.els;
+
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.mkcl.els.domain.District;
+import org.mkcl.els.domain.Division;
+import org.springframework.transaction.annotation.Transactional;
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Class DistrictTest.
+ * 
+ * @author Dhananjay
+ * @since v1.0.0
+ */
+public class DistrictTest extends AbstractTest {
+
+	/**
+	 * Test find districts by division id.
+	 */
+	@Test
+	public void testFindDistrictsByDivisionId() {
+		Division division = new Division();
+		division.setName("div1");
+		division.persist();
+		District district = new District("testDistrict", division);
+		district.persist();
+		List<District> districts = District.getDistrictRepository()
+				.findDistrictsByDivisionId(division.getId(), "name", "asc",
+						division.getLocale());
+		Assert.assertNotNull("Districts by Division ID is :- ", districts);
+	}
+
+	/**
+	 * Test find districts by division name.
+	 */
+	@Test
+	public void testFindDistrictsByDivisionName() {
+		Division division = new Division();
+		division.setName("div1");
+		division.persist();
+		District district = new District("testDistrict", division);
+		district.persist();
+		List<District> districts = District.getDistrictRepository()
+				.findDistrictsByDivisionName(division.getName(), "name", "asc",
+						division.getLocale());
+		Assert.assertNotNull("Districts by Division ID is :- ", districts);
+	}
+
+	/**
+	 * Test persist.
+	 */
+	@Test
+	@Transactional
+	public void testPersist() {
+		Division division = new Division();
+		division.setName("div1");
+		division.persist();
+		District district = new District("testDistrict", division);
+		district.persist();
+		Assert.assertNotNull("Saved District Data ", district);
+	}
+
+	/**
+	 * Test merge.
+	 */
+	@Test
+	public void testMerge() {
+		Division division = new Division();
+		division.setName("div1");
+		division.persist();
+		District district = new District("testDistrict", division);
+		district.persist();
+		district.setName("testNewName");
+		district.merge();
+		Assert.assertNotNull("Updated District Data", district);
+	}
+
+	/**
+	 * Test remove.
+	 */
+	@Test
+	public void testRemove() {
+		Division division = new Division();
+		division.setName("div1");
+		division.persist();
+		District district = new District("testDistrict", division);
+		district.persist();
+		District.getBaseRepository().remove();
+		Assert.assertNotNull("Deleted District Data", district);
+	}
+
+	/**
+	 * Test find by id.
+	 */
+	@Test
+	public void testFindById() {
+		Division division = new Division();
+		division.setName("div1");
+		division.persist();
+		District district = new District("testDistrict", division);
+		district.persist();
+		District district2 = District
+				.findById(District.class, district.getId());
+		Assert.assertNotNull("Getting District Data by ID ", district2);
+	}
+
+	/**
+	 * Test find by name.
+	 */
+	@Test
+	public void testFindByName() {
+		Division division = new Division();
+		division.setName("div1");
+		division.persist();
+		District district = new District("uniqueDistrict8", division); // update
+																		// with
+																		// unique
+																		// number
+																		// each
+																		// time
+																		// you
+																		// run
+		district.persist();
+		District district2 = District.getBaseRepository().findByName(
+				District.class, district.getName(), district.getLocale());
+		Assert.assertNotNull("Getting District Data by Name ", district2);
+	}
+
+	/**
+	 * Test find by field name.
+	 */
+	@Test
+	public void testFindByFieldName() {
+		Division division = new Division(); // update with
+											// unique number
+		// each time you
+		// run
+		division.setName("div4");
+		division.persist();
+		District district = new District("uniqueDistrict9", division); // update
+																		// with
+																		// unique
+																		// number
+																		// each
+																		// time
+																		// you
+																		// run
+		district.persist();
+		District district2 = District.findByFieldName(District.class,
+				"division.name", district.getDivision().getName(),
+				district.getLocale());
+		Assert.assertNotNull("Getting District Data by Field Division ",
+				district2);
+	}
+
+}
