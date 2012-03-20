17a7c121347b0615d71814295e67429ff131ab8a
anandk <anandk@348444cb-318d-0e48-9875-12270ded7a77>
2012-03-20 06:28:04 +0000

    ELS-171 ELS-172 ELS-173 ELS-174 The controller for reservationtype is implemented

---
+++ src/main/java/org/mkcl/els/controller/ReservationController.java
@@ -0,0 +1,75 @@
+/**
+ * See the file LICENSE for redistribution information.
+ *
+ * Copyright (c) 2012 MKCL.  All rights reserved.
+ *
+ * Project: e-Legislature
+ * File: org.mkcl.els.controller.ReservationController.java
+ * Created On: Mar 17, 2012
+ */
+package org.mkcl.els.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.mkcl.els.domain.Reservation;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Class ReservationController.
+ *
+ * @author Anand
+ * @since v1.0.0
+ */
+@Controller
+@RequestMapping("/masters_reservations")
+public class ReservationController extends GenericController<Reservation>{
+	
+	/* (non-Javadoc)
+	 * @see org.mkcl.els.controller.GenericController#customValidateCreate(org.mkcl.els.domain.BaseDomain, org.springframework.validation.BindingResult, javax.servlet.http.HttpServletRequest)
+	 */
+	protected void customValidateCreate(final Reservation domain,
+			final BindingResult result, final HttpServletRequest request) {
+		customValidate(domain, result, request);
+	}
+	
+	/* (non-Javadoc)
+	 * @see org.mkcl.els.controller.GenericController#customValidateUpdate(org.mkcl.els.domain.BaseDomain, org.springframework.validation.BindingResult, javax.servlet.http.HttpServletRequest)
+	 */
+	protected void customValidateUpdate(final Reservation domain,
+			final BindingResult result, final HttpServletRequest request) {
+		customValidate(domain, result, request);
+	}
+	
+	/**
+	 * Custom validate.
+	 *
+	 * @param domain the domain
+	 * @param result the result
+	 * @param request the request
+	 */
+	private void customValidate(final Reservation domain,
+			final BindingResult result, final HttpServletRequest request) {
+		Map<String, String> names = new HashMap<String, String>();
+		names.put("reservationType", domain.getReservationType());
+		names.put("shortName",domain.getShortName());
+
+		// Check for duplicate instances
+		Boolean duplicateParameter = domain.isDuplicate(names);
+		Object[] params = new Object[1];
+		params[0] = domain.getLocale().toString();
+		if (duplicateParameter) {
+			result.rejectValue("reservationType", "NonUnique", params,
+					"Duplicate Parameter");
+		}
+		// Check for version mismatch
+		if (domain.isVersionMismatch()) {
+			result.rejectValue("VersionMismatch", "version");
+		}
+	}
+}
