72b09caf49c095ec6db0feb8522115aee8c0f667
dhananjayb <dhananjayb@348444cb-318d-0e48-9875-12270ded7a77>
2012-03-20 07:05:45 +0000

    ELS-131 ELS-132 ELS-133 ELS-134

--- src/test/java/org/mkcl/els/StateTest.java
+++ src/test/java/org/mkcl/els/StateTest.java
@@ -1,99 +1,112 @@
-///**
-// * See the file LICENSE for redistribution information.
-// *
-// * Copyright (c) 2011 MKCL.  All rights reserved.
-// *
-// * Project: e-Legislature
-// * File: org.mkcl.els.StateTest.java
-// * Created On: Dec 19, 2011
-// */
-//package org.mkcl.els;
-//
-//import java.util.List;
-//
-//import junit.framework.Assert;
-//
-//import org.junit.Test;
-//import org.mkcl.els.domain.State;
-//import org.springframework.transaction.annotation.Transactional;
-//
-///**
-// * The Class StateTest.
-// *
-// * @author sujitas
-// * @since v1.0.0
-// */
-//public class StateTest extends AbstractTest {
-//
-//    /**
-//     * Test persist.
-//     */
-//    @Test
-//    @Transactional
-//    public final void testPersist() {
-//        final State state = new State("Karnataka");
-//        state.persist();
-//        Assert.assertNotNull("Saved state Data ", state);
-//    }
-//
-//    /**
-//     * Test find all.
-//     */
-//    @Test
-//    public final void testFindAll() {
-//        final List<State> lstState = State.findAll();
-//        Assert.assertNotNull(lstState);
-//    }
-//
-//    /**
-//     * Test find by id.
-//     */
-//    @Test
-//    @Transactional
-//    public final void testFindById() {
-//        final State statePersist = new State("Karnataka");
-//        statePersist.persist();
-//        final State state = State.findById(statePersist.getId());
-//        Assert.assertEquals("Karnataka", state.getName());
-//    }
-//
-//    /**
-//     * Test find by name.
-//     */
-//    @Test
-//    @Transactional
-//    public final void testFindByName() {
-//        final State statePersist = new State("Karnataka");
-//        statePersist.persist();
-//        final State state = State.findByName("Karnataka");
-//        Assert.assertEquals("Karnataka", state.getName());
-//    }
-//
-//    /**
-//     * Test update state.
-//     */
-//    @Test
-//    @Transactional
-//    public final void testUpdateState() {
-//        final State statePersist = new State("Karnataka");
-//        statePersist.persist();
-//        final State state = State.findById(statePersist.getId());
-//        state.setName("KarnatakaState");
-//        state.update();
-//        Assert.assertNotNull("updated state Data ", state);
-//    }
-//
-//    /**
-//     * Test remove state.
-//     */
-//    @Test
-//    @Transactional
-//    public final void testRemoveState() {
-//        State statePresist = new State("Karnataka");
-//        statePresist.persist();
-//        final State state = State.findByName("Karnataka");
-//        state.remove();
-//        Assert.assertNotNull("removed state Data ", state);
-//    }
-//
-//}
+package org.mkcl.els;
+
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.mkcl.els.domain.State;
+import org.springframework.transaction.annotation.Transactional;
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Class StateTest1.
+ * 
+ * @author Dhananjay
+ * @since v1.0.0
+ */
+public class StateTest extends AbstractTest {
+
+	/**
+	 * Test persist.
+	 */
+	@Test
+	@Transactional
+	public void testPersist() {
+		State state = new State("testState");
+		state.persist();
+		Assert.assertNotNull("Saved State Data ", state);
+	}
+
+	/**
+	 * Test merge.
+	 */
+	@Test
+	@Transactional
+	public void testMerge() {
+		State state = new State("testState");
+		state.persist();
+		state.setName("new State");
+		state.merge();
+		Assert.assertNotNull("Updated State Data ", state);
+
+	}
+
+	/**
+	 * Test remove.
+	 */
+	@Test
+	@Transactional
+	public void testRemove() {
+		State state = new State("testState");
+		state.persist();
+		state.remove();
+		Assert.assertNotNull("Deleted State Data ", state);
+
+	}
+
+	/**
+	 * Test find by id.
+	 */
+	@Test
+	@Transactional
+	public void testFindById() {
+		State state = new State("testState");
+		state.persist();
+		State state2 = State.findById(State.class, state.getId());
+		Assert.assertNotNull("Getting State Data by ID ", state2);
+
+	}
+
+	/**
+	 * Test find by name.
+	 */
+	@Test
+	@Transactional
+	public void testFindByName() {
+		State state = new State("testState");
+		state.persist();
+		State state2 = State.findByName(State.class, "testState",
+				state.getLocale());
+		Assert.assertNotNull("Getting State Data by Field Name ", state2);
+
+	}
+
+	/**
+	 * Test find by field name.
+	 */
+	@Test
+	@Transactional
+	public void testFindByFieldName() {
+		State state = new State("testState");
+		state.persist();
+		State state2 = State.findByFieldName(State.class, "name", "testState",
+				state.getLocale());
+		Assert.assertNotNull("Getting State Data by Field Name ", state2);
+
+	}
+
+	/**
+	 * Test find all.
+	 */
+	@Test
+	@Transactional
+	public void testFindAll() {
+		State state = new State("testState");
+		state.persist();
+		List<State> listState = State
+				.findAll(State.class, "name", "desc", "en");
+		Assert.assertNotNull("Getting All State Data ", listState);
+
+	}
+
+}
