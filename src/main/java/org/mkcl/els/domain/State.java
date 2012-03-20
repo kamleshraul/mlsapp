712932794b101633fd55c9735d648500a0d2b7bb
nileshp <nileshp@348444cb-318d-0e48-9875-12270ded7a77>
2012-03-20 06:39:45 +0000

    ELS-131 ELS-132 ELS-133 ELS-134

--- src/main/java/org/mkcl/els/domain/State.java
+++ src/main/java/org/mkcl/els/domain/State.java
@@ -20,62 +20,65 @@ import org.springframework.beans.factory.annotation.Configurable;
 
 /**
  * The Class State.
- *
- * @author nileshp
+ * 
+ * @author Dhananjay
  * @since v1.0.0
  */
 @Configurable
 @Entity
-@Table(name = "states")
+@Table(name = "masters_states")
 public class State extends BaseDomain implements Serializable {
 
-    // ---------------------------------Attributes-------------------------------------------------
-    /** The Constant serialVersionUID. */
-    private static final transient long serialVersionUID = 1L;
+	// ---------------------------------Attributes-------------------------------------------------
+	/** The Constant serialVersionUID. */
+	private static final transient long serialVersionUID = 1L;
 
-    // Reason:The longest state in terms of name is Arunachal Pradesh(17
-    // characters).so,name is taken to be 17*3
-    /** The name. */
-    @Column(length = 51)
-    @NotEmpty
-    private String name;
+	// Reason:The longest state in terms of name is Arunachal Pradesh(17
+	// characters).so,name is taken to be 17*3
+	/** The name. */
+	@Column(length = 51)
+	@NotEmpty
+	private String name;
 
-    // ---------------------------------Constructors----------------------------------------------
-    /**
-     * Instantiates a new state.
-     */
-    public State() {
-        super();
-    }
+	// ---------------------------------Constructors----------------------------------------------
+	/**
+	 * Instantiates a new state.
+	 */
+	public State() {
+		super();
+	}
 
-    /**
-     * Instantiates a new state.
-     *
-     * @param name the name
-     */
-    public State(final String name) {
-        super();
-        this.name = name;
-    }
+	/**
+	 * Instantiates a new state.
+	 * 
+	 * @param name
+	 *            the name
+	 */
+	public State(final String name) {
+		super();
+		this.name = name;
+	}
 
-    // -------------------------------Domain_Methods----------------------------------------------
+	// -------------------------------Domain_Methods----------------------------------------------
 
-    // ------------------------------------------Getters/Setters-----------------------------------
-    /**
-     * Gets the name.
-     *
-     * @return the name
-     */
-    public String getName() {
-        return name;
-    }
+	// ------------------------------------------Getters/Setters-----------------------------------
+	/**
+	 * Gets the name.
+	 * 
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
 
-    /**
-     * Sets the name.
-     *
-     * @param name the new name
-     */
-    public void setName(final String name) {
-        this.name = name;
-    }
+	/**
+	 * Sets the name.
+	 * 
+	 * @param name
+	 *            the new name
+	 */
+	public void setName(final String name) {
+		this.name = name;
+	}
+
 }
