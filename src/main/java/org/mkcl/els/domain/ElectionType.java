15c3e5e3f2b4b51dc47b9544259dcac8dce841c9
anandk <anandk@348444cb-318d-0e48-9875-12270ded7a77>
2012-03-28 12:06:22 +0000

    ELS-159 ELS-160 ELS-161 ELS-162

--- src/main/java/org/mkcl/els/domain/ElectionType.java
+++ src/main/java/org/mkcl/els/domain/ElectionType.java
@@ -13,145 +13,74 @@ import java.io.Serializable;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+
 import org.hibernate.validator.constraints.NotEmpty;
 import org.springframework.beans.factory.annotation.Configurable;
 
-// TODO: Auto-generated Javadoc
 /**
  * The Class ElectionType.
- *
+ * 
  * @author Anand
  * @since v1.0.0
  */
 @Configurable
 @Entity
-@Table(name = "masters_electionTypes")
-public class ElectionType extends BaseDomain implements Serializable{
-	
-	// ---------------------------------Attributes-------------------------------------------------
+@Table(name = "masters_electiontypes")
+public class ElectionType extends BaseDomain implements Serializable {
+
+    // ---------------------------------Attributes-------------------------------------------------
     /** The Constant serialVersionUID. */
     private static final transient long serialVersionUID = 1L;
-    
-    /** The type. */
-    @Column(length = 150)
-    @NotEmpty
-    private String electionType;
 
-   
-    /** The assemblycounciltype. */
+    /** The type. */
+    @Column(length = 600)
+    @NotEmpty
+    private String name;
+
     @ManyToOne
+    @JoinColumn(name = "housetype_id")
     private HouseType houseType;
 
-    /** The election type repository. */
-//    @Autowired
-//    private static transient ElectionTypeRepository electionTypeRepository;
-
-// ---------------------------------Constructors----------------------------------------------
+    // ---------------------------------Constructors----------------------------------------------
     /**
- * Instantiates a new election type.
- */
-public ElectionType() {
-		super();
-	}
+     * Instantiates a new election type.
+     */
+    public ElectionType() {
+        super();
+    }
 
-	
-	/**
-	 * Instantiates a new election type.
-	 *
-	 * @param electionType the election type
-	 * @param assemblycounciltype the assemblycounciltype
-	 */
-	public ElectionType(final String electionType,
-						final HouseType houseType) {
-	super();
-	this.electionType = electionType;
-	this.houseType = houseType;
-}
+    /**
+     * Instantiates a new election type.
+     * 
+     * @param electionType the election type
+     * @param assemblycounciltype the assemblycounciltype
+     */
+    public ElectionType(final String name, final HouseType houseType) {
+        super();
+        this.name = name;
+        this.houseType = houseType;
+    }
 
+    // -------------------------------Domain_Methods----------------------------------------------
 
-	// -------------------------------Domain_Methods----------------------------------------------
-	
-//	/**
-//	 * Gets the election type repository.
-//	 *
-//	 * @return the election type repository
-//	 */
-//	public static ElectionTypeRepository getElectionTypeRepository() {
-//		ElectionTypeRepository electionTypeRepository = new ElectionType().electionTypeRepository;
-//        if (electionTypeRepository == null) {
-//            throw new IllegalStateException(
-//                    "ElectionTypeRepository has not been injected in ElectionType Domain");
-//        }
-//        return electionTypeRepository;
-//    }
-//	
-//	 /**
-// 	 * Find election type by assembly council type id.
-// 	 *
-// 	 * @param assemblyCouncilTypeid the assembly council typeid
-// 	 * @param sortBy the sort by
-// 	 * @param sortOrder the sort order
-// 	 * @return the list
-// 	 * @author compaq
-// 	 * @since v1.0.0
-// 	 */
-// 	@Transactional(readOnly = true)
-//	    public static List<ElectionType> findElectionTypeByAssemblyCouncilTypeId(
-//	   final Long assemblyCouncilTypeid, final String sortBy, final String sortOrder) {
-//	        return getElectionTypeRepository().findElectionTypeByAssemblyCouncilTypeId(
-//	                assemblyCouncilTypeid, sortBy, sortOrder);
-//	    }
-//	 
-//	 /**
-// 	 * Find election type by assembly council type.
-// 	 *
-// 	 * @param type the type
-// 	 * @param orderBy the order by
-// 	 * @param sortOrder the sort order
-// 	 * @return the list
-// 	 * @author compaq
-// 	 * @since v1.0.0
-// 	 */
-// 	@Transactional(readOnly = true)
-//	    public static List<ElectionType> findElectionTypeByAssemblyCouncilType(
-//	    		final String type, final String orderBy, final String sortOrder) {
-//	        return getElectionTypeRepository().findElectionTypeByAssemblyCouncilType(
-//	                type, orderBy, sortOrder);
-//	    }
-	 // ------------------------------------------Getters/Setters-------------------------------
+    // ------------------------------------------Getters/Setters-------------------------------
+    public String getName() {
+        return name;
+    }
 
-	
-	/**
-	 * Gets the election type.
-	 *
-	 * @return the election type
-	 */
-	public String getElectionType() {
-		return electionType;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
+    public HouseType getHouseType() {
+        return houseType;
+    }
 
-	public HouseType getHouseType() {
-		return houseType;
-	}
+    public void setHouseType(HouseType houseType) {
+        this.houseType = houseType;
+    }
 
-
-	public void setHouseType(HouseType houseType) {
-		this.houseType = houseType;
-	}
-
-
-	/**
-	 * Sets the election type.
-	 *
-	 * @param electionType the new election type
-	 */
-	public void setElectionType(final String electionType) {
-		this.electionType = electionType;
-	}
-
-
-	
 }
