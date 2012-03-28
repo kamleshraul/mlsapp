2b6ed3edf82e1e6609f533f2d2b6b2a09e476889
anandk <anandk@348444cb-318d-0e48-9875-12270ded7a77>
2012-03-28 12:04:46 +0000

    ELS-183 ELS-185 ELS-187 The domain for House is constructed

---
+++ src/main/java/org/mkcl/els/domain/House.java
@@ -0,0 +1,324 @@
+/**
+ * See the file LICENSE for redistribution information.
+ *
+ * Copyright (c) 2012 MKCL.  All rights reserved.
+ *
+ * Project: e-Legislature
+ * File: org.mkcl.els.domain.House.java
+ * Created On: Mar 20, 2012
+ */
+package org.mkcl.els.domain;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.mkcl.els.common.vo.GridData;
+import org.mkcl.els.repository.HouseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * The Class House.
+ * 
+ * @author amitd
+ * @author sandeeps
+ * @since v1.0.0
+ */
+@Configurable
+@Entity
+@Table(name = "house")
+public class House extends BaseDomain implements Serializable {
+
+    // ---------------------------------Attributes------------------------------------------
+
+    /** The Constant serialVersionUID. */
+    private transient static final long serialVersionUID = 1L;
+
+    /** The name. */
+    @Column(length = 300)
+    private String name;
+
+    /** The number. */
+    private Integer number;
+
+    /** The type. */
+    @OneToOne
+    @JoinColumn(name = "housetype_id")
+    private HouseType type;
+
+    /** The formation date. */
+    @Temporal(TemporalType.DATE)
+    private Date formationDate;
+
+    /** The dissolve date. */
+    @Temporal(TemporalType.DATE)
+    private Date dissolveDate;
+
+    /** The first date. */
+    @Temporal(TemporalType.DATE)
+    private Date firstDate;
+
+    /** The last date. */
+    @Temporal(TemporalType.DATE)
+    private Date lastDate;
+
+    /** The governor address date. */
+    @Temporal(TemporalType.DATE)
+    private Date governorAddressDate;
+
+    /** The total members. */
+    private Integer totalMembers;
+
+    /** The remarks. */
+    @Column(length = 1000)
+    private String remarks;
+
+    /** The sessions. */
+    @OneToMany
+    @JoinColumn(name = "house_id", referencedColumnName = "id")
+    private List<Session> sessions;
+    
+    /** The HouseRepository repository. */
+   @Autowired
+    private static transient HouseRepository houserepository;
+
+    // ---------------------------------Constructors----------------------------------------------
+
+    /**
+     * Instantiates a new house.
+     */
+    public House() {
+        super();
+    }
+
+    /**
+     * Instantiates a new house.
+     * 
+     * @param name the name
+     * @param number the number
+     * @param type the type
+     * @param formationDate the formation date
+     */
+    public House(String name, Integer number, HouseType type, Date formationDate) {
+        super();
+        this.name = name;
+        this.number = number;
+        this.type = type;
+        this.formationDate = formationDate;
+    }
+
+    // -------------------------------Domain_Methods----------------------------------------------
+   
+    // ------------------------------------------Getters/Setters-----------------------------------
+
+    /**
+     * Gets the name.
+     * 
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Sets the name.
+     * 
+     * @param name the new name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Gets the number.
+     * 
+     * @return the number
+     */
+    public Integer getNumber() {
+        return number;
+    }
+
+    /**
+     * Sets the number.
+     * 
+     * @param number the new number
+     */
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+
+    /**
+     * Gets the type.
+     * 
+     * @return the type
+     */
+    public HouseType getType() {
+        return type;
+    }
+
+    /**
+     * Sets the type.
+     * 
+     * @param type the new type
+     */
+    public void setType(HouseType type) {
+        this.type = type;
+    }
+
+    /**
+     * Gets the formation date.
+     * 
+     * @return the formation date
+     */
+    public Date getFormationDate() {
+        return formationDate;
+    }
+
+    /**
+     * Sets the formation date.
+     * 
+     * @param formationDate the new formation date
+     */
+    public void setFormationDate(Date formationDate) {
+        this.formationDate = formationDate;
+    }
+
+    /**
+     * Gets the dissolve date.
+     * 
+     * @return the dissolve date
+     */
+    public Date getDissolveDate() {
+        return dissolveDate;
+    }
+
+    /**
+     * Sets the dissolve date.
+     * 
+     * @param dissolveDate the new dissolve date
+     */
+    public void setDissolveDate(Date dissolveDate) {
+        this.dissolveDate = dissolveDate;
+    }
+
+    /**
+     * Gets the first date.
+     * 
+     * @return the first date
+     */
+    public Date getFirstDate() {
+        return firstDate;
+    }
+
+    /**
+     * Sets the first date.
+     * 
+     * @param firstDate the new first date
+     */
+    public void setFirstDate(Date firstDate) {
+        this.firstDate = firstDate;
+    }
+
+    /**
+     * Gets the last date.
+     * 
+     * @return the last date
+     */
+    public Date getLastDate() {
+        return lastDate;
+    }
+
+    /**
+     * Sets the last date.
+     * 
+     * @param lastDate the new last date
+     */
+    public void setLastDate(Date lastDate) {
+        this.lastDate = lastDate;
+    }
+
+    /**
+     * Gets the governor address date.
+     * 
+     * @return the governor address date
+     */
+    public Date getGovernorAddressDate() {
+        return governorAddressDate;
+    }
+
+    /**
+     * Sets the governor address date.
+     * 
+     * @param governorAddressDate the new governor address date
+     */
+    public void setGovernorAddressDate(Date governorAddressDate) {
+        this.governorAddressDate = governorAddressDate;
+    }
+
+    /**
+     * Gets the total members.
+     * 
+     * @return the total members
+     */
+    public Integer getTotalMembers() {
+        return totalMembers;
+    }
+
+    /**
+     * Sets the total members.
+     * 
+     * @param totalMembers the new total members
+     */
+    public void setTotalMembers(Integer totalMembers) {
+        this.totalMembers = totalMembers;
+    }
+
+    /**
+     * Gets the remarks.
+     * 
+     * @return the remarks
+     */
+    public String getRemarks() {
+        return remarks;
+    }
+
+    /**
+     * Sets the remarks.
+     * 
+     * @param remarks the new remarks
+     */
+    public void setRemarks(String remarks) {
+        this.remarks = remarks;
+    }
+
+    /**
+     * Gets the sessions.
+     * 
+     * @return the sessions
+     */
+    public List<Session> getSessions() {
+        return sessions;
+    }
+
+    /**
+     * Sets the sessions.
+     * 
+     * @param sessions the new sessions
+     */
+    public void setSessions(List<Session> sessions) {
+        this.sessions = sessions;
+    }
+
+}
