7572f75ad211e37b8f000041572681455bdeb090
anandk <anandk@348444cb-318d-0e48-9875-12270ded7a77>
2012-03-28 12:11:24 +0000

    ELS-196 ELS-197 ELS-198 ELS-199

--- src/main/java/org/mkcl/els/domain/Profession.java
+++ src/main/java/org/mkcl/els/domain/Profession.java
@@ -17,69 +17,64 @@ import javax.persistence.Table;
 
 import org.hibernate.validator.constraints.NotEmpty;
 import org.springframework.beans.factory.annotation.Configurable;
-// TODO: Auto-generated Javadoc
 
 /**
  * The Class Profession.
- *
+ * 
  * @author Anand
  * @since v1.0.0
  */
 @Configurable
 @Entity
 @Table(name = "masters_professions")
-public class Profession extends BaseDomain implements Serializable{
-	
-	// ---------------------------------Attributes------------------------------------------
+public class Profession extends BaseDomain implements Serializable {
+
+    // ---------------------------------Attributes------------------------------------------
     /** The Constant serialVersionUID. */
     private static final transient long serialVersionUID = 1L;
 
-    
     /** The profession. */
-    @Column(length = 150, nullable = false)
+    @Column(length = 600)
     @NotEmpty
     private String name;
 
-    
- // ---------------------------------Constructors----------------------------------------------
-    
-    /**
-  * Instantiates a new profession.
-  */
- public Profession() {
-		super();
-	}
-
+    // ---------------------------------Constructors----------------------------------------------
 
     /**
      * Instantiates a new profession.
-     *
+     */
+    public Profession() {
+        super();
+    }
+
+    /**
+     * Instantiates a new profession.
+     * 
      * @param profession the profession
      */
     public Profession(final String name) {
-	super();
-	this.name = name;
+        super();
+        this.name = name;
     }
 
- // -------------------------------Domain_Methods----------------------------------------------
+    // -------------------------------Domain_Methods----------------------------------------------
 
- // ------------------------------------------Getters/Setters-----------------------------------
+    // ------------------------------------------Getters/Setters-----------------------------------
     /**
-  * Gets the profession.
-  *
-  * @return the profession
-  */
- public String getName() {
-		return name;
-	}
+     * Gets the profession.
+     * 
+     * @return the profession
+     */
+    public String getName() {
+        return name;
+    }
 
-
-	/**
-	 * Sets the profession.
-	 *
-	 * @param profession the new profession
-	 */
-	public void setName(final String name) {
-		this.name = name;
-	}
+    /**
+     * Sets the profession.
+     * 
+     * @param profession the new profession
+     */
+    public void setName(final String name) {
+        this.name = name;
+    }
 }
