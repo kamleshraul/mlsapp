d8baa8cfe0e25cf3e50306e67a8d6834ad3a5600
anandk <anandk@348444cb-318d-0e48-9875-12270ded7a77>
2012-03-28 12:16:28 +0000

    ELS-186 ELS-188

--- src/main/java/org/mkcl/els/domain/Session.java
+++ src/main/java/org/mkcl/els/domain/Session.java
@@ -5,78 +5,306 @@
  *
  * Project: e-Legislature
  * File: org.mkcl.els.domain.Session.java
- * Created On: Mar 13, 2012
+ * Created On: Mar 20, 2012
  */
 package org.mkcl.els.domain;
 
 import java.io.Serializable;
+import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
-import org.hibernate.validator.constraints.NotEmpty;
+import org.codehaus.jackson.annotate.JsonIgnore;
+import org.codehaus.jackson.annotate.JsonIgnoreProperties;
+import org.junit.Ignore;
 import org.springframework.beans.factory.annotation.Configurable;
 
-// TODO: Auto-generated Javadoc
 /**
  * The Class Session.
  * 
- * @author Anand
+ * @author anand
  * @since v1.0.0
  */
 @Configurable
 @Entity
-@Table(name = "masters_sessions")
+@Table(name = "sessions")
+@JsonIgnoreProperties({"house"})
 public class Session extends BaseDomain implements Serializable {
 
-	// ---------------------------------Attributes------------------------------------------
-	/** The Constant serialVersionUID. */
-	private static final transient long serialVersionUID = 1L;
+    // ---------------------------------Attributes------------------------------------------
 
-	/** The name. */
-	@Column(length = 150, nullable = false)
-	@NotEmpty
-	private String sessionType;
+    /** The Constant serialVersionUID. */
+    private transient static final long serialVersionUID = 1L;
 
-	// ---------------------------------Constructors----------------------------------------------
+    /** The number. */
+    private Integer number;
 
-	/**
-	 * Instantiates a new session.
-	 *
-	 * @param session_type the session_type
-	 */
-	public Session(final String sessiontype) {
-		super();
-		this.sessionType = sessiontype;
+    /** The start date. */
+    @Temporal(TemporalType.DATE)
+    private Date startDate;
+
+    /** The end date. */
+    @Temporal(TemporalType.DATE)
+    private Date endDate;
+
+    /** The type. */
+    @OneToOne
+    @JoinColumn(name = "sessiontype_id")
+    private SessionType type;
+
+    /** The place. */
+    @OneToOne
+    @JoinColumn(name = "sessionplace_id")
+    private SessionPlace place;
+
+    /** The year. */
+    private Integer year;
+
+    /** The duration in days. */
+    private Integer durationInDays;
+
+    /** The duration in hrs. */
+    private Integer durationInHrs;
+
+    /** The duration in mins. */
+    private Integer durationInMins;
+
+    /** The remarks. */
+    @Column(length = 1000)
+    private String remarks;
+   
+   @ManyToOne
+   @JoinColumn(name = "house_id")
+     private House house;
+    
+
+    // -------------------------------Constructors----------------------------------------------
+
+    /**
+     * Instantiates a new session.
+     */
+    public Session() {
+        super();
+    }
+
+    /**
+     * Instantiates a new session.
+     * 
+     * @param number the number
+     * @param startDate the start date
+     * @param endDate the end date
+     * @param type the type
+     * @param place the place
+     * @param year the year
+     */
+    public Session(Integer number, Date startDate, Date endDate,
+            SessionType type, SessionPlace place, Integer year,House house) {
+        super();
+        this.number = number;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.type = type;
+        this.place = place;
+        this.year = year;
+        this.house=house;
+    }
+
+    // -------------------------------Domain_Methods----------------------------------------------
+
+    // ------------------------------Getters/Setters-----------------------
+    /**
+     * Gets the number.
+     * 
+     * @return the number
+     */
+    public Integer getNumber() {
+        return number;
+    }
+
+    /**
+     * Sets the number.
+     * 
+     * @param number the new number
+     */
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+
+    /**
+     * Gets the start date.
+     * 
+     * @return the start date
+     */
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    /**
+     * Sets the start date.
+     * 
+     * @param startDate the new start date
+     */
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    /**
+     * Gets the end date.
+     * 
+     * @return the end date
+     */
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    /**
+     * Sets the end date.
+     * 
+     * @param endDate the new end date
+     */
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    /**
+     * Gets the type.
+     * 
+     * @return the type
+     */
+    public SessionType getType() {
+        return type;
+    }
+
+    /**
+     * Sets the type.
+     * 
+     * @param type the new type
+     */
+    public void setType(SessionType type) {
+        this.type = type;
+    }
+
+    /**
+     * Gets the place.
+     * 
+     * @return the place
+     */
+    public SessionPlace getPlace() {
+        return place;
+    }
+
+    /**
+     * Sets the place.
+     * 
+     * @param place the new place
+     */
+    public void setPlace(SessionPlace place) {
+        this.place = place;
+    }
+
+    /**
+     * Gets the year.
+     * 
+     * @return the year
+     */
+    public Integer getYear() {
+        return year;
+    }
+
+    /**
+     * Sets the year.
+     * 
+     * @param year the new year
+     */
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+    /**
+     * Gets the duration in days.
+     * 
+     * @return the duration in days
+     */
+    public Integer getDurationInDays() {
+        return durationInDays;
+    }
+
+    /**
+     * Sets the duration in days.
+     * 
+     * @param durationInDays the new duration in days
+     */
+    public void setDurationInDays(Integer durationInDays) {
+        this.durationInDays = durationInDays;
+    }
+
+    /**
+     * Gets the duration in hrs.
+     * 
+     * @return the duration in hrs
+     */
+    public Integer getDurationInHrs() {
+        return durationInHrs;
+    }
+
+    /**
+     * Sets the duration in hrs.
+     * 
+     * @param durationInHrs the new duration in hrs
+     */
+    public void setDurationInHrs(Integer durationInHrs) {
+        this.durationInHrs = durationInHrs;
+    }
+
+    /**
+     * Gets the duration in mins.
+     * 
+     * @return the duration in mins
+     */
+    public Integer getDurationInMins() {
+        return durationInMins;
+    }
+
+    /**
+     * Sets the duration in mins.
+     * 
+     * @param durationInMins the new duration in mins
+     */
+    public void setDurationInMins(Integer durationInMins) {
+        this.durationInMins = durationInMins;
+    }
+
+    /**
+     * Gets the remarks.
+     * 
+     * @return the remarks
+     */
+    public String getRemarks() {
+        return remarks;
+    }
+
+    /**
+     * Sets the remarks.
+     * 
+     * @param remarks the new remarks
+     */
+    public void setRemarks(String remarks) {
+        this.remarks = remarks;
+    }
+
+	public House getHouse() {
+		return house;
 	}
 
-	/**
-	 * Instantiates a new session.
-	 */
-	public Session() {
-		super();
-	}
-
-	// -------------------------------Domain_Methods-----------------------------------
-
-	// ------------------------------------------Getters/Setters-----------------------
-	/**
-	 * Gets the session type.
-	 *
-	 * @return the session type
-	 */
-	public String getSessionType() {
-		return sessionType;
-	}
-
-	/**
-	 * Sets the session type.
-	 *
-	 * @param session_type the new session type
-	 */
-	public void setSessionType(String sessiontype) {
-		this.sessionType = sessiontype;
+	public void setHouse(House house) {
+		this.house = house;
 	}
 
 }
