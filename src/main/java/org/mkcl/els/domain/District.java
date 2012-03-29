4e68cd4f972c77c000851a55f3eb1a3367f64a8e
dhananjayb <dhananjayb@348444cb-318d-0e48-9875-12270ded7a77>
2012-03-29 05:22:10 +0000

    ELS-143 ELS-144 ELS-145 ELS-146 removed domain methods that can be used from BaseRepository

--- src/main/java/org/mkcl/els/domain/District.java
+++ src/main/java/org/mkcl/els/domain/District.java
@@ -5,7 +5,7 @@
  *
  * Project: e-Legislature
  * File: org.mkcl.els.domain.District.java
- * Created On: Mar 8, 2012
+ * Created On: Mar 20, 2012
  */
 package org.mkcl.els.domain;
 
@@ -14,6 +14,7 @@ import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
@@ -24,11 +25,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.transaction.annotation.Transactional;
 
-// TODO: Auto-generated Javadoc
 /**
  * The Class District.
  * 
- * @author nileshp
+ * @author dhananjay
  * @since v1.0.0
  */
 @Configurable
@@ -41,25 +41,19 @@ public class District extends BaseDomain implements Serializable {
 	private static final transient long serialVersionUID = 1L;
 
 	/** The name. */
-	@Column(length = 100)
+	@Column(length = 600)
 	@NotEmpty
 	private String name;
 
 	/** The total constituencies. */
-	@Column(name = "total_constituencies")
 	@NotNull
-	private int totalConstituencies;
+	private Integer totalConstituencies = 0;
 
 	/** The division. */
 	@ManyToOne
+	@JoinColumn(name = "division_id")
 	private Division division;
 
-	/** The state. */
-	/*
-	 * @ManyToOne private State state;
-	 */
-
-	/** The district repository. */
 	@Autowired
 	private transient DistrictRepository districtRepository;
 
@@ -86,11 +80,6 @@ public class District extends BaseDomain implements Serializable {
 	}
 
 	// -------------------------------Domain_Methods----------------------------------------------
-	/**
-	 * Gets the district repository.
-	 * 
-	 * @return the district repository
-	 */
 	public static DistrictRepository getDistrictRepository() {
 		DistrictRepository districtRepository = new District().districtRepository;
 		if (districtRepository == null) {
@@ -100,113 +89,11 @@ public class District extends BaseDomain implements Serializable {
 		return districtRepository;
 	}
 
-	/**
-	 * Find districts by division id.
-	 * 
-	 * @param divisionid
-	 *            the divisionid
-	 * @param sortBy
-	 *            the sort by
-	 * @param sortOrder
-	 *            the sort order
-	 * @param locale
-	 *            the locale
-	 * @return the list
-	 * @author nileshp
-	 * @since v1.0.0
-	 */
 	@Transactional(readOnly = true)
-	public static List<District> findDistrictsByDivisionId(
-			final Long divisionid, final String sortBy, final String sortOrder,
-			final String locale) {
-		return getDistrictRepository().findDistrictsByDivisionId(divisionid,
-				sortBy, sortOrder, locale);
-	}
-
-	/**
-	 * Find districts by division name.
-	 * 
-	 * @param divisionName
-	 *            the division name
-	 * @param orderBy
-	 *            the order by
-	 * @param sortOrder
-	 *            the sort order
-	 * @param locale
-	 *            the locale
-	 * @return the list
-	 * @author nileshp
-	 * @since v1.0.0
-	 */
-	@Transactional(readOnly = true)
-	public static List<District> findDistrictsByDivisionName(
-			final String divisionName, final String orderBy,
-			final String sortOrder, final String locale) {
-		return getDistrictRepository().findDistrictsByDivisionName(
-				divisionName, orderBy, sortOrder, locale);
-	}
-
-	/**
-	 * Find districts by state id.
-	 * 
-	 * @param stateid
-	 *            the stateid
-	 * @param sortBy
-	 *            the sort by
-	 * @param sortOrder
-	 *            the sort order
-	 * @param locale
-	 *            the locale
-	 * @return the list
-	 * @author nileshp
-	 * @since v1.0.0
-	 */
-	@Transactional(readOnly = true)
-	public static List<District> findDistrictsBystateId(final Long stateid,
+	public static List<District> findDistrictsByStateId(final Long stateid,
 			final String sortBy, final String sortOrder, final String locale) {
 		return getDistrictRepository().findDistrictsByStateId(stateid, sortBy,
 				sortOrder, locale);
-	}
-
-	/**
-	 * Find districts by state name.
-	 * 
-	 * @param stateName
-	 *            the state name
-	 * @param orderBy
-	 *            the order by
-	 * @param sortOrder
-	 *            the sort order
-	 * @return the list
-	 * @author nileshp
-	 * @since v1.0.0
-	 */
-	@Transactional(readOnly = true)
-	public static List<District> findDistrictsBystateName(
-			final String stateName, final String orderBy, final String sortOrder) {
-		return getDistrictRepository().findDistrictsByStateName(stateName,
-				orderBy, sortOrder);
-	}
-
-	/**
-	 * Find districts by constituency id.
-	 * 
-	 * @param constituencyId
-	 *            the constituency id
-	 * @param sortBy
-	 *            the sort by
-	 * @param sortOrder
-	 *            the sort order
-	 * @return the list
-	 * @author nileshp
-	 * @since v1.0.0
-	 */
-	@Transactional(readOnly = true)
-	public static List<District> findDistrictsByConstituencyId(
-			final Long constituencyId, final String sortBy,
-			final String sortOrder) {
-		return getDistrictRepository().findDistrictsByConstituencyId(
-				constituencyId, sortBy, sortOrder);
 	}
 
 	// ------------------------------------------Getters/Setters-----------------------------------
@@ -267,21 +154,4 @@ public class District extends BaseDomain implements Serializable {
 	public void setDivision(final Division division) {
 		this.division = division;
 	}
-
-	/**
-	 * Gets the state.
-	 * 
-	 * @return the state
-	 */
-	/*
-	 * public State getState() { return state; }
-	 *//**
-	 * Sets the state.
-	 * 
-	 * @param state
-	 *            the new state
-	 */
-	/*
-	 * public void setState(final State state) { this.state = state; }
-	 */
 }
