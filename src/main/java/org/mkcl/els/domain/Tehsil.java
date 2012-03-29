68443687fdae5447395e26699c12e164c141c33a
dhananjayb <dhananjayb@348444cb-318d-0e48-9875-12270ded7a77>
2012-03-29 05:08:09 +0000

    changed to remove TehsilRepository

--- src/main/java/org/mkcl/els/domain/Tehsil.java
+++ src/main/java/org/mkcl/els/domain/Tehsil.java
@@ -5,33 +5,31 @@
  *
  * Project: e-Legislature
  * File: org.mkcl.els.domain.Tehsil.java
- * Created On: Mar 8, 2012
+ * Created On: Mar 20, 2012
  */
 package org.mkcl.els.domain;
 
 import java.io.Serializable;
-import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import org.hibernate.validator.constraints.NotEmpty;
-import org.mkcl.els.repository.TehsilRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Configurable;
-import org.springframework.transaction.annotation.Transactional;
 
+// TODO: Auto-generated Javadoc
 /**
  * The Class Tehsil.
- *
- * @author amitb
- * @version v1.0.0
+ * 
+ * @author dhananjay
+ * @since v1.0.0
  */
 @Configurable
 @Entity
-@Table(name = "tehsils")
+@Table(name = "masters_tehsils")
 public class Tehsil extends BaseDomain implements Serializable {
 
     // --------------------Attributes-------------------------------------------
@@ -40,15 +38,17 @@ public class Tehsil extends BaseDomain implements Serializable {
     private static final transient long serialVersionUID = 1L;
 
     /** The name. */
-    @Column(length = 100, nullable = false)
+    @Column(length = 600)
     @NotEmpty
     private String name;
 
     /** The district. */
     @ManyToOne
+    @JoinColumn(name = "district_id")
     private District district;
 
     // --------------------Constructors-------------------------------------------
+
     /**
      * Instantiates a new tehsil.
      */
@@ -58,7 +58,7 @@ public class Tehsil extends BaseDomain implements Serializable {
 
     /**
      * Instantiates a new tehsil.
-     *
+     * 
      * @param name the name
      * @param district the district
      */
@@ -68,11 +68,13 @@ public class Tehsil extends BaseDomain implements Serializable {
         this.district = district;
     }
 
+    // -------------------------------Domain_Methods----------------------------------------------
+
     // ------------------------------------------Getters/Setters-----------------------------------
 
     /**
      * Gets the name.
-     *
+     * 
      * @return the name
      */
     public String getName() {
@@ -81,7 +83,7 @@ public class Tehsil extends BaseDomain implements Serializable {
 
     /**
      * Sets the name.
-     *
+     * 
      * @param name the new name
      */
     public void setName(final String name) {
@@ -90,7 +92,7 @@ public class Tehsil extends BaseDomain implements Serializable {
 
     /**
      * Gets the district.
-     *
+     * 
      * @return the district
      */
     public District getDistrict() {
@@ -99,55 +101,11 @@ public class Tehsil extends BaseDomain implements Serializable {
 
     /**
      * Sets the district.
-     *
+     * 
      * @param district the new district
      */
     public void setDistrict(final District district) {
         this.district = district;
     }
 
-    /** The tehsil repository. */
-    @Autowired
-    private transient TehsilRepository tehsilRepository;
-
-    // -------------------------------Domain_Methods----------------------------------------------
-    /**
-     * Gets the repository.
-     *
-     * @return the repository
-     */
-    public static TehsilRepository getTehsilRepository() {
-        TehsilRepository tehsilRepository = new Tehsil().tehsilRepository;
-        if (tehsilRepository == null) {
-            throw new IllegalStateException(
-                    "TehsilRepository has not been injected in Tehsil Domail");
-        }
-        return tehsilRepository;
-    }
-
-    /**
-     * Find tehsils by district name.
-     *
-     * @param name the name
-     * @return the list< tehsil>
-     * @author nileshp
-     * @since v1.0.0
-     */
-    @Transactional
-    public static List<Tehsil> findTehsilsByDistrictName(final String name) {
-        return getTehsilRepository().findTehsilsByDistrictName(name);
-    }
-
-    /**
-     * Find tehsils by district id.
-     *
-     * @param districtId the district id
-     * @return the list< tehsil>
-     * @author nileshp
-     * @since v1.0.0
-     */
-    @Transactional
-    public static List<Tehsil> findTehsilsByDistrictId(final Long districtId) {
-        return getTehsilRepository().findTehsilsByDistrictId(districtId);
-    }
 }
