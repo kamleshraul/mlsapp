e4a129cc8a542863f7782ef7118c8d18b149596f
dhananjayb <dhananjayb@348444cb-318d-0e48-9875-12270ded7a77>
2012-03-29 10:25:09 +0000

    ELS-179 ELS-180 ELS-181 ELS-182

--- src/main/java/org/mkcl/els/domain/Constituency.java
+++ src/main/java/org/mkcl/els/domain/Constituency.java
@@ -5,8 +5,9 @@
  *
  * Project: e-Legislature
  * File: org.mkcl.els.domain.Constituency.java
- * Created On: Mar 8, 2012
+ * Created On: Mar 20, 2012
  */
+
 package org.mkcl.els.domain;
 
 import java.io.Serializable;
@@ -18,57 +19,83 @@ import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
-import javax.validation.constraints.NotNull;
 
 import org.hibernate.validator.constraints.NotEmpty;
-import org.mkcl.els.repository.ConstituencyRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Configurable;
-import org.springframework.transaction.annotation.Transactional;
 
+// TODO: Auto-generated Javadoc
 /**
  * The Class Constituency.
- *
- * @author sandeeps
+ * 
+ * @author dhananjay
  * @since v1.0.0
  */
 @Configurable
 @Entity
-@Table(name = "constituencies")
+@Table(name = "masters_constituencies")
 public class Constituency extends BaseDomain implements Serializable {
 
     // ---------------------------------Attributes-------------------------------------------------
     /** The Constant serialVersionUID. */
-    private static final transient long serialVersionUID = 1L;
+    private transient static final long serialVersionUID = 1L;
 
     /** The name. */
-    @Column(length = 100, nullable = false)
+    @Column(length = 600)
     @NotEmpty
     private String name;
 
-    /** The number. */
-    @Column(length = 100)
-    private String number;
+    /** The assembly council type. */
+    @ManyToOne
+    @JoinColumn(name = "house_id")
+    private HouseType houseType;
 
     /** The districts. */
     @ManyToMany(fetch = FetchType.LAZY)
-    @JoinTable(name = "constituency_district", joinColumns = @JoinColumn(
-            name = "constituency_id", referencedColumnName = "id"),
+    @JoinTable(name = "associations_constituency_district",
+            joinColumns = @JoinColumn(name = "constituency_id",
+                    referencedColumnName = "id"),
             inverseJoinColumns = @JoinColumn(name = "district_id",
                     referencedColumnName = "id"))
-    @NotNull
     private List<District> districts;
 
-    /** The reserved. */
-    private Boolean reserved = false;
+    /** The division. */
+    @Column(length = 600)    
+    private String divisionName;
 
-    /** The constituency repository. */
-    @Autowired
-    private transient ConstituencyRepository constituencyRepository;
+    /** The voters. */
+    private Integer voters;
+
+    /** The is reserved. */
+    @Column
+    private Boolean isReserved=false;
+
+    /** The reserved for. */
+    @ManyToOne
+    @JoinColumn(name = "reservation_id")
+    private Reservation reservedFor;
+
+    /** The nearest railway station. */
+    @OneToOne
+    @JoinColumn(name = "railwaystation_id")
+    private RailwayStation nearestRailwayStation;
+
+    /** The nearest airport. */
+    @OneToOne
+    @JoinColumn(name = "airport_id")
+    private Airport nearestAirport;
+
+    /** The is retired. */
+    @Column
+    private Boolean isRetired = false;
+
+    /** The number. */
+    @Column(length = 300)
+    private String number;
 
     // ---------------------------------Constructors----------------------------------------------
-
     /**
      * Instantiates a new constituency.
      */
@@ -76,100 +103,12 @@ public class Constituency extends BaseDomain implements Serializable {
         super();
     }
 
-    /**
-     * Instantiates a new constituency.
-     *
-     * @param name the name
-     * @param number the number
-     * @param districts the districts
-     * @param reserved the reserved
-     */
-    public Constituency(final String name,
-            final String number,
-            final List<District> districts,
-            final boolean reserved) {
-        super();
-        this.name = name;
-        this.number = number;
-        this.districts = districts;
-        this.reserved = reserved;
-    }
-
     // -------------------------------Domain_Methods----------------------------------------------
 
-    /**
-     * Gets the constituency repository.
-     *
-     * @return the constituency repository
-     */
-    public static ConstituencyRepository getConstituencyRepository() {
-        final ConstituencyRepository repository = new Constituency().constituencyRepository;
-        if (repository == null) {
-            throw new IllegalStateException(
-                    "ConstituencyRepository has not been injected");
-        }
-        return repository;
-    }
-
-    /**
-     * Find constituencies starting with.
-     *
-     * @param param the param
-     * @param locale the locale
-     * @return the list
-     * @author sandeeps
-     * @since v1.0.0
-     */
-    @Transactional(readOnly = true)
-    public static List<Reference> findConstituenciesStartingWith(final String param,
-                                                                 final String locale) {
-        return getConstituencyRepository().findConstituenciesRefStartingWith(
-                param, locale);
-    }
-
-    /**
-     * Find constituencies by district id.
-     *
-     * @param districtId the district id
-     * @return the list
-     * @author sandeeps
-     * @since v1.0.0
-     */
-    @Transactional(readOnly = true)
-    public static List<Constituency> findConstituenciesByDistrictId(final Long districtId) {
-        return getConstituencyRepository().findConstituenciesByDistrictId(
-                districtId);
-    }
-
-    /**
-     * Find constituencies by district name.
-     *
-     * @param name the name
-     * @param locale the locale
-     * @return the list
-     * @author sandeeps
-     * @since v1.0.0
-     */
-    @Transactional(readOnly = true)
-    public static List<Constituency> findConstituenciesByDistrictName(final String name,
-                                                                      final String locale) {
-        return getConstituencyRepository().findConstituenciesByDistrictName(
-                name, locale);
-    }
-
-    /* (non-Javadoc)
-     * @see java.lang.Object#toString()
-     */
-    @Override
-    public String toString() {
-        return this.getName();
-    }
-
     // ------------------------------------------Getters/Setters-----------------------------------
-
     /**
      * Gets the name.
-     *
+     * 
      * @return the name
      */
     public String getName() {
@@ -178,34 +117,34 @@ public class Constituency extends BaseDomain implements Serializable {
 
     /**
      * Sets the name.
-     *
+     * 
      * @param name the new name
      */
-    public void setName(final String name) {
+    public void setName(String name) {
         this.name = name;
     }
 
     /**
-     * Gets the number.
-     *
-     * @return the number
+     * Gets the house type.
+     * 
+     * @return the house type
      */
-    public String getNumber() {
-        return number;
+    public HouseType getHouseType() {
+        return houseType;
     }
 
     /**
-     * Sets the number.
-     *
-     * @param number the new number
+     * Sets the house type.
+     * 
+     * @param houseType the new house type
      */
-    public void setNumber(final String number) {
-        this.number = number;
+    public void setHouseType(HouseType houseType) {
+        this.houseType = houseType;
     }
 
     /**
      * Gets the districts.
-     *
+     * 
      * @return the districts
      */
     public List<District> getDistricts() {
@@ -214,28 +153,146 @@ public class Constituency extends BaseDomain implements Serializable {
 
     /**
      * Sets the districts.
-     *
+     * 
      * @param districts the new districts
      */
-    public void setDistricts(final List<District> districts) {
+    public void setDistricts(List<District> districts) {
         this.districts = districts;
     }
 
     /**
-     * Checks if is reserved.
-     *
-     * @return true, if is reserved
+     * Gets the division name.
+     * 
+     * @return the division name
      */
-    public boolean isReserved() {
-        return reserved;
+    public String getDivisionName() {
+        return divisionName;
     }
 
     /**
-     * Sets the reserved.
-     *
-     * @param reserved the new reserved
+     * Sets the division name.
+     * 
+     * @param divisionName the new division name
      */
-    public void setReserved(final boolean reserved) {
-        this.reserved = reserved;
+    public void setDivisionName(String divisionName) {
+        this.divisionName = divisionName;
     }
+
+    /**
+     * Gets the voters.
+     * 
+     * @return the voters
+     */
+    public Integer getVoters() {
+        return voters;
+    }
+
+    /**
+     * Sets the voters.
+     * 
+     * @param voters the new voters
+     */
+    public void setVoters(Integer voters) {
+        this.voters = voters;
+    }
+    
+    /**
+     * Sets the checks if is reserved.
+     *
+     * @param isReserved the new checks if is reserved
+     */
+    public void setIsReserved(Boolean isReserved) {
+        this.isReserved = isReserved;
+    }
+
+    /**
+     * Gets the reserved for.
+     * 
+     * @return the reserved for
+     */
+    public Reservation getReservedFor() {
+        return reservedFor;
+    }
+
+    /**
+     * Sets the reserved for.
+     * 
+     * @param reservedFor the new reserved for
+     */
+    public void setReservedFor(Reservation reservedFor) {
+        this.reservedFor = reservedFor;
+    }
+
+    /**
+     * Gets the nearest railway station.
+     * 
+     * @return the nearest railway station
+     */
+    public RailwayStation getNearestRailwayStation() {
+        return nearestRailwayStation;
+    }
+
+    /**
+     * Sets the nearest railway station.
+     * 
+     * @param nearestRailwayStation the new nearest railway station
+     */
+    public void setNearestRailwayStation(RailwayStation nearestRailwayStation) {
+        this.nearestRailwayStation = nearestRailwayStation;
+    }
+
+    /**
+     * Gets the nearest airport.
+     * 
+     * @return the nearest airport
+     */
+    public Airport getNearestAirport() {
+        return nearestAirport;
+    }
+
+    /**
+     * Sets the nearest airport.
+     * 
+     * @param nearestAirport the new nearest airport
+     */
+    public void setNearestAirport(Airport nearestAirport) {
+        this.nearestAirport = nearestAirport;
+    }
+
+    /**
+     * Gets the number.
+     * 
+     * @return the number
+     */
+    public String getNumber() {
+        return number;
+    }
+
+    /**
+     * Sets the number.
+     * 
+     * @param number the new number
+     */
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    /**
+     * Gets the checks if is reserved.
+     * 
+     * @return the checks if is reserved
+     */
+    public Boolean getIsReserved() {
+        return isReserved;
+    }
+
+    /**
+     * Gets the checks if is retired.
+     * 
+     * @return the checks if is retired
+     */
+    public Boolean getIsRetired() {
+        return isRetired;
+    }
+
 }
