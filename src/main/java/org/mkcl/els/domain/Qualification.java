7c7bb9fca335414cb873ef902efe9ed0648f9879
vishals <vishals@348444cb-318d-0e48-9875-12270ded7a77>
2012-04-04 06:17:57 +0000

    code formatted

--- src/main/java/org/mkcl/els/domain/Qualification.java
+++ src/main/java/org/mkcl/els/domain/Qualification.java
@@ -1,3 +1,12 @@
+/**
+ * See the file LICENSE for redistribution information.
+ *
+ * Copyright (c) 2011 MKCL.  All rights reserved.
+ *
+ * Project: els
+ * File: org.mkcl.els.domain.Qualification
+ * Created On: Apr 4, 2012
+ */
 
 package org.mkcl.els.domain;
 
@@ -11,42 +20,80 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import org.springframework.beans.factory.annotation.Configurable;
+
+/**
+ * The Class Qualification.
+ *
+ * @author vishals
+ * @version 1.0.0
+ */
 @Configurable
 @Entity
-@Table(name="qualifications")
-public class Qualification extends BaseDomain implements Serializable{
+@Table(name = "qualifications")
+public class Qualification extends BaseDomain implements Serializable {
 
-	private transient static final long serialVersionUID = 1L;
-	
-	@ManyToOne(fetch=FetchType.LAZY)
-	@JoinColumn(name="degree_id")
-	private Degree degree;
-	
-	@Column(length=1000)
-	private String details;
+    /** The Constant serialVersionUID. */
+    private transient static final long serialVersionUID = 1L;
 
-	public Qualification() {
-		super();
-	}
+    /** The degree. */
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "degree_id")
+    private Degree degree;
 
-	public Qualification(Degree degree) {
-		super();
-		this.degree = degree;
-	}
+    /** The details. */
+    @Column(length = 1000)
+    private String details;
 
-	public Degree getDegree() {
-		return degree;
-	}
+    /**
+     * Instantiates a new qualification.
+     */
+    public Qualification() {
+        super();
+    }
 
-	public void setDegree(Degree degree) {
-		this.degree = degree;
-	}
+    /**
+     * Instantiates a new qualification.
+     *
+     * @param degree the degree
+     */
+    public Qualification(final Degree degree) {
+        super();
+        this.degree = degree;
+    }
 
-	public String getDetails() {
-		return details;
-	}
+    /**
+     * Gets the degree.
+     *
+     * @return the degree
+     */
+    public Degree getDegree() {
+        return degree;
+    }
 
-	public void setDetails(String details) {
-		this.details = details;
-	}	
+    /**
+     * Sets the degree.
+     *
+     * @param degree the new degree
+     */
+    public void setDegree(final Degree degree) {
+        this.degree = degree;
+    }
+
+    /**
+     * Gets the details.
+     *
+     * @return the details
+     */
+    public String getDetails() {
+        return details;
+    }
+
+    /**
+     * Sets the details.
+     *
+     * @param details the new details
+     */
+    public void setDetails(final String details) {
+        this.details = details;
+    }
 }
