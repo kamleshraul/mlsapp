1ea7192e3a6651e69673cbd0faf3f409dbfda719
vishals <vishals@348444cb-318d-0e48-9875-12270ded7a77>
2012-04-05 06:36:16 +0000

    code formatted

--- src/main/java/org/mkcl/els/repository/MemberRoleRepository.java
+++ src/main/java/org/mkcl/els/repository/MemberRoleRepository.java
@@ -1,13 +1,36 @@
+/**
+ * See the file LICENSE for redistribution information.
+ *
+ * Copyright (c) 2011 MKCL.  All rights reserved.
+ *
+ * Project: els
+ * File: org.mkcl.els.repository.MemberRoleRepository
+ * Created On: Apr 5, 2012
+ */
 package org.mkcl.els.repository;
 
 import org.mkcl.els.domain.MemberRole;
 import org.springframework.stereotype.Repository;
 
+/**
+ * The Class MemberRoleRepository.
+ *
+ * @author vishals
+ * @version 1.0.0
+ */
 @Repository
 public class MemberRoleRepository extends BaseRepository<MemberRole, Long> {
 
-    public MemberRole findByNameHouseTypeLocale(String roleName,
-            Long houseTypeId, String locale) {
+    /**
+     * Find by name house type locale.
+     *
+     * @param roleName the role name
+     * @param houseTypeId the house type id
+     * @param locale the locale
+     * @return the member role
+     */
+    public MemberRole findByNameHouseTypeLocale(final String roleName,
+            final Long houseTypeId, final String locale) {
         String query = "SELECT m FROM MemberRole m WHERE m.name='" + roleName
                 + "' AND m.houseType.id=" + houseTypeId + " AND m.locale='"
                 + locale + "'";
