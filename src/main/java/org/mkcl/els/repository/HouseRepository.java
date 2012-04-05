48e66d35b81d6755e08f1f76462e841480ab1ca4
vishals <vishals@348444cb-318d-0e48-9875-12270ded7a77>
2012-04-05 08:42:36 +0000

    code formatted

--- src/main/java/org/mkcl/els/repository/HouseRepository.java
+++ src/main/java/org/mkcl/els/repository/HouseRepository.java
@@ -1,28 +1,61 @@
+/**
+ * See the file LICENSE for redistribution information.
+ *
+ * Copyright (c) 2011 MKCL.  All rights reserved.
+ *
+ * Project: els
+ * File: org.mkcl.els.repository.HouseRepository
+ * Created On: Apr 5, 2012
+ */
 package org.mkcl.els.repository;
 
 import java.util.Date;
+
 import org.mkcl.els.domain.House;
 import org.springframework.stereotype.Repository;
 
 import com.trg.search.Search;
 
+/**
+ * The Class HouseRepository.
+ *
+ * @author vishals
+ * @version 1.0.0
+ */
 @Repository
-public class HouseRepository extends BaseRepository<House,Long>{
+public class HouseRepository extends BaseRepository<House, Long> {
 
-	public House findCurrentHouse(String locale){
-		Search search=new Search();
-		search.addFilterEqual("locale",locale);
-		Date currentDate=new Date();
-		search.addFilterGreaterOrEqual("lastDate", currentDate);
-		search.addFilterLessOrEqual("firstDate",currentDate);
-		return this.searchUnique(search);
-	}
-	
-	public House findHouseByToFromDate(Date fromDate,Date toDate,String locale){
-		Search search=new Search();
-		search.addFilterEqual("locale",locale);
-		search.addFilterGreaterOrEqual("lastDate", toDate);
-		search.addFilterLessOrEqual("firstDate",fromDate);
-		return this.searchUnique(search);
-	}
+    /**
+     * Find current house.
+     *
+     * @param locale the locale
+     * @return the house
+     */
+    public House findCurrentHouse(final String locale) {
+        Search search = new Search();
+        search.addFilterEqual("locale", locale);
+        Date currentDate = new Date();
+        search.addFilterGreaterOrEqual("lastDate", currentDate);
+        search.addFilterLessOrEqual("firstDate", currentDate);
+        return this.searchUnique(search);
+    }
+
+    /**
+     * Find house by to from date.
+     *
+     * @param fromDate the from date
+     * @param toDate the to date
+     * @param locale the locale
+     * @return the house
+     */
+    public House findHouseByToFromDate(
+            final Date fromDate,
+            final Date toDate,
+            final String locale) {
+        Search search = new Search();
+        search.addFilterEqual("locale", locale);
+        search.addFilterGreaterOrEqual("lastDate", toDate);
+        search.addFilterLessOrEqual("firstDate", fromDate);
+        return this.searchUnique(search);
+    }
 }
