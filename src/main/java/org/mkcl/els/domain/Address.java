ab61bf6a50d3443100e43b30126fd076a76bbfd5
vishals <vishals@348444cb-318d-0e48-9875-12270ded7a77>
2012-04-05 05:01:11 +0000

    code formatted

--- src/main/java/org/mkcl/els/domain/Address.java
+++ src/main/java/org/mkcl/els/domain/Address.java
@@ -17,12 +17,11 @@ import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
-import org.hibernate.validator.constraints.NotEmpty;
 import org.springframework.beans.factory.annotation.Configurable;
 
 /**
  * The Class Address.
- * 
+ *
  * @author Dhananjay
  * @since v1.0.0
  */
@@ -39,20 +38,25 @@ public class Address extends BaseDomain implements Serializable {
     @Column(length = 255)
     private String details; // for Address field in class diagram
 
+    /** The city. */
     @Column(length = 100)
     private String city;
 
+    /** The pincode. */
     @Column(length = 30)
     private String pincode;
 
+    /** The tehsil. */
     @OneToOne
     @JoinColumn(name = "tehsil_id")
     private Tehsil tehsil;
 
+    /** The district. */
     @OneToOne
     @JoinColumn(name = "district_id")
     private District district;
 
+    /** The state. */
     @OneToOne
     @JoinColumn(name = "state_id")
     private State state;
@@ -67,7 +71,7 @@ public class Address extends BaseDomain implements Serializable {
 
     /**
      * Instantiates a new address.
-     * 
+     *
      * @param details the details
      * @param city the city
      * @param pincode the pincode
@@ -75,8 +79,13 @@ public class Address extends BaseDomain implements Serializable {
      * @param district the district
      * @param state the state
      */
-    public Address(String details, String city, String pincode, Tehsil tehsil,
-            District district, State state) {
+    public Address(
+            final String details,
+            final String city,
+            final String pincode,
+            final Tehsil tehsil,
+            final District district,
+            final State state) {
         super();
         this.details = details;
         this.city = city;
@@ -92,7 +101,7 @@ public class Address extends BaseDomain implements Serializable {
 
     /**
      * Gets the details.
-     * 
+     *
      * @return the details
      */
     public String getDetails() {
@@ -101,50 +110,100 @@ public class Address extends BaseDomain implements Serializable {
 
     /**
      * Sets the details.
-     * 
+     *
      * @param details the new details
      */
     public void setDetails(final String details) {
         this.details = details;
     }
 
+    /**
+     * Gets the city.
+     *
+     * @return the city
+     */
     public String getCity() {
         return city;
     }
 
-    public void setCity(String city) {
+    /**
+     * Sets the city.
+     *
+     * @param city the new city
+     */
+    public void setCity(final String city) {
         this.city = city;
     }
 
+    /**
+     * Gets the pincode.
+     *
+     * @return the pincode
+     */
     public String getPincode() {
         return pincode;
     }
 
-    public void setPincode(String pincode) {
+    /**
+     * Sets the pincode.
+     *
+     * @param pincode the new pincode
+     */
+    public void setPincode(final String pincode) {
         this.pincode = pincode;
     }
 
+    /**
+     * Gets the tehsil.
+     *
+     * @return the tehsil
+     */
     public Tehsil getTehsil() {
         return tehsil;
     }
 
-    public void setTehsil(Tehsil tehsil) {
+    /**
+     * Sets the tehsil.
+     *
+     * @param tehsil the new tehsil
+     */
+    public void setTehsil(final Tehsil tehsil) {
         this.tehsil = tehsil;
     }
 
+    /**
+     * Gets the district.
+     *
+     * @return the district
+     */
     public District getDistrict() {
         return district;
     }
 
-    public void setDistrict(District district) {
+    /**
+     * Sets the district.
+     *
+     * @param district the new district
+     */
+    public void setDistrict(final District district) {
         this.district = district;
     }
 
+    /**
+     * Gets the state.
+     *
+     * @return the state
+     */
     public State getState() {
         return state;
     }
 
-    public void setState(State state) {
+    /**
+     * Sets the state.
+     *
+     * @param state the new state
+     */
+    public void setState(final State state) {
         this.state = state;
     }
 
