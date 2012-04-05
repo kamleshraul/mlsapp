5c3e7694a200f0d633fa98d4fcb2816590e00055
vishals <vishals@348444cb-318d-0e48-9875-12270ded7a77>
2012-04-05 08:39:56 +0000

    code formatted

--- src/main/java/org/mkcl/els/repository/MemberHouseRoleRepository.java
+++ src/main/java/org/mkcl/els/repository/MemberHouseRoleRepository.java
@@ -1,3 +1,12 @@
+/**
+ * See the file LICENSE for redistribution information.
+ *
+ * Copyright (c) 2011 MKCL.  All rights reserved.
+ *
+ * Project: els
+ * File: org.mkcl.els.repository.MemberHouseRoleRepository
+ * Created On: Apr 5, 2012
+ */
 package org.mkcl.els.repository;
 
 import java.io.Serializable;
@@ -10,40 +19,63 @@ import org.springframework.stereotype.Repository;
 
 import com.trg.search.Search;
 
+/**
+ * The Class MemberHouseRoleRepository.
+ *
+ * @author vishals
+ * @version 1.0.0
+ */
 @Repository
 public class MemberHouseRoleRepository extends
-        BaseRepository<HouseMemberRoleAssociation, Serializable> {
+BaseRepository<HouseMemberRoleAssociation, Serializable> {
 
-    public HouseMemberRoleAssociation findByMemberIdAndId(Long memberId,
-            int recordIndex) {
+    /**
+     * Find by member id and id.
+     *
+     * @param memberId the member id
+     * @param recordIndex the record index
+     * @return the house member role association
+     */
+    public HouseMemberRoleAssociation findByMemberIdAndId(final Long memberId,
+            final int recordIndex) {
         String query = "SELECT m FROM HouseMemberRoleAssociation m WHERE m.member.id="
                 + memberId + " AND m.recordIndex=" + recordIndex;
 
         try {
             return (HouseMemberRoleAssociation) this.em().createQuery(query)
                     .getSingleResult();
-        }
-        catch (NoResultException e) {
+        } catch (NoResultException e) {
             e.printStackTrace();
             return new HouseMemberRoleAssociation();
         }
     }
 
+    /**
+     * Find highest record index.
+     *
+     * @param member the member
+     * @return the int
+     */
     @SuppressWarnings("unchecked")
-    public int findHighestRecordIndex(Long member) {
+    public int findHighestRecordIndex(final Long member) {
         String query = "SELECT m FROM HouseMemberRoleAssociation m WHERE m.member.id="
                 + member + " ORDER BY m.recordIndex desc LIMIT 1";
         List<HouseMemberRoleAssociation> associations = this.em()
                 .createQuery(query).getResultList();
-        if(associations.isEmpty()){
-        	return 0;
-        }else{
-        return associations.get(0).getRecordIndex();
+        if (associations.isEmpty()) {
+            return 0;
+        } else {
+            return associations.get(0).getRecordIndex();
         }
     }
 
-    public HouseMemberRoleAssociation findByPK(
-            HouseMemberRoleAssociation association) {
+    /**
+     * Find by pk.
+     *
+     * @param association the association
+     * @return the house member role association
+     */
+    public HouseMemberRoleAssociation findByPK(final HouseMemberRoleAssociation association) {
         Search search = new Search();
         search.addFilterEqual("member", association.getMember());
         search.addFilterEqual("role", association.getRole());
