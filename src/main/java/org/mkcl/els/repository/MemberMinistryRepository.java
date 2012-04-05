9d1cea90737163e64a67a3ebb8513e2b283acaf9
vishals <vishals@348444cb-318d-0e48-9875-12270ded7a77>
2012-04-05 08:38:29 +0000

    code formatted

--- src/main/java/org/mkcl/els/repository/MemberMinistryRepository.java
+++ src/main/java/org/mkcl/els/repository/MemberMinistryRepository.java
@@ -1,3 +1,12 @@
+/**
+ * See the file LICENSE for redistribution information.
+ *
+ * Copyright (c) 2011 MKCL.  All rights reserved.
+ *
+ * Project: els
+ * File: org.mkcl.els.repository.MemberMinistryRepository
+ * Created On: Apr 5, 2012
+ */
 package org.mkcl.els.repository;
 
 import java.io.Serializable;
@@ -10,41 +19,64 @@ import org.springframework.stereotype.Repository;
 
 import com.trg.search.Search;
 
+/**
+ * The Class MemberMinistryRepository.
+ *
+ * @author vishals
+ * @version 1.0.0
+ */
 @Repository
 public class MemberMinistryRepository extends
-        BaseRepository<MemberMinistryAssociation, Serializable> {
+BaseRepository<MemberMinistryAssociation, Serializable> {
 
-    public MemberMinistryAssociation findByMemberIdAndId(Long memberId,
-            int recordIndex) {
+    /**
+     * Find by member id and id.
+     *
+     * @param memberId the member id
+     * @param recordIndex the record index
+     * @return the member ministry association
+     */
+    public MemberMinistryAssociation findByMemberIdAndId(final Long memberId,
+            final int recordIndex) {
         String query = "SELECT m FROM MemberMinistryAssociation m WHERE m.member.id="
                 + memberId + " AND m.recordIndex=" + recordIndex;
 
         try {
             return (MemberMinistryAssociation) this.em().createQuery(query)
                     .getSingleResult();
-        }
-        catch (NoResultException e) {
+        } catch (NoResultException e) {
             e.printStackTrace();
             return new MemberMinistryAssociation();
         }
     }
 
+    /**
+     * Find highest record index.
+     *
+     * @param member the member
+     * @return the int
+     */
     @SuppressWarnings("unchecked")
-    public int findHighestRecordIndex(Long member) {
+    public int findHighestRecordIndex(final Long member) {
         String query = "SELECT m FROM MemberMinistryAssociation m WHERE m.member.id="
                 + member + " ORDER BY m.recordIndex desc LIMIT 1";
         List<MemberMinistryAssociation> associations = this.em()
                 .createQuery(query).getResultList();
-        if(associations.isEmpty()){
-        return 0;
-        }else{
-        return associations.get(0).getRecordIndex();
+        if (associations.isEmpty()) {
+            return 0;
+        } else {
+            return associations.get(0).getRecordIndex();
         }
 
     }
 
-    public MemberMinistryAssociation findByPK(
-            MemberMinistryAssociation association) {
+    /**
+     * Find by pk.
+     *
+     * @param association the association
+     * @return the member ministry association
+     */
+    public MemberMinistryAssociation findByPK(final MemberMinistryAssociation association) {
         Search search = new Search();
         search.addFilterEqual("member", association.getMember());
         search.addFilterEqual("ministry", association.getMinistry());
