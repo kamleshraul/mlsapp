cad086136492e4fce1a000b9bd8c74a7f8df29c5
sandeeps <sandeeps@348444cb-318d-0e48-9875-12270ded7a77>
2012-04-06 07:49:44 +0000

    added changes a/c to  discussion with vishal sir

--- src/main/java/org/mkcl/els/service/impl/SecurityServiceImpl.java
+++ src/main/java/org/mkcl/els/service/impl/SecurityServiceImpl.java
@@ -13,12 +13,11 @@ import java.util.Collection;
 import java.util.Date;
 import java.util.HashSet;
 
+import org.mkcl.els.common.util.ApplicationConstants;
 import org.mkcl.els.common.vo.AuthUser;
-import org.mkcl.els.domain.AppUser;
 import org.mkcl.els.domain.Credential;
-import org.mkcl.els.domain.Employee;
-import org.mkcl.els.domain.Member;
 import org.mkcl.els.domain.Role;
+import org.mkcl.els.domain.User;
 import org.springframework.dao.DataAccessException;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.GrantedAuthorityImpl;
@@ -48,89 +47,38 @@ public class SecurityServiceImpl implements UserDetailsService {
     @Transactional(readOnly = true)
     public UserDetails loadUserByUsername(final String username)
             throws UsernameNotFoundException, DataAccessException {
-        Credential credential = Credential.findByFieldName(Credential.class,
-                "username", username, "");
-        // users will only be allowed login if they have an entry in credential
-        // table.
-        if (credential == null) {
-            throw new UsernameNotFoundException("User Not Found");
-        }
-        else if (!credential.isEnabled()) {
-            throw new UsernameNotFoundException("User Is Disabled");
-        }
-        // for locale based login add code here
-        //
-        Collection<GrantedAuthority> roles = new HashSet<GrantedAuthority>();
-        boolean hasUserRole = false;
-        if (credential.getRoles().isEmpty()) {
-            throw new UsernameNotFoundException(
-                    "User must have minimum role of 'USER' to login");
-        }
-        for (Role role : credential.getRoles()) {
-            roles.add(new GrantedAuthorityImpl(role.getName()));
-        }
-        // Here we are just looking for users by locale mr_IN and setting their
-        // values in the AuthUser.
-        // Here there needs to be code for getting the locale of the application
-        String firstName = "";
-        String title = "";
-        String middleName = "";
-        String lastName = "";
-        String photo = "";
-        if (credential.getUserType().equals("M")) {
-            Member member = Member.findByFieldName(Member.class, "credential",
-                    credential, "mr_IN");
-            if (member != null) {
-                firstName = member.getFirstName();
-                if (member.getTitle() == null) {
-                    title = "";
-                }
-                else {
-                    title = member.getTitle().getName();
-                }
-                middleName = member.getMiddleName();
-                lastName = member.getLastName();
-                photo = member.getPhoto();
-            }
-        }
-        else if (credential.getUserType().equals("E")) {
-            Employee employee = Employee.findByFieldName(Employee.class,
-                    "crdential", credential, "mr_IN");
-            if (employee != null) {
-                firstName = employee.getFirstName();
-                if (employee.getTitle() == null) {
-                    title = "";
-                }
-                else {
-                    title = employee.getTitle().getName();
-                }
-                middleName = employee.getMiddleName();
-                lastName = employee.getLastName();
-                photo = employee.getPhoto();
-            }
-        }
-        else {
-            AppUser user = AppUser.findByFieldName(AppUser.class, "credential",
-                    credential, "en_US");
-            if (user != null) {
-                firstName = user.getFirstName();
-                if (user.getTitle() == null) {
-                    title = "";
-                }
-                else {
-                    title = user.getTitle().getName();
-                }
-                middleName = user.getMiddleName();
-                lastName = user.getLastName();
-                photo = user.getPhoto();
-            }
-        }
-        // update the last login time of user
-        credential.setLastLoginTime(new Date());
-        credential.merge();
-        return new AuthUser(credential.getUsername(), credential.getPassword(),
-                credential.isEnabled(), true, true, true, roles, title,
-                firstName, middleName, lastName, credential.getDefaultHouseType()
-                        , photo, credential.getRoles(),credential.getId());
-    }
+    	//Here way needs to be found to read the current locale so that user info can be found
+    	//locale based.Currently it is set to mr_IN.
+    	User user=User.findByUserName(username, "mr_IN");
+		String houseType=null;
+    	if(user.getId()==null){
+            	throw new UsernameNotFoundException("User Not Found");
+    	}else{
+    		Credential credential=user.getCredential();
+    		if(!credential.isEnabled()){
+                throw new UsernameNotFoundException("User Not Found");
+    		}else{
+    			Collection<GrantedAuthority> roles = new HashSet<GrantedAuthority>();
+    			if(credential.getRoles().isEmpty()){
+                    throw new UsernameNotFoundException("User Not Found");
+    			}
+    	        for (Role role : credential.getRoles()) {
+    	            roles.add(new GrantedAuthorityImpl(role.getName()));
+    	            if(role.getName().equals("DATA_ENTRY_OPERATOR_LOWERHOUSE")||role.getName().equals("ADMIN_LOWERHOUSE")){
+    	            	houseType=ApplicationConstants.LOWER_HOUSE;
+    	            }else if(role.getName().equals("DATA_ENTRY_OPERATOR_UPPERHOUSE")||role.getName().equals("ADMIN_UPPERHOUSE")){
+    	            	houseType=ApplicationConstants.UPPER_HOUSE;
+    	            }else if(role.getName().equals("DATA_ENTRY_OPERATOR_BOTHHOUSE")||role.getName().equals("ADMIN_BOTHHOUSE")){
+    	            	houseType=ApplicationConstants.BOTH_HOUSE;
+    	            }
+    	        }
+    	        credential.setLastLoginTime(new Date());
+    	        credential.merge();
+    	        return new AuthUser(credential.getUsername(), credential.getPassword(),
+    	                credential.isEnabled(), true, true, true, roles, user.getTitle(),
+    	                user.getFirstName(),user.getMiddleName(), user.getLastName(),houseType,
+    	                        credential.getRoles());
+    		}
+    	}
+}
 }
