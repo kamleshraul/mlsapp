4272dc44c86a55d4c77f162f8a6bbb8cbdb4b419
sandeeps <sandeeps@348444cb-318d-0e48-9875-12270ded7a77>
2012-04-06 07:48:12 +0000

    removed associations from table name

--- src/main/java/org/mkcl/els/domain/associations/MemberMinistryAssociation.java
+++ src/main/java/org/mkcl/els/domain/associations/MemberMinistryAssociation.java
@@ -13,6 +13,7 @@ import java.io.Serializable;
 import java.util.Date;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.Id;
 import javax.persistence.IdClass;
 import javax.persistence.JoinColumn;
@@ -34,16 +35,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.transaction.annotation.Transactional;
 
+// TODO: Auto-generated Javadoc
 /**
  * The Class MemberMinistryAssociation.
- *
+ * 
  * @author amitd
  * @author sandeeps
  * @since v1.0.0
  */
 @Entity
 @Configurable
-@Table(name = "associations_member_ministry")
+@Table(name = "members_ministries")
 @IdClass(value = MemberMinistryAssociationPk.class)
 @JsonIgnoreProperties({ "member" })
 public class MemberMinistryAssociation implements Serializable {
@@ -54,13 +56,13 @@ public class MemberMinistryAssociation implements Serializable {
 
     /** The member. */
     @Id
-    @ManyToOne
+    @ManyToOne(fetch=FetchType.LAZY)
     @PrimaryKeyJoinColumn(name = "member_id", referencedColumnName = "id")
     private Member member;
 
     /** The ministry. */
     @Id
-    @ManyToOne
+    @ManyToOne(fetch=FetchType.LAZY)
     @PrimaryKeyJoinColumn(name = "ministry_id", referencedColumnName = "id")
     private Ministry ministry;
 
@@ -87,7 +89,6 @@ public class MemberMinistryAssociation implements Serializable {
     @Version
     private Long version;
 
-    /** The member ministry repository. */
     @Autowired
     private transient MemberMinistryRepository memberMinistryRepository;
 
@@ -101,41 +102,21 @@ public class MemberMinistryAssociation implements Serializable {
     }
 
     // ------------------------------------------Domain_Methods-----------------------------------
-    /**
-     * Gets the member ministry repository.
-     *
-     * @return the member ministry repository
-     */
     public static MemberMinistryRepository getMemberMinistryRepository() {
-        MemberMinistryRepository memberMinistryRepository =
-                new MemberMinistryAssociation().memberMinistryRepository;
+        MemberMinistryRepository memberMinistryRepository = new MemberMinistryAssociation().memberMinistryRepository;
         if (memberMinistryRepository == null) {
             throw new IllegalStateException(
-                    "MemberMinistryRepository has not been "
-                            +
-                    "injected in MemberMinistryAssociation Domain");
+                    "MemberMinistryRepository has not been injected in MemberMinistryAssociation Domain");
         }
         return memberMinistryRepository;
     }
 
-    /**
-     * Find by member id and id.
-     *
-     * @param memberId the member id
-     * @param id the id
-     * @return the member ministry association
-     */
     @Transactional(readOnly = true)
-    public static MemberMinistryAssociation findByMemberIdAndId(final Long memberId,
-            final int id) {
+    public static MemberMinistryAssociation findByMemberIdAndId(Long memberId,
+            int id) {
         return getMemberMinistryRepository().findByMemberIdAndId(memberId, id);
     }
 
-    /**
-     * Persist.
-     *
-     * @return the member ministry association
-     */
     @Transactional
     public MemberMinistryAssociation persist() {
         memberMinistryRepository.save(this);
@@ -143,11 +124,6 @@ public class MemberMinistryAssociation implements Serializable {
         return this;
     }
 
-    /**
-     * Merge.
-     *
-     * @return the member ministry association
-     */
     @Transactional
     public MemberMinistryAssociation merge() {
         memberMinistryRepository.merge(this);
@@ -155,43 +131,21 @@ public class MemberMinistryAssociation implements Serializable {
         return this;
     }
 
-    /**
-     * Removes the.
-     *
-     * @return true, if successful
-     */
     @Transactional
     public boolean remove() {
         return memberMinistryRepository.remove(this);
     }
 
-    /**
-     * Find highest record index.
-     *
-     * @param member the member
-     * @return the int
-     */
     @Transactional(readOnly = true)
-    public static int findHighestRecordIndex(final Long member) {
+    public static int findHighestRecordIndex(Long member) {
         return getMemberMinistryRepository().findHighestRecordIndex(member);
     }
 
-    /**
-     * Find by pk.
-     *
-     * @param association the association
-     * @return the member ministry association
-     */
     public static MemberMinistryAssociation findByPK(
-            final MemberMinistryAssociation association) {
+            MemberMinistryAssociation association) {
         return getMemberMinistryRepository().findByPK(association);
     }
 
-    /**
-     * Checks if is duplicate.
-     *
-     * @return the boolean
-     */
     public Boolean isDuplicate() {
         MemberMinistryAssociation duplicate = MemberMinistryAssociation
                 .findByPK(this);
@@ -200,25 +154,18 @@ public class MemberMinistryAssociation implements Serializable {
         }
         return true;
     }
-
-
-    /**
-     * Checks if is version mismatch.
-     *
-     * @return true, if is version mismatch
-     */
     @Transactional(readOnly = true)
     public boolean isVersionMismatch() {
         Boolean retVal = false;
         MemberMinistryAssociation domain = getMemberMinistryRepository().findByPK(
-                this);
+                    this);
         retVal = (!domain.getVersion().equals(this.version));
         return retVal;
     }
     // ------------------------------------------Getters/Setters-----------------------------------
     /**
      * Gets the member.
-     *
+     * 
      * @return the member
      */
     public Member getMember() {
@@ -227,16 +174,16 @@ public class MemberMinistryAssociation implements Serializable {
 
     /**
      * Sets the member.
-     *
+     * 
      * @param member the new member
      */
-    public void setMember(final Member member) {
+    public void setMember(Member member) {
         this.member = member;
     }
 
     /**
      * Gets the ministry.
-     *
+     * 
      * @return the ministry
      */
     public Ministry getMinistry() {
@@ -245,16 +192,16 @@ public class MemberMinistryAssociation implements Serializable {
 
     /**
      * Sets the ministry.
-     *
+     * 
      * @param ministry the new ministry
      */
-    public void setMinistry(final Ministry ministry) {
+    public void setMinistry(Ministry ministry) {
         this.ministry = ministry;
     }
 
     /**
      * Gets the from date.
-     *
+     * 
      * @return the from date
      */
     public Date getFromDate() {
@@ -263,16 +210,16 @@ public class MemberMinistryAssociation implements Serializable {
 
     /**
      * Sets the from date.
-     *
+     * 
      * @param fromDate the new from date
      */
-    public void setFromDate(final Date fromDate) {
+    public void setFromDate(Date fromDate) {
         this.fromDate = fromDate;
     }
 
     /**
      * Gets the to date.
-     *
+     * 
      * @return the to date
      */
     public Date getToDate() {
@@ -281,16 +228,16 @@ public class MemberMinistryAssociation implements Serializable {
 
     /**
      * Sets the to date.
-     *
+     * 
      * @param toDate the new to date
      */
-    public void setToDate(final Date toDate) {
+    public void setToDate(Date toDate) {
         this.toDate = toDate;
     }
 
     /**
      * Gets the role.
-     *
+     * 
      * @return the role
      */
     public MemberRole getRole() {
@@ -299,16 +246,16 @@ public class MemberMinistryAssociation implements Serializable {
 
     /**
      * Sets the role.
-     *
+     * 
      * @param role the new role
      */
-    public void setRole(final MemberRole role) {
+    public void setRole(MemberRole role) {
         this.role = role;
     }
 
     /**
      * Gets the record index.
-     *
+     * 
      * @return the record index
      */
     public Integer getRecordIndex() {
@@ -317,16 +264,16 @@ public class MemberMinistryAssociation implements Serializable {
 
     /**
      * Sets the record index.
-     *
+     * 
      * @param recordIndex the new record index
      */
-    public void setRecordIndex(final Integer recordIndex) {
+    public void setRecordIndex(Integer recordIndex) {
         this.recordIndex = recordIndex;
     }
 
     /**
      * Gets the version.
-     *
+     * 
      * @return the version
      */
     public Long getVersion() {
@@ -335,10 +282,10 @@ public class MemberMinistryAssociation implements Serializable {
 
     /**
      * Sets the version.
-     *
+     * 
      * @param version the new version
      */
-    public void setVersion(final Long version) {
+    public void setVersion(Long version) {
         this.version = version;
     }
 
