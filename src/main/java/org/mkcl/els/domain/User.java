cc54a0bb48ad1c0668d0e4262b53257abdc65512
sandeeps <sandeeps@348444cb-318d-0e48-9875-12270ded7a77>
2012-04-06 07:46:36 +0000

    added changes a/c to discussion with vishal sir

--- src/main/java/org/mkcl/els/domain/User.java
+++ src/main/java/org/mkcl/els/domain/User.java
@@ -1,110 +1,102 @@
-//package org.mkcl.els.domain;
-//
-//import java.io.Serializable;
-//import java.util.Date;
-//import java.util.Set;
-//
-//import javax.persistence.Column;
-//import javax.persistence.Entity;
-//import javax.persistence.FetchType;
-//import javax.persistence.JoinColumn;
-//import javax.persistence.JoinTable;
-//import javax.persistence.ManyToMany;
-//import javax.persistence.Temporal;
-//import javax.persistence.TemporalType;
-//
-//import org.springframework.beans.factory.annotation.Configurable;
-//import org.springframework.transaction.annotation.Transactional;
-//
-//@Configurable
-//@Entity
-//public class User extends Person implements Serializable {
-//
-//    // ---------------------------------Attributes------------------------------------------
-//
-//    private transient static final long serialVersionUID = 1L;
-//
-//    @Column(length = 50)
-//    private String username;
-//
-//    @Column(length = 20)
-//    private String password;
-//
-//    private boolean enabled = true;
-//
-//    @Temporal(TemporalType.DATE)
-//    private Date lastLoginTime;
-//
-//    @ManyToMany(fetch = FetchType.LAZY)
-//    @JoinTable(name = "role_membership", joinColumns = @JoinColumn(
-//            name = "user_id", referencedColumnName = "id"),
-//            inverseJoinColumns = @JoinColumn(name = "role_id",
-//                    referencedColumnName = "id"))
-//    private Set<Role> roles;
-//
-//    // ---------------------------------Constructors----------------------------------------------
-//
-//    public User() {
-//        super();
-//    }
-//
-//    public User(String username, String password, boolean enabled) {
-//        super();
-//        this.username = username;
-//        this.password = password;
-//        this.enabled = enabled;
-//    }
-//
-//    // Domain Methods
-//    // --------------------------------------------------------------------------
-//
-//    @Transactional
-//    public void changePassword(final String username, final String newpassword) {
-//        final User user = User.findByFieldName(User.class, "username",
-//                username, this.getLocale());
-//        user.setPassword(newpassword);
-//        user.merge();
-//    }
-//
-//    // ------------------------------------------Getters/Setters-----------------------------------
-//
-//    public String getUsername() {
-//        return username;
-//    }
-//
-//    public void setUsername(String username) {
-//        this.username = username;
-//    }
-//
-//    public String getPassword() {
-//        return password;
-//    }
-//
-//    public void setPassword(String password) {
-//        this.password = password;
-//    }
-//
-//    public boolean isEnabled() {
-//        return enabled;
-//    }
-//
-//    public void setEnabled(boolean enabled) {
-//        this.enabled = enabled;
-//    }
-//
-//    public Date getLastLoginTime() {
-//        return lastLoginTime;
-//    }
-//
-//    public void setLastLoginTime(Date lastLoginTime) {
-//        this.lastLoginTime = lastLoginTime;
-//    }
-//
-//    public Set<Role> getRoles() {
-//        return roles;
-//    }
-//
-//    public void setRoles(Set<Role> roles) {
-//        this.roles = roles;
-//    }
-//}
+package org.mkcl.els.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import org.mkcl.els.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Configurable;
+
+@Configurable
+@Entity
+@Table(name="user")
+public class User extends BaseDomain implements Serializable {
+
+    // ---------------------------------Attributes------------------------------------------
+    private transient static final long serialVersionUID = 1L;
+
+    @Column(length=300)
+    private String title;
+
+    @Column(length=300)
+    private String firstName;
+
+    @Column(length=300)
+    private String middleName;
+
+    @Column(length=300)
+    private String lastName;
+
+    @ManyToOne(fetch=FetchType.LAZY)
+    @JoinColumn(name="credential_id")
+    private Credential credential;
+
+    @Autowired
+    private transient UserRepository userRepository;
+
+    // ---------------------------------Constructors----------------------------------------------
+
+    public User() {
+        super();
+    }
+    // ----------------Domain_Methods------------------------------------------
+    public static UserRepository getUserRepository() {
+        UserRepository userRepository = new User().userRepository;
+        if (userRepository == null) {
+            throw new IllegalStateException(
+                    "UserRepository has not been injected in User Domain");
+        }
+        return userRepository;
+    }
+
+    public static User findByUserName(final String username,final String locale){
+    	return getUserRepository().findByUserName(username, locale);
+    }
+    // ------------------------------------------Getters/Setters-----------------------------------
+
+    public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(final String title) {
+		this.title = title;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(final String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getMiddleName() {
+		return middleName;
+	}
+
+	public void setMiddleName(final String middleName) {
+		this.middleName = middleName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(final String lastName) {
+		this.lastName = lastName;
+	}
+
+	public Credential getCredential() {
+		return credential;
+	}
+
+	public void setCredential(final Credential credential) {
+		this.credential = credential;
+	}
+}
