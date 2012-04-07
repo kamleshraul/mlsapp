e91c5d1dcad5a3eb497b3d3fc14c75d6c17685ce
vishals <vishals@348444cb-318d-0e48-9875-12270ded7a77>
2012-04-07 11:22:47 +0000

    code formatted

--- src/main/java/org/mkcl/els/controller/AirportController.java
+++ src/main/java/org/mkcl/els/controller/AirportController.java
@@ -11,7 +11,6 @@ package org.mkcl.els.controller;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Locale;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -23,10 +22,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-// TODO: Auto-generated Javadoc
 /**
  * The Class AirportController.
- * 
+ *
  * @author Dhananjay
  * @since v1.0.0
  */
@@ -34,90 +32,75 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/airport")
 public class AirportController extends GenericController<Airport> {
 
-	/**
-	 * Populate model.
-	 * 
-	 * @param model
-	 *            the model
-	 * @param domain
-	 *            the domain
-	 * @param locale
-	 *            the locale
-	 * @param request
-	 *            the request
-	 * @author nileshp
-	 * @since v1.0.0 Populate new.
-	 */
-	@Override
-	protected void populateNew(final ModelMap model, final Airport domain,
-			final String locale, final HttpServletRequest request) {
-		String stateName = ((CustomParameter) CustomParameter.findByName(
-				CustomParameter.class, "DEFAULT_STATE", locale.toString()))
-				.getValue();
-		populate(model, domain, locale, request, stateName);
-		model.addAttribute("airport", domain);
-	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * org.mkcl.els.controller.GenericController#populateEdit(org.springframework
-	 * .ui.ModelMap, org.mkcl.els.domain.BaseDomain,
-	 * javax.servlet.http.HttpServletRequest)
-	 */
-	@Override
-	protected void populateEdit(final ModelMap model, final Airport domain,
-			final HttpServletRequest request) {
-		District district = domain.getDistrict();
-		String stateName;
-		if (district != null) {
-			stateName = district.getDivision().getState().getName();
-		} else {
-			stateName = ((CustomParameter) CustomParameter.findByName(
-					CustomParameter.class, "DEFAULT_STATE", domain.getLocale()
-							)).getValue();
-		}
-		populate(model, domain, domain.getLocale(), request,
-				stateName);
-		model.addAttribute("airport", domain);
-	}
+    /*
+     * (non-Javadoc)
+     * @see org.mkcl.els.controller.GenericController#populateNew(org.springframework.ui.ModelMap,
+     * org.mkcl.els.domain.BaseDomain, java.lang.String, javax.servlet.http.HttpServletRequest)
+     */
+    @Override
+    protected void populateNew(final ModelMap model, final Airport domain,
+            final String locale, final HttpServletRequest request) {
+        String stateName = ((CustomParameter) CustomParameter.findByName(
+                CustomParameter.class, "DEFAULT_STATE", locale.toString()))
+                .getValue();
+        populate(model, domain, locale, request, stateName);
+        model.addAttribute("airport", domain);
+    }
 
-	/**
-	 * Populate.
-	 * 
-	 * @param model
-	 *            the model
-	 * @param domain
-	 *            the domain
-	 * @param locale
-	 *            the locale
-	 * @param request
-	 *            the request
-	 * @param stateName
-	 *            the state name
-	 * @author nileshp
-	 * @since v1.0.0 Populate.
-	 */
-	private void populate(final ModelMap model, final Airport domain,
-			final String locale, final HttpServletRequest request,
-			final String stateName) {
-		domain.setLocale(locale);
-		List<State> states = State.findAll(State.class, "name", "asc",
-				locale);
-		State selectedState = State.findByName(State.class, stateName,
-				locale);
-		List<State> newStates = new ArrayList<State>();
-		newStates.add(selectedState);
-		states.remove(selectedState);
-		newStates.addAll(states);
-		model.addAttribute("states", newStates);
-		List<District> districts = District.getDistrictRepository()
-				.findDistrictsByStateId(selectedState.getId(), "name", "asc",
-						locale);
-		if (districts != null) {
-			model.addAttribute("districts", districts);
-		}
-	}
+    /*
+     * (non-Javadoc)
+     *
+     * @see
+     * org.mkcl.els.controller.GenericController#populateEdit(org.springframework
+     * .ui.ModelMap, org.mkcl.els.domain.BaseDomain,
+     * javax.servlet.http.HttpServletRequest)
+     */
+    @Override
+    protected void populateEdit(final ModelMap model, final Airport domain,
+            final HttpServletRequest request) {
+        District district = domain.getDistrict();
+        String stateName;
+        if (district != null) {
+            stateName = district.getDivision().getState().getName();
+        } else {
+            stateName = ((CustomParameter) CustomParameter.findByName(
+                    CustomParameter.class, "DEFAULT_STATE", domain.getLocale()
+                    )).getValue();
+        }
+        populate(model, domain, domain.getLocale(), request,
+                stateName);
+        model.addAttribute("airport", domain);
+    }
 
+
+    /**
+     * Populate.
+     *
+     * @param model the model
+     * @param domain the domain
+     * @param locale the locale
+     * @param request the request
+     * @param stateName the state name
+     */
+    private void populate(final ModelMap model, final Airport domain,
+            final String locale, final HttpServletRequest request,
+            final String stateName) {
+        domain.setLocale(locale);
+        List<State> states = State.findAll(State.class, "name", "asc",
+                locale);
+        State selectedState = State.findByName(State.class, stateName,
+                locale);
+        List<State> newStates = new ArrayList<State>();
+        newStates.add(selectedState);
+        states.remove(selectedState);
+        newStates.addAll(states);
+        model.addAttribute("states", newStates);
+        List<District> districts = District.getDistrictRepository()
+                .findDistrictsByStateId(selectedState.getId(), "name", "asc",
+                        locale);
+        if (districts != null) {
+            model.addAttribute("districts", districts);
+        }
+    }
 }
