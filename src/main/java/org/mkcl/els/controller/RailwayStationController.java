d43efe2567c9ea7a8f545f3c4fd116bf55a34caf
vishals <vishals@348444cb-318d-0e48-9875-12270ded7a77>
2012-04-09 07:04:21 +0000

    removed populate method and refactored code

--- src/main/java/org/mkcl/els/controller/RailwayStationController.java
+++ src/main/java/org/mkcl/els/controller/RailwayStationController.java
@@ -1,81 +1,3 @@
-///**
-// * See the file LICENSE for redistribution information.
-// *
-// * Copyright (c) 2012 MKCL.  All rights reserved.
-// *
-// * Project: e-Legislature
-// * File: org.mkcl.els.controller.RailwayStationController.java
-// * Created On: Mar 8, 2012
-// */
-//package org.mkcl.els.controller;
-//
-//import java.util.List;
-//import java.util.Locale;
-//
-//import javax.servlet.http.HttpServletRequest;
-//
-//import org.mkcl.els.domain.CustomParameter;
-//import org.mkcl.els.domain.District;
-//import org.mkcl.els.domain.RailwayStation;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.ui.ModelMap;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//
-//// TODO: Auto-generated Javadoc
-///**
-// * The Class RailwayStationController.
-// * 
-// * @author nileshp
-// * @since v1.0.0
-// */
-//@Controller
-//@RequestMapping("/masters_railwaystations")
-//public class RailwayStationController extends GenericController<RailwayStation> {
-//
-//	/** The Constant ASC. */
-//	private static final String ASC = "asc";
-//
-//	/*
-//	 * (non-Javadoc)
-//	 * 
-//	 * @see
-//	 * org.mkcl.els.controller.GenericController#populateNew(org.springframework
-//	 * .ui.ModelMap, org.mkcl.els.domain.BaseDomain, java.util.Locale,
-//	 * javax.servlet.http.HttpServletRequest)
-//	 */
-//	@Override
-//	protected void populateNew(final ModelMap model,
-//			final RailwayStation domain, final Locale locale,
-//			final HttpServletRequest request) {
-//		domain.setLocale(locale.toString());
-//		List<District> districts = District.findAll(District.class, "name",
-//				ASC, locale.toString());
-//		model.addAttribute("districts", districts);
-//		String districtName = ((CustomParameter) CustomParameter.findByName(
-//				CustomParameter.class, "DEFAULT_STATE", locale.toString()))
-//				.getValue();
-//		District defaultDistrict = District.findByName(District.class,
-//				districtName, locale.toString());
-//		domain.setDistrict(defaultDistrict);
-//	}
-//
-//	/*
-//	 * (non-Javadoc)
-//	 * 
-//	 * @see
-//	 * org.mkcl.els.controller.GenericController#populateEdit(org.springframework
-//	 * .ui.ModelMap, org.mkcl.els.domain.BaseDomain,
-//	 * javax.servlet.http.HttpServletRequest)
-//	 */
-//	@Override
-//	protected void populateEdit(final ModelMap model,
-//			final RailwayStation domain, final HttpServletRequest request) {
-//		List<District> districts = District.findAll(District.class, "name",
-//				ASC, domain.getLocale());
-//		model.addAttribute("districts", districts);
-//	}
-//}
-
 /**
  * See the file LICENSE for redistribution information.
  *
@@ -87,12 +9,11 @@
  */
 package org.mkcl.els.controller;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Locale;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.mkcl.els.common.util.ApplicationConstants;
 import org.mkcl.els.domain.CustomParameter;
 import org.mkcl.els.domain.District;
 import org.mkcl.els.domain.RailwayStation;
@@ -101,10 +22,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-// TODO: Auto-generated Javadoc
 /**
  * The Class RailwayStationController.
- * 
+ *
  * @author Dhananjay
  * @since v1.0.0
  */
@@ -112,91 +32,53 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/railwaystation")
 public class RailwayStationController extends GenericController<RailwayStation> {
 
-	/**
-	 * Populate model.
-	 * 
-	 * @param model
-	 *            the model
-	 * @param domain
-	 *            the domain
-	 * @param locale
-	 *            the locale
-	 * @param request
-	 *            the request
-	 * @author nileshp
-	 * @since v1.0.0 Populate new.
-	 */
-	@Override
-	protected void populateNew(final ModelMap model,
-			final RailwayStation domain, final String locale,
-			final HttpServletRequest request) {
-		String stateName = ((CustomParameter) CustomParameter.findByName(
-				CustomParameter.class, "DEFAULT_STATE", locale.toString()))
-				.getValue();
-		populate(model, domain, locale, request, stateName);
-		model.addAttribute("railwayStation", domain);
-	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * org.mkcl.els.controller.GenericController#populateEdit(org.springframework
-	 * .ui.ModelMap, org.mkcl.els.domain.BaseDomain,
-	 * javax.servlet.http.HttpServletRequest)
-	 */
-	@Override
-	protected void populateEdit(final ModelMap model,
-			final RailwayStation domain, final HttpServletRequest request) {
-		District district = domain.getDistrict();
-		String stateName;
-		if (district != null) {
-			stateName = district.getDivision().getState().getName();
-		} else {
-			stateName = ((CustomParameter) CustomParameter.findByName(
-					CustomParameter.class, "DEFAULT_STATE", domain.getLocale()
-							.toString())).getValue();
-		}
-		populate(model, domain, domain.getLocale(), request,
-				stateName);
-		model.addAttribute("railwayStation", domain);
-	}
+    /*
+     * (non-Javadoc)
+     * @see org.mkcl.els.controller.GenericController#populateNew(org.springframework.ui.ModelMap,
+     * org.mkcl.els.domain.BaseDomain, java.lang.String, javax.servlet.http.HttpServletRequest)
+     */
+    @Override
+    protected void populateNew(final ModelMap model,
+            final RailwayStation domain, final String locale,
+            final HttpServletRequest request) {
+        String stateName = ((CustomParameter) CustomParameter.findByName(
+                CustomParameter.class, "DEFAULT_STATE", locale.toString()))
+                .getValue();
+        State defState = State.findByName(State.class, stateName, locale);
+        List<State> states = State.findAll(State.class, "name", ApplicationConstants.ASC, locale);
+        List<District> districts = District.findDistrictsByStateId(defState.getId(), "name",
+                ApplicationConstants.ASC, locale);
+        //following two lines added to make maharashtra default state
+        states.remove(defState);
+        states.add(0, defState);
+        domain.setDistrict(districts.get(0));
+        domain.setLocale(locale);
+        model.addAttribute("states", states);
+        model.addAttribute("districts", districts);
+    }
 
-	/**
-	 * Populate.
-	 * 
-	 * @param model
-	 *            the model
-	 * @param domain
-	 *            the domain
-	 * @param locale
-	 *            the locale
-	 * @param request
-	 *            the request
-	 * @param stateName
-	 *            the state name
-	 * @author nileshp
-	 * @since v1.0.0 Populate.
-	 */
-	private void populate(final ModelMap model, final RailwayStation domain,
-			final String locale, final HttpServletRequest request,
-			final String stateName) {
-		domain.setLocale(locale.toString());
-		List<State> states = State.findAll(State.class, "name", "asc",
-				locale.toString());
-		State selectedState = State.findByName(State.class, stateName,
-				locale.toString());
-		List<State> newStates = new ArrayList<State>();
-		newStates.add(selectedState);
-		states.remove(selectedState);
-		newStates.addAll(states);
-		model.addAttribute("states", newStates);
-		List<District> districts = District.getDistrictRepository()
-				.findDistrictsByStateId(selectedState.getId(), "name", "asc",
-						locale.toString());
-		if (districts != null) {
-			model.addAttribute("districts", districts);
-		}
-	}
+    /*
+     * (non-Javadoc)
+     *
+     * @see
+     * org.mkcl.els.controller.GenericController#populateEdit(org.springframework
+     * .ui.ModelMap, org.mkcl.els.domain.BaseDomain,
+     * javax.servlet.http.HttpServletRequest)
+     */
+    @Override
+    protected void populateEdit(final ModelMap model,
+            final RailwayStation domain, final HttpServletRequest request) {
+        State state = domain.getDistrict().getDivision().getState();
+        List<State> states = State.findAll(State.class, "name", ApplicationConstants.ASC,
+                domain.getLocale());
+        List<District> districts = District.findDistrictsByStateId(state.getId(), "name",
+                ApplicationConstants.ASC, domain.getLocale());
+        //following two lines added to make maharashtra default state
+        states.remove(state);
+        states.add(0, state);
+        model.addAttribute("states", states);
+        model.addAttribute("districts", districts);
+    }
 
 }
