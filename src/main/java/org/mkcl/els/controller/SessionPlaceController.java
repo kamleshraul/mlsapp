1c26c45525883686b052a6ab8e86b047c9e30129
vishals <vishals@348444cb-318d-0e48-9875-12270ded7a77>
2012-04-09 10:34:16 +0000

    code formatted

--- src/main/java/org/mkcl/els/controller/SessionPlaceController.java
+++ src/main/java/org/mkcl/els/controller/SessionPlaceController.java
@@ -19,7 +19,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-// TODO: Auto-generated Javadoc
 /**
  * The Class SessionPlaceController.
  *
@@ -29,46 +28,54 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/sessionplace")
 public class SessionPlaceController extends GenericController<SessionPlace> {
-	
-	/* (non-Javadoc)
-	 * @see org.mkcl.els.controller.GenericController#customValidateCreate(org.mkcl.els.domain.BaseDomain, org.springframework.validation.BindingResult, javax.servlet.http.HttpServletRequest)
-	 */
-	protected void customValidateCreate(final SessionPlace domain,
-			final BindingResult result, final HttpServletRequest request) {
-		customValidate(domain, result, request);
-	}
-	
-	/* (non-Javadoc)
-	 * @see org.mkcl.els.controller.GenericController#customValidateUpdate(org.mkcl.els.domain.BaseDomain, org.springframework.validation.BindingResult, javax.servlet.http.HttpServletRequest)
-	 */
-	protected void customValidateUpdate(final SessionPlace domain,
-			final BindingResult result, final HttpServletRequest request) {
-		customValidate(domain, result, request);
-	}
-	
-	/**
-	 * Custom validate.
-	 *
-	 * @param domain the domain
-	 * @param result the result
-	 * @param request the request
-	 */
-	private void customValidate(final SessionPlace domain,
-			final BindingResult result, final HttpServletRequest request) {
-		Map<String, String> names = new HashMap<String, String>();
-		names.put("place", domain.getPlace());
 
-		// Check for duplicate instances
-		Boolean duplicateParameter = domain.isDuplicate(names);
-		Object[] params = new Object[1];
-		params[0] = domain.getLocale().toString();
-		if (duplicateParameter) {
-			result.rejectValue("place", "NonUnique", params,
-					"Duplicate Parameter");
-		}
-		// Check for version mismatch
-		if (domain.isVersionMismatch()) {
-			result.rejectValue("VersionMismatch", "version");
-		}
-	}
+    /*
+     * (non-Javadoc)
+     * @see
+     * org.mkcl.els.controller.GenericController#customValidateCreate(org.mkcl.els.domain.BaseDomain
+     * , org.springframework.validation.BindingResult, javax.servlet.http.HttpServletRequest)
+     */
+    @Override
+    protected void customValidateCreate(final SessionPlace domain,
+            final BindingResult result, final HttpServletRequest request) {
+        customValidate(domain, result, request);
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see
+     * org.mkcl.els.controller.GenericController#customValidateUpdate(org.mkcl.els.domain.BaseDomain
+     * , org.springframework.validation.BindingResult, javax.servlet.http.HttpServletRequest)
+     */
+    @Override
+    protected void customValidateUpdate(final SessionPlace domain,
+            final BindingResult result, final HttpServletRequest request) {
+        customValidate(domain, result, request);
+    }
+
+    /**
+     * Custom validate.
+     *
+     * @param domain the domain
+     * @param result the result
+     * @param request the request
+     */
+    private void customValidate(final SessionPlace domain,
+            final BindingResult result, final HttpServletRequest request) {
+        Map<String, String> names = new HashMap<String, String>();
+        names.put("place", domain.getPlace());
+
+        // Check for duplicate instances
+        Boolean duplicateParameter = domain.isDuplicate(names);
+        Object[] params = new Object[1];
+        params[0] = domain.getLocale().toString();
+        if (duplicateParameter) {
+            result.rejectValue("place", "NonUnique", params,
+                    "Duplicate Parameter");
+        }
+        // Check for version mismatch
+        if (domain.isVersionMismatch()) {
+            result.rejectValue("VersionMismatch", "version");
+        }
+    }
 }
