bd002c568fcb690b4fa538db5199ce223e1c8909
anandk <anandk@348444cb-318d-0e48-9875-12270ded7a77>
2012-04-09 10:13:22 +0000

    As per new changes the Controller is changed

--- src/main/java/org/mkcl/els/controller/MinistryController.java
+++ src/main/java/org/mkcl/els/controller/MinistryController.java
@@ -30,46 +30,4 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/ministry")
 public class MinistryController extends GenericController<Ministry>{
 	
-	/* (non-Javadoc)
-	 * @see org.mkcl.els.controller.GenericController#customValidateCreate(org.mkcl.els.domain.BaseDomain, org.springframework.validation.BindingResult, javax.servlet.http.HttpServletRequest)
-	 */
-	protected void customValidateCreate(final Ministry domain,
-			final BindingResult result, final HttpServletRequest request) {
-		customValidate(domain, result, request);
-	}
-	
-	/* (non-Javadoc)
-	 * @see org.mkcl.els.controller.GenericController#customValidateUpdate(org.mkcl.els.domain.BaseDomain, org.springframework.validation.BindingResult, javax.servlet.http.HttpServletRequest)
-	 */
-	protected void customValidateUpdate(final Ministry domain,
-			final BindingResult result, final HttpServletRequest request) {
-		customValidate(domain, result, request);
-	}
-	
-	/**
-	 * Custom validate.
-	 *
-	 * @param domain the domain
-	 * @param result the result
-	 * @param request the request
-	 */
-	private void customValidate(final Ministry domain,
-			final BindingResult result, final HttpServletRequest request) {
-		Map<String, String> names = new HashMap<String, String>();
-		names.put("department", domain.getDepartment());
-		names.put("alias", domain.getAlias());
-
-		// Check for duplicate instances
-		Boolean duplicateParameter = domain.isDuplicate(names);
-		Object[] params = new Object[1];
-		params[0] = domain.getLocale().toString();
-		if (duplicateParameter) {
-			result.rejectValue("department", "NonUnique", params,
-					"Duplicate Parameter");
-		}
-		// Check for version mismatch
-		if (domain.isVersionMismatch()) {
-			result.rejectValue("VersionMismatch", "version");
-		}
-	}
 }
