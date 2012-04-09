ff03830336cc895463cae271ae453cf411f10ea7
vishals <vishals@348444cb-318d-0e48-9875-12270ded7a77>
2012-04-09 05:34:37 +0000

    removed populate method and refactored code

--- src/main/java/org/mkcl/els/controller/ElectionTypeController.java
+++ src/main/java/org/mkcl/els/controller/ElectionTypeController.java
@@ -9,23 +9,17 @@
  */
 package org.mkcl.els.controller;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Locale;
-import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.mkcl.els.domain.HouseType;
 import org.mkcl.els.domain.CustomParameter;
 import org.mkcl.els.domain.ElectionType;
+import org.mkcl.els.domain.HouseType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-// TODO: Auto-generated Javadoc
 /**
  * The Class ElectionTypeController.
  *
@@ -35,66 +29,39 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/electiontype")
 public class ElectionTypeController extends GenericController<ElectionType> {
-	/** The Constant ASC. */
-    private static final String ASC = "asc";
-    
-    /* (non-Javadoc)
-     * @see org.mkcl.els.controller.GenericController#customValidateCreate(org.mkcl.els.domain.BaseDomain, org.springframework.validation.BindingResult, javax.servlet.http.HttpServletRequest)
-     */
-    /* (non-Javadoc)
-     * @see org.mkcl.els.controller.GenericController#populateNew(org.springframework.ui.ModelMap, org.mkcl.els.domain.BaseDomain, java.util.Locale, javax.servlet.http.HttpServletRequest)
+
+    /*
+     * (non-Javadoc)
+     * @see org.mkcl.els.controller.GenericController#populateNew(org.springframework.ui.ModelMap,
+     * org.mkcl.els.domain.BaseDomain, java.lang.String, javax.servlet.http.HttpServletRequest)
      */
     @Override
     protected void populateNew(final ModelMap model,
-                               final ElectionType domain,
-                               final String locale,
-                               final HttpServletRequest request) {
-        
-    	 String assemblycounciltype = ((CustomParameter) CustomParameter.findByName(
-                 CustomParameter.class, "DEFAULT_HOUSETYPE", locale)).getValue();
-         populate(model, domain, locale, request, assemblycounciltype);
+            final ElectionType domain,
+            final String locale,
+            final HttpServletRequest request) {
+        String strHouseTypeName = ((CustomParameter) CustomParameter.findByName(
+                CustomParameter.class, "DEFAULT_HOUSETYPE", locale)).getValue();
+        HouseType defaultHouseType = HouseType.findByFieldName(HouseType.class, "type",
+                strHouseTypeName, locale);
+        domain.setHouseType(defaultHouseType);
+        List<HouseType> houseTypes = HouseType.findAll(
+                HouseType.class, "type", "asc", locale);
+        model.addAttribute("assemblycounciltype", houseTypes);
     }
 
-   /**
-    * Populate.
-    *
-    * @param model the model
-    * @param domain the domain
-    * @param locale the locale
-    * @param request the request
-    * @param assemblycounciltype the assemblycounciltype
-    */
-   protected void populate(final ModelMap model, final ElectionType domain, final String locale,
-			final HttpServletRequest request, final String assemblycounciltype) {
-		// TODO Auto-generated method stub
-	   domain.setLocale(locale);
-       List<HouseType> assemblycounciltypelist = HouseType.findAll(
-    		   HouseType.class, "type", "asc", locale);
-       HouseType selectedAssemblycounciltype = HouseType.findByFieldName(
-    		   HouseType.class, "type", assemblycounciltype, locale);
-       List<HouseType> newassemblycounciltype = new ArrayList<HouseType>();
-       newassemblycounciltype.add(selectedAssemblycounciltype);
-       assemblycounciltypelist.remove(selectedAssemblycounciltype);
-       newassemblycounciltype.addAll(assemblycounciltypelist);
-       model.addAttribute("assemblycounciltype", newassemblycounciltype);
-       domain.setHouseType(selectedAssemblycounciltype);
-     }
 
-/* (non-Javadoc)
- * @see org.mkcl.els.controller.GenericController#populateEdit(org.springframework.ui.ModelMap, org.mkcl.els.domain.BaseDomain, javax.servlet.http.HttpServletRequest)
- */
-   @Override
-protected void populateEdit(final ModelMap model,
-                                final ElectionType domain,
-                                final HttpServletRequest request) {
-        /*List<AssemblyCouncilType> assemblycounciltype = AssemblyCouncilType.findAll(
-        		AssemblyCouncilType.class, "name", ASC, domain.getLocale());
-        model.addAttribute("assemblycounciltype", assemblycounciltype);*/
-	domain.setLocale(domain.getLocale());
-	String assemblycounciltype = domain.getHouseType().getType();
-    populate(
-            model, domain,domain.getLocale(), request,
-            assemblycounciltype);
-    model.addAttribute("electionType", domain);
+    /*
+     * (non-Javadoc)
+     * @see org.mkcl.els.controller.GenericController#populateEdit(org.springframework.ui.ModelMap,
+     * org.mkcl.els.domain.BaseDomain, javax.servlet.http.HttpServletRequest)
+     */
+    @Override
+    protected void populateEdit(final ModelMap model,
+            final ElectionType domain,
+            final HttpServletRequest request) {
+        List<HouseType> houseTypes = HouseType.findAll(
+                HouseType.class, "type", "asc", domain.getLocale());
+        model.addAttribute("assemblycounciltype", houseTypes);
     }
 }
