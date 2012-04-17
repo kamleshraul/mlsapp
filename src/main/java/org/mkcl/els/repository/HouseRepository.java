1537c7bd13f2936c81bdbcb69e31e771db88a99e
sandeeps <sandeeps@348444cb-318d-0e48-9875-12270ded7a77>
2012-04-17 11:35:28 +0000

    changes till april 13 meeting

--- src/main/java/org/mkcl/els/repository/HouseRepository.java
+++ src/main/java/org/mkcl/els/repository/HouseRepository.java
@@ -1,16 +1,18 @@
 /**
  * See the file LICENSE for redistribution information.
  *
- * Copyright (c) 2011 MKCL.  All rights reserved.
+ * Copyright (c) 2012 MKCL.  All rights reserved.
  *
- * Project: els
- * File: org.mkcl.els.repository.HouseRepository
- * Created On: Apr 5, 2012
+ * Project: e-Legislature
+ * File: org.mkcl.els.repository.HouseRepository.java
+ * Created On: Apr 17, 2012
  */
 package org.mkcl.els.repository;
 
 import java.util.Date;
+import java.util.List;
 
+import org.mkcl.els.common.util.ApplicationConstants;
 import org.mkcl.els.domain.House;
 import org.springframework.stereotype.Repository;
 
@@ -19,43 +21,59 @@ import com.trg.search.Search;
 /**
  * The Class HouseRepository.
  *
- * @author vishals
- * @version 1.0.0
+ * @author amitd
+ * @author sandeeps
+ * @since v1.0.0
  */
 @Repository
-public class HouseRepository extends BaseRepository<House, Long> {
+public class HouseRepository extends BaseRepository<House,Long>{
 
-    /**
-     * Find current house.
-     *
-     * @param locale the locale
-     * @return the house
-     */
-    public House findCurrentHouse(final String locale) {
-        Search search = new Search();
-        search.addFilterEqual("locale", locale);
-        Date currentDate = new Date();
-        search.addFilterGreaterOrEqual("lastDate", currentDate);
-        search.addFilterLessOrEqual("firstDate", currentDate);
-        return this.searchUnique(search);
-    }
+	/**
+	 * Find current house.
+	 *
+	 * @param locale the locale
+	 * @return the house
+	 */
+	public House findCurrentHouse(final String locale){
+		Search search=new Search();
+		search.addFilterEqual("locale",locale);
+		Date currentDate=new Date();
+		search.addFilterGreaterOrEqual("lastDate", currentDate);
+		search.addFilterLessOrEqual("firstDate",currentDate);
+		return this.searchUnique(search);
+	}
 
-    /**
-     * Find house by to from date.
-     *
-     * @param fromDate the from date
-     * @param toDate the to date
-     * @param locale the locale
-     * @return the house
-     */
-    public House findHouseByToFromDate(
-            final Date fromDate,
-            final Date toDate,
-            final String locale) {
-        Search search = new Search();
-        search.addFilterEqual("locale", locale);
-        search.addFilterGreaterOrEqual("lastDate", toDate);
-        search.addFilterLessOrEqual("firstDate", fromDate);
-        return this.searchUnique(search);
-    }
+	/**
+	 * Find house by to from date.
+	 *
+	 * @param fromDate the from date
+	 * @param toDate the to date
+	 * @param locale the locale
+	 * @return the house
+	 */
+	public House findHouseByToFromDate(final Date fromDate,final Date toDate,final String locale){
+		Search search=new Search();
+		search.addFilterEqual("locale",locale);
+		search.addFilterGreaterOrEqual("lastDate", toDate);
+		search.addFilterLessOrEqual("firstDate",fromDate);
+		return this.searchUnique(search);
+	}
+
+	/**
+	 * Find by house type.
+	 *
+	 * @param houseType the house type
+	 * @param locale the locale
+	 * @return the list
+	 */
+	@SuppressWarnings("unchecked")
+	public List<House> findByHouseType(final String houseType, final String locale) {
+		String query=null;
+		if(houseType.equals(ApplicationConstants.BOTH_HOUSE)){
+			query="SELECT h FROM House h WHERE h.locale='"+locale+"' ORDER BY h.formationDate DESC";
+		}else{
+			query="SELECT h FROM House h WHERE h.locale='"+locale+"' AND h.type.type='"+houseType+"' ORDER BY h.formationDate DESC";
+		}
+		return this.em().createQuery(query).getResultList();
+	}
 }
