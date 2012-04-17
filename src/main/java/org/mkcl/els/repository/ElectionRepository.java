d5502fb4d7f67d058db43177b321dd5886356253
sandeeps <sandeeps@348444cb-318d-0e48-9875-12270ded7a77>
2012-04-17 11:35:12 +0000

    changes till april 13 meeting

--- src/main/java/org/mkcl/els/repository/ElectionRepository.java
+++ src/main/java/org/mkcl/els/repository/ElectionRepository.java
@@ -1,54 +1,52 @@
 /**
  * See the file LICENSE for redistribution information.
  *
- * Copyright (c) 2011 MKCL.  All rights reserved.
+ * Copyright (c) 2012 MKCL.  All rights reserved.
  *
- * Project: els
- * File: org.mkcl.els.repository.ElectionRepository
- * Created On: Apr 5, 2012
+ * Project: e-Legislature
+ * File: org.mkcl.els.repository.ElectionRepository.java
+ * Created On: Apr 17, 2012
  */
 package org.mkcl.els.repository;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import org.mkcl.els.common.vo.ElectionVO;
+import org.mkcl.els.common.util.ApplicationConstants;
 import org.mkcl.els.domain.Election;
 import org.springframework.stereotype.Repository;
 
+// TODO: Auto-generated Javadoc
 /**
  * The Class ElectionRepository.
  *
- * @author vishals
- * @version 1.0.0
+ * @author amitd
+ * @author sandeeps
+ * @since v1.0.0
  */
 @Repository
-public class ElectionRepository extends BaseRepository<Election, Long> {
+public class ElectionRepository extends BaseRepository<Election,Long> {
 
-    /**
-     * Find by house type.
-     *
-     * @param houseType the house type
-     * @param locale the locale
-     * @return the list
-     */
-    @SuppressWarnings("rawtypes")
-    public List<ElectionVO> findByHouseType(final String houseType, final String locale) {
+	/**
+	 * Find by house type.
+	 *
+	 * @param houseType the house type
+	 * @param locale the locale
+	 * @return the list
+	 */
+	@SuppressWarnings("unchecked")
+    public List<Election> findByHouseType(final String houseType,final String locale) {
+        /*
+         * SELECT * FROM masters_elections  AS e JOIN  masters_electiontypes AS et JOIN masters_housetype AS h WHERE e.electiontype_id=et.id
+            AND et.housetype_id=h.id AND h.type='lowerhouse' AND e.locale='en_US'
+         */
+        String query=null;
+        if(houseType.equals(ApplicationConstants.BOTH_HOUSE)){
+            query="SELECT e FROM Election e WHERE e.locale='"+locale+"'";
+        }else{
+            query="SELECT e FROM Election e  WHERE e.locale='"+locale+"' AND e.electionType.houseType.type='"+houseType+"'";
 
-        String query = "SELECT e.id,e.name FROM masters_elections AS e JOIN  "
-                + "masters_electiontypes AS et JOIN masters_housetype AS h "
-                + "WHERE e.electiontype_id=et.id AND et.housetype_id=h.id AND h.type='"
-                + houseType
-                + "' AND e.locale='" + locale + "'";
-        List elections = this.em().createNativeQuery(query).getResultList();
-        List<ElectionVO> electionVOs = new ArrayList<ElectionVO>();
-        for (Object i : elections) {
-            Object[] o = (Object[]) i;
-            ElectionVO electionVO = new ElectionVO(Long.parseLong(o[0].toString()),
-                    o[1].toString());
-            electionVOs.add(electionVO);
         }
-        return electionVOs;
+        return this.em().createQuery(query).getResultList();
     }
 
 }
