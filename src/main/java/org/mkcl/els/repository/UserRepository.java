ddada2e694f10bed71c576e47e12901b958f929f
sandeeps <sandeeps@348444cb-318d-0e48-9875-12270ded7a77>
2012-04-17 11:36:55 +0000

    changes till april 13 meeting

--- src/main/java/org/mkcl/els/repository/UserRepository.java
+++ src/main/java/org/mkcl/els/repository/UserRepository.java
@@ -1,3 +1,12 @@
+/**
+ * See the file LICENSE for redistribution information.
+ *
+ * Copyright (c) 2012 MKCL.  All rights reserved.
+ *
+ * Project: e-Legislature
+ * File: org.mkcl.els.repository.UserRepository.java
+ * Created On: Apr 17, 2012
+ */
 package org.mkcl.els.repository;
 
 import org.mkcl.els.domain.User;
@@ -5,11 +14,26 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 
+/**
+ * The Class UserRepository.
+ *
+ * @author amitd
+ * @author sandeeps
+ * @since v1.0.0
+ */
 @Repository
 public class UserRepository extends BaseRepository<User,Long>{
 
+	/** The logger. */
 	protected final Logger logger = LoggerFactory.getLogger(getClass());
 
+	/**
+	 * Find by user name.
+	 *
+	 * @param username the username
+	 * @param locale the locale
+	 * @return the user
+	 */
 	public User findByUserName(final String username,final String locale){
 		String query="SELECT u FROM User u JOIN FETCH u.credential c  where u.locale='"+locale+"' AND c.username='"+username+"'";
 		try {
