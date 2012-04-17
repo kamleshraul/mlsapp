e631c1898b6d6b70b902819f0dfa6dc581d5f264
sandeeps <sandeeps@348444cb-318d-0e48-9875-12270ded7a77>
2012-04-17 11:36:46 +0000

    changes till april 13 meeting

--- src/main/java/org/mkcl/els/repository/TehsilRepository.java
+++ src/main/java/org/mkcl/els/repository/TehsilRepository.java
@@ -1,11 +1,11 @@
 /**
  * See the file LICENSE for redistribution information.
  *
- * Copyright (c) 2011 MKCL.  All rights reserved.
+ * Copyright (c) 2012 MKCL.  All rights reserved.
  *
- * Project: els
- * File: org.mkcl.els.repository.TehsilRepository
- * Created On: Apr 5, 2012
+ * Project: e-Legislature
+ * File: org.mkcl.els.repository.TehsilRepository.java
+ * Created On: Apr 17, 2012
  */
 package org.mkcl.els.repository;
 
@@ -19,34 +19,33 @@ import org.springframework.stereotype.Repository;
 /**
  * The Class TehsilRepository.
  *
- * @author vishals
- * @version 1.0.0
+ * @author amitd
+ * @author sandeeps
+ * @since v1.0.0
  */
 @Repository
-public class TehsilRepository extends BaseRepository<Tehsil, Long> {
+public class TehsilRepository extends BaseRepository<Tehsil,Long> {
 
-    /**
-     * Find tehsils ref by district id.
-     *
-     * @param districtId the district id
-     * @param sortBy the sort by
-     * @param sortOrder the sort order
-     * @param locale the locale
-     * @return the list
-     */
-    @SuppressWarnings("rawtypes")
-    public List<Reference> findTehsilsRefByDistrictId(final Long districtId,
-            final String sortBy, final String sortOrder, final String locale) {
-        String query = "SELECT t.id,t.name FROM Tehsil t WHERE t.district.id=" + districtId
-                + " AND t.locale='" + locale + "' ORDER BY t." + sortBy + " " + sortOrder;
-        List tehsils = this.em().createQuery(query).getResultList();
-        List<Reference> tehsilsRef = new ArrayList<Reference>();
-        for (Object i : tehsils) {
-            Object[] o = (Object[]) i;
-            Reference reference = new Reference(o[0].toString(), o[1].toString());
-            tehsilsRef.add(reference);
-        }
-        return tehsilsRef;
-    }
+	/**
+	 * Find tehsils ref by district id.
+	 *
+	 * @param districtId the district id
+	 * @param sortBy the sort by
+	 * @param sortOrder the sort order
+	 * @param locale the locale
+	 * @return the list
+	 */
+	public List<Reference> findTehsilsRefByDistrictId(final Long districtId,
+			final String sortBy, final String sortOrder, final String locale) {
+		String query="SELECT t.id,t.name FROM Tehsil t WHERE t.district.id="+districtId+" AND t.locale='"+locale+"' ORDER BY t."+sortBy+" "+sortOrder;
+		List tehsils=this.em().createQuery(query).getResultList();
+		List<Reference> tehsilsRef=new ArrayList<Reference>();
+		for(Object i:tehsils){
+			Object[] o=(Object[]) i;
+			Reference reference=new Reference(o[0].toString(),o[1].toString());
+			tehsilsRef.add(reference);
+		}
+		return tehsilsRef;
+	}
 
 }
