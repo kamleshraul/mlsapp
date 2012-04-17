781e85e7749506d5e5078b7df8d760ef44819f35
sandeeps <sandeeps@348444cb-318d-0e48-9875-12270ded7a77>
2012-04-17 10:04:36 +0000

    removed unused services

--- src/main/java/org/mkcl/els/service/IAssemblyTermService.java
+++
@@ -1,9 +0,0 @@
-//package org.mkcl.els.service;
-//
-//import org.mkcl.els.domain.AssemblyTerm;
-//
-//public interface IAssemblyTermService extends IGenericService<AssemblyTerm ,Long> {
-//
-//	// public AssemblyTerm findByAssemblyTerm(Integer term);
-//
-//}
