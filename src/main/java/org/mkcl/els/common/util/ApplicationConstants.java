4a4c5ac02a6b187a037af3506835e11d510d055d
sandeeps <sandeeps@348444cb-318d-0e48-9875-12270ded7a77>
2012-05-04 05:21:39 +0000

    fixed checkstyle violations

--- src/main/java/org/mkcl/els/common/util/ApplicationConstants.java
+++ src/main/java/org/mkcl/els/common/util/ApplicationConstants.java
@@ -1,31 +1,60 @@
+/**
+ * See the file LICENSE for redistribution information.
+ *
+ * Copyright (c) 2012 ${company_name}.  All rights reserved.
+ *
+ * Project: e-Legislature
+ * File: org.mkcl.els.common.util.ApplicationConstants.java
+ * Created On: May 4, 2012
+ */
 package org.mkcl.els.common.util;
 
 
+/**
+ * The Class ApplicationConstants.
+ *
+ * @author amitd
+ * @author sandeeps
+ * @since v1.0.0
+ */
 public class ApplicationConstants {
 
+    /** The Constant ASC. */
     public static final String ASC = "asc";
 
+    /** The Constant DESC. */
     public static final String DESC = "desc";
 
+    /** The Constant ALL_LOCALE. */
     public static final String ALL_LOCALE = "all";
 
+    /** The Constant DEFAULT_LOCALE. */
     public static final String DEFAULT_LOCALE = "mr_IN";
 
+    /** The Constant LOWER_HOUSE. */
     public static final String LOWER_HOUSE="lowerhouse";
 
+    /** The Constant UPPER_HOUSE. */
     public static final String UPPER_HOUSE="upperhouse";
 
+    /** The Constant BOTH_HOUSE. */
     public static final String BOTH_HOUSE="both";
 
+    /** The Constant DEFAULT_HOUSE. */
     public static final String DEFAULT_HOUSE="defaulthouse";
 
+    /** The Constant en_US_INFONOTFOUND. */
     public static final String en_US_INFONOTFOUND="Information Not Available For";
 
+    /** The Constant mr_IN_INFONOTFOUND. */
     public static final String mr_IN_INFONOTFOUND="माहिती प्राप्‍त झालेली नाही";
 
+    /** The Constant mr_IN_INFOFOUND. */
     public static final String mr_IN_INFOFOUND="माहिती मिळालेली एकूण";
 
+    /** The Constant en_US_INFOFOUND. */
     public static final String en_US_INFOFOUND="Information Available For";
 
+    /** The Constant SERVER_DATEFORMAT. */
     public static final String SERVER_DATEFORMAT = "dd/MM/yyyy";
 }
