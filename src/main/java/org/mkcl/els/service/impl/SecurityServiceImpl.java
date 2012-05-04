37a9d9ba77be89b8a60c9f21389fec71a574c631
sandeeps <sandeeps@348444cb-318d-0e48-9875-12270ded7a77>
2012-05-04 06:02:38 +0000

    added javadoc comment and fixed checkstyle violations

--- src/main/java/org/mkcl/els/service/impl/SecurityServiceImpl.java
+++ src/main/java/org/mkcl/els/service/impl/SecurityServiceImpl.java
@@ -46,41 +46,45 @@ public class SecurityServiceImpl implements UserDetailsService {
     @SuppressWarnings("deprecation")
     @Transactional(readOnly = true)
     public UserDetails loadUserByUsername(final String username)
-            throws UsernameNotFoundException, DataAccessException {
-    	//Here way needs to be found to read the current locale so that user info can be found
-    	//locale based.Currently it is set to mr_IN.
-    	User user=User.findByUserName(username, "mr_IN");
-		String houseType=null;
-    	if(user.getId()==null){
-            	throw new UsernameNotFoundException("User Not Found");
-    	}else{
-    		Credential credential=user.getCredential();
-    		if(!credential.isEnabled()){
+    throws UsernameNotFoundException , DataAccessException {
+        //Here way needs to be found to read the current locale so that user info can be found
+        //locale based.Currently it is set to mr_IN.
+        User user = User.findByUserName(username, "mr_IN");
+        String houseType = null;
+        if (user.getId() == null) {
+            throw new UsernameNotFoundException("User Not Found");
+        } else {
+            Credential credential = user.getCredential();
+            if (!credential.isEnabled()) {
                 throw new UsernameNotFoundException("User Not Found");
-    		}else{
-    			Collection<GrantedAuthority> roles = new HashSet<GrantedAuthority>();
-    			if(credential.getRoles().isEmpty()){
+            } else {
+                Collection<GrantedAuthority> roles = new HashSet<GrantedAuthority>();
+                if (credential.getRoles().isEmpty()) {
                     throw new UsernameNotFoundException("User Not Found");
-    			}
-    	        for (Role role : credential.getRoles()) {
-    	            roles.add(new GrantedAuthorityImpl(role.getName()));
-    	            if(role.getName().equals("DATA_ENTRY_OPERATOR_LOWERHOUSE")||role.getName().equals("ADMIN_LOWERHOUSE")){
-    	            	houseType=ApplicationConstants.LOWER_HOUSE;
-    	            }else if(role.getName().equals("DATA_ENTRY_OPERATOR_UPPERHOUSE")||role.getName().equals("ADMIN_UPPERHOUSE")){
-    	            	houseType=ApplicationConstants.UPPER_HOUSE;
-    	            }else if(role.getName().equals("DATA_ENTRY_OPERATOR_BOTHHOUSE")||role.getName().equals("ADMIN_BOTHHOUSE")){
-    	            	houseType=ApplicationConstants.BOTH_HOUSE;
-    	            }else if(role.getName().equals("DATA_ENTRY_OPERATOR_DEFAULTHOUSE")||role.getName().equals("ADMIN_DEFAULTHOUSE")){
-                        houseType=ApplicationConstants.DEFAULT_HOUSE;
+                }
+                for (Role role : credential.getRoles()) {
+                    roles.add(new GrantedAuthorityImpl(role.getName()));
+                    if (role.getName().equals("DATA_ENTRY_OPERATOR_LOWERHOUSE")
+                            || role.getName().equals("ADMIN_LOWERHOUSE")) {
+                        houseType = ApplicationConstants.LOWER_HOUSE;
+                    } else if (role.getName().equals("DATA_ENTRY_OPERATOR_UPPERHOUSE")
+                            || role.getName().equals("ADMIN_UPPERHOUSE")) {
+                        houseType = ApplicationConstants.UPPER_HOUSE;
+                    } else if (role.getName().equals("DATA_ENTRY_OPERATOR_BOTHHOUSE")
+                            || role.getName().equals("ADMIN_BOTHHOUSE")) {
+                        houseType = ApplicationConstants.BOTH_HOUSE;
+                    } else if (role.getName().equals("DATA_ENTRY_OPERATOR_DEFAULTHOUSE")
+                            || role.getName().equals("ADMIN_DEFAULTHOUSE")) {
+                        houseType = ApplicationConstants.DEFAULT_HOUSE;
                     }
-    	        }
-    	        credential.setLastLoginTime(new Date());
-    	        credential.merge();
-    	        return new AuthUser(credential.getUsername(), credential.getPassword(),
-    	                credential.isEnabled(), true, true, true, roles, user.getTitle(),
-    	                user.getFirstName(),user.getMiddleName(), user.getLastName(),houseType,
-    	                        credential.getRoles());
-    		}
-    	}
-}
+                }
+                credential.setLastLoginTime(new Date());
+                credential.merge();
+                return new AuthUser(credential.getUsername(), credential.getPassword(),
+                        credential.isEnabled(), true, true, true, roles, user.getTitle(),
+                        user.getFirstName(), user.getMiddleName() , user.getLastName() , houseType,
+                        credential.getRoles());
+            }
+        }
+    }
 }
