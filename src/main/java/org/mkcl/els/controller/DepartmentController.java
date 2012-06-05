d94fbd206f1d670e513ab7bbfea740834d8b1b0c
anandk <anandk@348444cb-318d-0e48-9875-12270ded7a77>
2012-06-05 08:18:00 +0000

    The Department Controller is updated

--- src/main/java/org/mkcl/els/controller/DepartmentController.java
+++ src/main/java/org/mkcl/els/controller/DepartmentController.java
@@ -9,13 +9,8 @@
  */
 package org.mkcl.els.controller;
 
-import java.util.List;
-
-import javax.servlet.http.HttpServletRequest;
-
 import org.mkcl.els.domain.Department;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -28,33 +23,5 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/department")
 public class DepartmentController extends GenericController<Department>{
 
-	 /* (non-Javadoc)
- 	 * @see org.mkcl.els.controller.GenericController#populateNew(org.springframework.ui.ModelMap, org.mkcl.els.domain.BaseDomain, java.lang.String, javax.servlet.http.HttpServletRequest)
- 	 */
- 	@Override
-	    protected void populateNew(final ModelMap model,
-	                               final Department domain,
-	                               final String locale,
-	                               final HttpServletRequest request) {
-	        domain.setLocale(locale);
-	    	List<Department> deptList = Department.findAll(
-	    			 Department.class, "name", "asc", locale);
-	    	 model.addAttribute("parentDepartment", deptList);
-	    }
 
-
-	   /* (non-Javadoc)
-   	 * @see org.mkcl.els.controller.GenericController#populateEdit(org.springframework.ui.ModelMap, org.mkcl.els.domain.BaseDomain, javax.servlet.http.HttpServletRequest)
-   	 */
-   	@Override
-	   protected void populateEdit(final ModelMap model,
-	                                   final Department domain,
-	                                   final HttpServletRequest request) {
-	        domain.setLocale(domain.getLocale());
-	        model.addAttribute("parent", domain.getParentId());
-	          List<Department> deptList = Department.findAll(
-	    			 Department.class, "name", "asc", domain.getLocale());
-	        model.addAttribute("parentDepartment", deptList);
-
-   	}
 }
