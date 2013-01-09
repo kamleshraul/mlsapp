c534b35da5de55add92b78ae54c6d1ff92244dd0
dhananjayb <dhananjayb@348444cb-318d-0e48-9875-12270ded7a77>
2013-01-09 09:10:46 +0000

    added method findAllSundaysInYear

--- src/main/java/org/mkcl/els/common/util/DateUtil.java
+++ src/main/java/org/mkcl/els/common/util/DateUtil.java
@@ -2,8 +2,10 @@ package org.mkcl.els.common.util;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 
 import org.mkcl.els.domain.CustomParameter;
 
@@ -28,4 +30,37 @@ public class DateUtil {
 		return date;
 	}
 	
+	public static List<Date> findAllSundaysInYear(final Integer year) {
+		List<Date> sundays = new ArrayList<Date>();
+		
+		String yearStr = year.toString();
+		
+		String startYearDateStr = "01/01/"+yearStr;
+		String endYearDateStr = "31/12/"+yearStr;		
+				
+		SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+		Date startYearDate = null;
+		Date endYearDate = null;
+		try {
+			startYearDate = dateFormat.parse(startYearDateStr);
+			endYearDate = dateFormat.parse(endYearDateStr);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		SimpleDateFormat sf=new SimpleDateFormat("EEEE");
+		if(startYearDate != null && endYearDate !=null) {
+			Calendar start = Calendar.getInstance();
+	    	start.setTime(startYearDate);    	
+	    	Calendar end = Calendar.getInstance();
+	    	end.setTime(endYearDate);		
+	    	for (; !start.after(end); start.add(Calendar.DATE, 1)) {
+	    		Date current = start.getTime();
+	    		if(sf.format(current).equals("Sunday")){
+	    			sundays.add(current);
+	    		}
+	    	}
+		}
+		return sundays;
+	}	
 }
