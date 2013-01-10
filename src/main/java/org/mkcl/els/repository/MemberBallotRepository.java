e6ef1fcb476820cc64611179e8c6c2aaa261c6ee
sandeeps <sandeeps@348444cb-318d-0e48-9875-12270ded7a77>
2013-01-10 12:04:49 +0000

    added comments

--- src/main/java/org/mkcl/els/repository/MemberBallotRepository.java
+++ src/main/java/org/mkcl/els/repository/MemberBallotRepository.java
@@ -8,9 +8,11 @@ import java.util.List;
 
 import org.mkcl.els.common.util.ApplicationConstants;
 import org.mkcl.els.domain.DeviceType;
+import org.mkcl.els.domain.Group;
 import org.mkcl.els.domain.Member;
 import org.mkcl.els.domain.MemberBallot;
 import org.mkcl.els.domain.MemberBallotAttendance;
+import org.mkcl.els.domain.QuestionDates;
 import org.mkcl.els.domain.Session;
 import org.springframework.stereotype.Repository;
 
@@ -54,6 +56,27 @@ public class MemberBallotRepository extends BaseRepository<MemberBallot, Seriali
         }
     }
 
+    public List<MemberBallot> findByMember(final Session session,
+            final DeviceType deviceType, final Member member, final String locale) {
+        Search search=new Search();
+        search.addFilterEqual("session.id",session.getId());
+        search.addFilterEqual("deviceType.id",deviceType.getId());
+        search.addFilterEqual("member.id",member.getId());
+        search.addFilterEqual("locale",locale);
+        return this.search(search);
+    }
+
+    public MemberBallot findByMemberRound(final Session session,
+            final DeviceType questionType, final Member member, final int round, final String locale) {
+        Search search=new Search();
+        search.addFilterEqual("session.id",session.getId());
+        search.addFilterEqual("deviceType.id",questionType.getId());
+        search.addFilterEqual("member.id",member.getId());
+        search.addFilterEqual("round",round);
+        search.addFilterEqual("locale",locale);
+        return this.searchUnique(search);
+    }
+
     @SuppressWarnings("unchecked")
     public List<MemberBallot> viewMemberBallot(final Session session, final DeviceType deviceType,final Boolean attendanceType,
             final int round, final String locale) {
@@ -64,14 +87,47 @@ public class MemberBallotRepository extends BaseRepository<MemberBallot, Seriali
         return memberBallots;
     }
 
-    public List<MemberBallot> findByMember(final Session session,
-            final DeviceType deviceType, final Member member, final String locale) {
-        Search search=new Search();
-        search.addFilterEqual("session.id",session.getId());
-        search.addFilterEqual("deviceType.id",deviceType.getId());
-        search.addFilterEqual("member.id",member.getId());
-        search.addFilterEqual("locale",locale);
-        search.addSort("round",false);
-        return this.search(search);
+    @SuppressWarnings("unchecked")
+    public List<MemberBallot> viewMemberBallot(final Session session,
+            final DeviceType deviceType, final boolean attendance, final int round, final Group group,
+            final String locale) {
+        String query="SELECT mb FROM MemberBallot mb JOIN mb.member m JOIN mb.questionChoices qc JOIN qc.question q JOIN q.group g WHERE mb.session.id="+session.getId()+"  "+
+        " AND mb.deviceType.id="+deviceType.getId()+" AND mb.round="+round+" AND mb.attendance="+attendance+" AND g.id="+group.getId()+" AND mb.locale='"+locale+"' ORDER BY mb.position "+ApplicationConstants.ASC;
+        List<MemberBallot> memberBallots=new ArrayList<MemberBallot>();
+        memberBallots=this.em().createQuery(query).getResultList();
+        return memberBallots;
     }
+
+    @SuppressWarnings("unchecked")
+    public List<MemberBallot> viewMemberBallot(final Session session,
+            final DeviceType deviceType, final boolean attendance, final int round,
+            final QuestionDates answeringDate, final String locale) {
+        String query="SELECT mb FROM MemberBallot mb JOIN mb.member m JOIN mb.questionChoices qc WHERE mb.session.id="+session.getId()+"  "+
+        " AND mb.deviceType.id="+deviceType.getId()+" AND mb.round="+round+" AND mb.attendance="+attendance+" AND qc.newAnsweringDate.id="+answeringDate.getId()+" AND mb.locale='"+locale+"' ORDER BY mb.position "+ApplicationConstants.ASC;
+        List<MemberBallot> memberBallots=new ArrayList<MemberBallot>();
+        memberBallots=this.em().createQuery(query).getResultList();
+        return memberBallots;
+    }
+
+    public Integer findPrimaryCount(final Session session, final DeviceType deviceType,
+            final String locale) {
+        String query="SELECT q FROM MemberBallot mb JOIN mb.questionChoices qc JOIN qc.question q"+
+        " WHERE mb.session.id="+session.getId()+" AND mb.deviceType.id="+deviceType.getId()+" "+
+        " AND mb.locale='"+locale+"' AND q.parent=null";
+        Integer count=this.em().createQuery(query).getResultList().size();
+        return count;
+    }
+
+    public Boolean updateClubbing(final Session session, final DeviceType deviceType,
+            final int start, final int size, final String locale) {
+        try {
+            this.em().createNativeQuery("call memberballot_updateclubbing_procedure(?,?,?,?,?)").setParameter(1,session.getId()).setParameter(2,deviceType.getId()).setParameter(3,start).setParameter(4,size).setParameter(5,locale).executeUpdate();
+            return true;
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
 }
