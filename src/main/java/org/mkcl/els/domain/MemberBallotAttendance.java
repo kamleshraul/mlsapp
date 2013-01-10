15742678ec15b9c19c12472525ea8ab615df56c1
sandeeps <sandeeps@348444cb-318d-0e48-9875-12270ded7a77>
2013-01-10 11:36:50 +0000

    added comments

--- src/main/java/org/mkcl/els/domain/MemberBallotAttendance.java
+++ src/main/java/org/mkcl/els/domain/MemberBallotAttendance.java
@@ -1,3 +1,12 @@
+/**
+ * See the file LICENSE for redistribution information.
+ *
+ * Copyright (c) 2013 MKCL.  All rights reserved.
+ *
+ * Project: e-Legislature
+ * File: org.mkcl.els.domain.MemberBallotAttendance.java
+ * Created On: Jan 10, 2013
+ */
 package org.mkcl.els.domain;
 
 import java.io.Serializable;
@@ -9,33 +18,61 @@ import javax.persistence.Table;
 
 import org.mkcl.els.repository.MemberBallotAttendanceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Configurable;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class MemberBallotAttendance.
+ *
+ * @author sandeeps
+ * @since v1.0.0
+ */
+@Configurable
 @Entity
 @Table(name="memberballot_attendance")
 public class MemberBallotAttendance extends BaseDomain implements Serializable{
 
+    /** The Constant serialVersionUID. */
     private static final long serialVersionUID = 1L;
 
+    /** The session. */
     @ManyToOne
     private Session session;
 
+    /** The device type. */
     @ManyToOne
     private DeviceType deviceType;
 
+    /** The member. */
     @ManyToOne
     private Member member;
 
+    /** The attendance. */
     private Boolean attendance;
 
+    /** The position. */
     private Integer position;
 
+    /** The member ballot attendance repository. */
     @Autowired
     private transient MemberBallotAttendanceRepository memberBallotAttendanceRepository;
 
+    /**
+     * Instantiates a new member ballot attendance.
+     */
     public MemberBallotAttendance() {
         super();
     }
 
+    /**
+     * Instantiates a new member ballot attendance.
+     *
+     * @param session the session
+     * @param deviceType the device type
+     * @param member the member
+     * @param attendance the attendance
+     * @param locale the locale
+     */
     public MemberBallotAttendance(final Session session, final DeviceType deviceType,
             final Member member, final Boolean attendance,final String locale) {
         super(locale);
@@ -45,6 +82,11 @@ public class MemberBallotAttendance extends BaseDomain implements Serializable{
         this.attendance = attendance;
     }
 
+    /**
+     * Gets the member ballot attendance repository.
+     *
+     * @return the member ballot attendance repository
+     */
     public static MemberBallotAttendanceRepository getMemberBallotAttendanceRepository() {
         MemberBallotAttendanceRepository memberBallotAttendanceRepository = new MemberBallotAttendance().memberBallotAttendanceRepository;
         if (memberBallotAttendanceRepository == null) {
@@ -54,53 +96,113 @@ public class MemberBallotAttendance extends BaseDomain implements Serializable{
         return memberBallotAttendanceRepository;
     }
 
+    /**
+     * Gets the session.
+     *
+     * @return the session
+     */
     public Session getSession() {
         return session;
     }
 
 
+    /**
+     * Sets the session.
+     *
+     * @param session the new session
+     */
     public void setSession(final Session session) {
         this.session = session;
     }
 
 
+    /**
+     * Gets the device type.
+     *
+     * @return the device type
+     */
     public DeviceType getDeviceType() {
         return deviceType;
     }
 
 
+    /**
+     * Sets the device type.
+     *
+     * @param deviceType the new device type
+     */
     public void setDeviceType(final DeviceType deviceType) {
         this.deviceType = deviceType;
     }
 
 
+    /**
+     * Gets the member.
+     *
+     * @return the member
+     */
     public Member getMember() {
         return member;
     }
 
+    /**
+     * Sets the member.
+     *
+     * @param member the new member
+     */
     public void setMember(final Member member) {
         this.member = member;
     }
 
 
+    /**
+     * Gets the attendance.
+     *
+     * @return the attendance
+     */
     public Boolean getAttendance() {
         return attendance;
     }
 
 
+    /**
+     * Sets the attendance.
+     *
+     * @param attendance the new attendance
+     */
     public void setAttendance(final Boolean attendance) {
         this.attendance = attendance;
     }
 
+    /**
+     * Gets the position.
+     *
+     * @return the position
+     */
     public Integer getPosition() {
         return position;
     }
 
 
+    /**
+     * Sets the position.
+     *
+     * @param position the new position
+     */
     public void setPosition(final Integer position) {
         this.position = position;
     }
 
+    /**
+     * Find all.
+     *
+     * @param session the session
+     * @param questionType the question type
+     * @param attendance the attendance
+     * @param sortBy the sort by
+     * @param locale the locale
+     * @return the list
+     */
     public static List<MemberBallotAttendance> findAll(
             final Session session, final DeviceType questionType,
             final String attendance,final String sortBy,
@@ -109,12 +211,29 @@ public class MemberBallotAttendance extends BaseDomain implements Serializable{
                 session, questionType,attendance,sortBy, locale);
     }
 
+    /**
+     * Find members by attendance.
+     *
+     * @param session the session
+     * @param deviceType the device type
+     * @param attendanceType the attendance type
+     * @param locale the locale
+     * @return the list
+     */
     public static List<Member> findMembersByAttendance(final Session session,
             final DeviceType deviceType, final Boolean attendanceType, final String locale) {
         return getMemberBallotAttendanceRepository().findMembersByAttendance(session,
                 deviceType,attendanceType,locale);
     }
 
+    /**
+     * Find eligible members.
+     *
+     * @param session the session
+     * @param deviceType the device type
+     * @param locale the locale
+     * @return the list
+     */
     public static List<Member> findEligibleMembers(final Session session,
             final DeviceType deviceType, final String locale) {
         return getMemberBallotAttendanceRepository().findEligibleMembers(session,
