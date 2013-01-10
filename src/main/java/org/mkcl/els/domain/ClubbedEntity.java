4714244e2f95c0cd81492684f166e9fe30f14ae5
sandeeps <sandeeps@348444cb-318d-0e48-9875-12270ded7a77>
2013-01-10 11:32:56 +0000

    added comments

---
+++ src/main/java/org/mkcl/els/domain/ClubbedEntity.java
@@ -0,0 +1,115 @@
+/**
+ * See the file LICENSE for redistribution information.
+ *
+ * Copyright (c) 2013 MKCL.  All rights reserved.
+ *
+ * Project: e-Legislature
+ * File: org.mkcl.els.domain.ClubbedEntity.java
+ * Created On: Jan 10, 2013
+ */
+package org.mkcl.els.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import org.springframework.beans.factory.annotation.Configurable;
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Class ClubbedEntity.
+ *
+ * @author amitd
+ * @author sandeeps
+ * @since v1.0.0
+ */
+@Configurable
+@Entity
+@Table(name="clubbed_entities")
+public class ClubbedEntity extends BaseDomain implements Serializable{
+
+    /** The Constant serialVersionUID. */
+    private static final long serialVersionUID = 1L;
+
+    /** The position. */
+    private Integer position;
+
+    /** The device type. */
+    @ManyToOne(fetch=FetchType.LAZY)
+    private DeviceType deviceType;
+
+    /** The question. */
+    @ManyToOne(fetch=FetchType.LAZY)
+    private Question question;
+
+    /**
+     * Instantiates a new clubbed entity.
+     */
+    public ClubbedEntity() {
+        super();
+    }
+
+
+    /**
+     * Gets the position.
+     *
+     * @return the position
+     */
+    public Integer getPosition() {
+        return position;
+    }
+
+
+    /**
+     * Sets the position.
+     *
+     * @param position the new position
+     */
+    public void setPosition(final Integer position) {
+        this.position = position;
+    }
+
+
+    /**
+     * Gets the question.
+     *
+     * @return the question
+     */
+    public Question getQuestion() {
+        return question;
+    }
+
+
+    /**
+     * Sets the question.
+     *
+     * @param question the new question
+     */
+    public void setQuestion(final Question question) {
+        this.question = question;
+    }
+
+
+    /**
+     * Sets the device type.
+     *
+     * @param deviceType the new device type
+     */
+    public void setDeviceType(final DeviceType deviceType) {
+        this.deviceType = deviceType;
+    }
+
+
+    /**
+     * Gets the device type.
+     *
+     * @return the device type
+     */
+    public DeviceType getDeviceType() {
+        return deviceType;
+    }
+
+}
