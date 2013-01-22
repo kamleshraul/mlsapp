f02b5c4e6baf0773ea340e36159bdad9c77365d5
dhananjayb <dhananjayb@348444cb-318d-0e48-9875-12270ded7a77>
2013-01-22 06:37:26 +0000

    updated with latest code

--- src/main/java/org/mkcl/els/controller/WorkflowConfigController.java
+++ src/main/java/org/mkcl/els/controller/WorkflowConfigController.java
@@ -7,6 +7,8 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 
 import org.mkcl.els.common.util.ApplicationConstants;
+import org.mkcl.els.common.util.FormaterUtil;
+import org.mkcl.els.common.vo.AuthUser;
 import org.mkcl.els.domain.DeviceType;
 import org.mkcl.els.domain.HouseType;
 import org.mkcl.els.domain.UserGroupType;
@@ -20,20 +22,47 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/workflowconfig")
 public class WorkflowConfigController extends GenericController<WorkflowConfig>{
 
 
+	@Override
+	protected void populateModule(ModelMap model, HttpServletRequest request,
+			String locale, AuthUser currentUser) {
+			/**** Device Types ****/
+			List<DeviceType> deviceTypes = DeviceType.findDeviceTypesStartingWith("questions", locale);
+			model.addAttribute("deviceTypes", deviceTypes);
+			/**** House Types ****/
+			List<HouseType> houseTypes = new ArrayList<HouseType>();
+			String houseType=this.getCurrentUser().getHouseType();
+			if(houseType.equals("lowerhouse")){
+				houseTypes=HouseType.findAllByFieldName(HouseType.class, "type",houseType,"name",ApplicationConstants.ASC, locale);
+			}else if(houseType.equals("upperhouse")){
+				houseTypes=HouseType.findAllByFieldName(HouseType.class, "type",houseType,"name",ApplicationConstants.ASC, locale);
+			}else if(houseType.equals("bothhouse")){
+				houseTypes=HouseType.findAll(HouseType.class, "type", ApplicationConstants.ASC, locale);
+			}
+			model.addAttribute("houseTypes", houseTypes);
+			if(houseType.equals("bothhouse")){
+				houseType="lowerhouse";
+			}
+			model.addAttribute("houseType",houseType);
+	}
     @Override
     protected void populateNew(final ModelMap model, final WorkflowConfig domain,
             final String locale, final HttpServletRequest request) {
         /**** locale ****/
         domain.setLocale(locale);
-        /**** HouseTypes ****/
-        List<HouseType> houseTypes=HouseType.findAll(HouseType.class,"type",ApplicationConstants.ASC, locale);
-        model.addAttribute("houseTypes",houseTypes);
+        /**** HouseType ****/        
+        String strHouseType=request.getParameter("houseType");
+        if(strHouseType!=null){
+        	if(!strHouseType.isEmpty()){
+        		model.addAttribute("houseType",strHouseType);
+        	}
+        }
         /**** workflows ****/
         List<Workflow> workflows=Workflow.findAll(Workflow.class,"name",ApplicationConstants.ASC, locale);
         model.addAttribute("workflows",workflows);
@@ -41,10 +70,19 @@ public class WorkflowConfigController extends GenericController<WorkflowConfig>{
         List<UserGroupType> userGroupTypes=UserGroupType.findAll(UserGroupType.class,"name",ApplicationConstants.ASC, locale);
         model.addAttribute("userGroupTypes",userGroupTypes);
         /**** device types ****/
-        List<DeviceType> deviceTypes=DeviceType.findAll(DeviceType.class, "name",ApplicationConstants.ASC, locale);
+        List<DeviceType> deviceTypes=DeviceType.findAll(DeviceType.class,"name",ApplicationConstants.ASC, locale);
         model.addAttribute("deviceTypes",deviceTypes);
+        String strdeviceType=request.getParameter("deviceType");
+        if(strdeviceType!=null){
+        	if(!strdeviceType.isEmpty()){
+        		DeviceType deviceType=DeviceType.findById(DeviceType.class,Long.parseLong(strdeviceType));
+        		domain.setDeviceType(deviceType);
+        	}
+        }
         /**** workflow actor count ****/
-        model.addAttribute("workflowactorCount", 0);     
+        model.addAttribute("workflowactorCount", 0);
+        /**** Created On ****/
+        model.addAttribute("createdOn", FormaterUtil.getDateFormatter("en_US").format(new Date()));
     }
 
     @Override
@@ -52,8 +90,7 @@ public class WorkflowConfigController extends GenericController<WorkflowConfig>{
             final HttpServletRequest request) {
         String locale=domain.getLocale();
         /**** HouseTypes ****/
-        List<HouseType> houseTypes=HouseType.findAll(HouseType.class,"type",ApplicationConstants.ASC, locale);
-        model.addAttribute("houseTypes",houseTypes);
+        model.addAttribute("houseType",domain.getHouseType().getId());
         /**** workflows ****/
         List<Workflow> workflows=Workflow.findAll(Workflow.class,"name",ApplicationConstants.ASC, locale);
         model.addAttribute("workflows",workflows);
@@ -63,6 +100,10 @@ public class WorkflowConfigController extends GenericController<WorkflowConfig>{
         /**** device types ****/
         List<DeviceType> deviceTypes=DeviceType.findAll(DeviceType.class, "name",ApplicationConstants.ASC, locale);
         model.addAttribute("deviceTypes",deviceTypes);
+        /**** Created On ****/
+        if(domain.getCreatedOn()!=null){
+        model.addAttribute("createdOn", FormaterUtil.getDateFormatter("en_US").format(domain.getCreatedOn()));
+        }
         /**** workflow actors ****/
         model.addAttribute("workflowactors", domain.getWorkflowactors());
         model.addAttribute("workflowactorCount", domain.getWorkflowactors().size());                
@@ -75,11 +116,6 @@ public class WorkflowConfigController extends GenericController<WorkflowConfig>{
         for (int i = 1; i <= workflowactorCount; i++) {
             WorkflowActor workflowActor=new WorkflowActor();
 
-            String level=request.getParameter("workflowactorLevel"+i);
-            if(level!=null){
-                workflowActor.setLevel(Integer.parseInt(level));
-            }           
-
             String strUserGroupType=request.getParameter("workflowactorName"+ i);
             if(strUserGroupType!=null){
                 UserGroupType userGroupType=UserGroupType.findById(UserGroupType.class,Long.parseLong(strUserGroupType));
@@ -90,6 +126,13 @@ public class WorkflowConfigController extends GenericController<WorkflowConfig>{
             if(id!=null){
                 if(!id.isEmpty()){
                     workflowActor.setId(Long.parseLong(id));
+                }
+            }
+            
+            String level=request.getParameter("workflowactorLevel"+ i);
+            if(level!=null){
+                if(!level.isEmpty()){
+                    workflowActor.setLevel(Integer.parseInt(level));
                 }
             }
 
@@ -126,33 +169,45 @@ protected void preValidateUpdate(final WorkflowConfig domain,
 }
 
 @Transactional
-@RequestMapping(value = "/workflowactor/{workflowconfigId}/{workflowactorId}/delete", method = RequestMethod.DELETE)
-public String deleteFamily(final @PathVariable("workflowconfigId") Long workflowconfigId,
-		final @PathVariable("workflowactorId") Long workflowactorId,
+@RequestMapping(value = "/{workflowconfigId}/{workflowactorid}/delete", method = RequestMethod.DELETE)
+public @ResponseBody String deleteWorkflowActor(final @PathVariable("workflowconfigId") Long workflowconfigId,
+		final @PathVariable("workflowactorid") Long workflowactorId,
         final ModelMap model, final HttpServletRequest request) {
-	WorkflowConfig.removeActor(workflowconfigId,workflowactorId);
-    return "info";
+	WorkflowConfig workflowConfig=WorkflowConfig.findById(WorkflowConfig.class,workflowconfigId);
+	Boolean status=false;
+	if(workflowConfig.getIsLocked()==false){
+	status=WorkflowConfig.removeActor(workflowconfigId,workflowactorId);
+	}
+	if(status){
+		return "SUCCESS";
+	}else{
+		return "FAILED";
+	}
+}
+
+@Override
+public String delete(Long id, ModelMap model, HttpServletRequest request) {
+	WorkflowConfig workflowConfig=WorkflowConfig.findById(WorkflowConfig.class,id);
+	if(workflowConfig.getIsLocked()==false){
+		workflowConfig.remove();
+		model.addAttribute("flag","SUCCESS");
+	}else{
+		model.addAttribute("flag","SUCCESS");
+	}	
+	return "deleteinfo";
 }
 
 @Override
 protected void customValidateCreate(WorkflowConfig domain,
 		BindingResult result, HttpServletRequest request) {
+	
 }
 
 @Override
 protected void customValidateUpdate(WorkflowConfig domain,
 		BindingResult result, HttpServletRequest request) {
+	
 }
 
-@Override
-protected void populateCreateIfNoErrors(ModelMap model,
-		WorkflowConfig domain, HttpServletRequest request) {
-	domain.setCreatedOn(new Date());
-}
 
-@Override
-protected void populateUpdateIfNoErrors(ModelMap model,
-		WorkflowConfig domain, HttpServletRequest request) {
-	domain.setCreatedOn(new Date());
-}
 }
