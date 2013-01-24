53f947639f278548644638183a51f019bd1aa094
amitd <amitd@348444cb-318d-0e48-9875-12270ded7a77>
2013-01-24 12:56:10 +0000

    Added as a replacement for HalfHourBallotVO

---
+++ src/main/java/org/mkcl/els/common/vo/BallotVO.java
@@ -0,0 +1,50 @@
+package org.mkcl.els.common.vo;
+
+public class BallotVO {
+
+	//=============== ATTRIBUTES ====================
+	private String memberName;
+	private Integer questionNumber;
+	private String questionSubject;
+	
+
+	//=============== CONSTRUCTORS ==================
+	public BallotVO() {
+		super();
+	}
+	
+	public BallotVO(String memberName, Integer questionNumber,
+			String questionSubject) {
+		super();
+		this.memberName = memberName;
+		this.questionNumber = questionNumber;
+		this.questionSubject = questionSubject;
+	}
+	
+	
+	//=============== GETTERS/SETTERS ===============
+	public String getMemberName() {
+		return memberName;
+	}
+
+	public void setMemberName(String memberName) {
+		this.memberName = memberName;
+	}
+
+	public Integer getQuestionNumber() {
+		return questionNumber;
+	}
+
+	public void setQuestionNumber(Integer questionNumber) {
+		this.questionNumber = questionNumber;
+	}
+
+	public String getQuestionSubject() {
+		return questionSubject;
+	}
+
+	public void setQuestionSubject(String questionSubject) {
+		this.questionSubject = questionSubject;
+	}
+
+}
