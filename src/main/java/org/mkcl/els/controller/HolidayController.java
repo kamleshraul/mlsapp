5dd8e35a3d81a8c42affa8a5afff0670c46af814
dhananjayb <dhananjayb@348444cb-318d-0e48-9875-12270ded7a77>
2013-01-31 06:54:56 +0000

    overridden validation methods as duplicate names of holidays should be allowed

--- src/main/java/org/mkcl/els/controller/HolidayController.java
+++ src/main/java/org/mkcl/els/controller/HolidayController.java
@@ -4,23 +4,46 @@
  * Copyright (c) 2012 MKCL.  All rights reserved.
  *
  * Project: e-Legislature
- * File: org.mkcl.els.controller.TitleController.java
- * Created On: Mar 8, 2012
+ * File: org.mkcl.els.controller.HolidayController.java
+ * Created On: Jan 8, 2013
  */
 package org.mkcl.els.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.mkcl.els.domain.Holiday;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
- * The Class TitleController.
+ * The Class HolidayController.
  *
- * @author anand
+ * @author dhananjay
  * @since v1.0.0
  */
 @Controller
 @RequestMapping("/holiday")
 public class HolidayController extends GenericController<Holiday> {
+	
+	@Override
+	protected void customValidateCreate(final Holiday domain,
+            final BindingResult result, final HttpServletRequest request) {
+        customValidateHoliday(domain, result, request);
+    }
+	
+	@Override
+	protected void customValidateUpdate(final Holiday domain,
+            final BindingResult result, final HttpServletRequest request) {
+        customValidateHoliday(domain, result, request);
+    }
+	
+	private void customValidateHoliday(final Holiday domain, final BindingResult result,
+            final HttpServletRequest request) {
+        // Check for version mismatch
+        if (domain.isVersionMismatch()) {
+            result.rejectValue("VersionMismatch", "version");
+        }       
+    }
 	
 }
