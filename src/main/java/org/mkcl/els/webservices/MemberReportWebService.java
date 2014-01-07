73ac9fa783eb8d25121f323e0dbb1419c5b9fc12
sandeeps <sandeeps@348444cb-318d-0e48-9875-12270ded7a77>
2014-01-07 09:32:40 +0000

    New from branch

--- src/main/java/org/mkcl/els/webservices/MemberReportWebService.java
+++ src/main/java/org/mkcl/els/webservices/MemberReportWebService.java
@@ -10,8 +10,10 @@
 package org.mkcl.els.webservices;
 
 import java.util.List;
+import java.util.Map;
 
-import org.mkcl.els.common.vo.MemberAgeWiseReportVO;
+import javax.servlet.http.HttpServletRequest;
+
 import org.mkcl.els.common.vo.MemberChildrenWiseReportVO;
 import org.mkcl.els.common.vo.MemberGeneralVO;
 import org.mkcl.els.common.vo.MemberPartyDistrictWiseVO;
@@ -19,6 +21,7 @@ import org.mkcl.els.common.vo.MemberPartyWiseReportVO;
 import org.mkcl.els.common.vo.MemberProfessionWiseReportVO;
 import org.mkcl.els.common.vo.MemberQualificationWiseReportVO;
 import org.mkcl.els.domain.Member;
+import org.mkcl.els.domain.Query;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,104 +36,17 @@ import org.springframework.web.bind.annotation.ResponseBody;
  */
 @Controller
 @RequestMapping("ws/memberreport")
-public class MemberReportWebService {
+public class MemberReportWebService {   
 
-    /**
-     * Find members by age.
-     *
-     * @param locale the locale
-     * @return the member age wise report vo
-     */
-    @RequestMapping(value="/age/{locale}")
-    public @ResponseBody MemberAgeWiseReportVO findMembersByAge(@PathVariable("locale") final String locale){
-        return Member.findMembersByAge(locale);
-    }
-
-    /**
-     * Find members by qualification.
-     *
-     * @param locale the locale
-     * @return the member qualification wise report vo
-     */
-    @RequestMapping(value="/qualification/{locale}")
-    public @ResponseBody MemberQualificationWiseReportVO findMembersByQualification(@PathVariable("locale") final String locale){
-        return Member.findMembersByQualification(locale);
-    }
-
-    /**
-     * Find members by profession.
-     *
-     * @param locale the locale
-     * @return the member profession wise report vo
-     */
-    @RequestMapping(value="/profession/{locale}")
-    public @ResponseBody MemberProfessionWiseReportVO findMembersByProfession(@PathVariable("locale") final String locale){
-        return Member.findMembersByProfession(locale);
-    }
-
-    /**
-     * Find members by children.
-     *
-     * @param locale the locale
-     * @return the member children wise report vo
-     */
-    @RequestMapping(value="/children/{locale}")
-    public @ResponseBody MemberChildrenWiseReportVO findMembersByChildren(@PathVariable("locale") final String locale){
-        return Member.findMembersByChildren(locale);
-    }
-
-    /**
-     * Find members by party.
-     *
-     * @param locale the locale
-     * @return the member party wise report vo
-     */
-    @RequestMapping(value="/party/{locale}")
-    public @ResponseBody MemberPartyWiseReportVO findMembersByParty(@PathVariable("locale") final String locale){
-        return Member.findMembersByParty(locale);
-    }
-
-    /**
-     * Find members by party district.
-     *
-     * @param locale the locale
-     * @return the list
-     */
-    @RequestMapping(value="/partydistrict/{locale}")
-    public @ResponseBody List<MemberPartyDistrictWiseVO> findMembersByPartyDistrict(@PathVariable("locale") final String locale){
-        return Member.findMembersByPartyDistrict(locale);
-    }
-
-    /**
-     * Findfemale members.
-     *
-     * @param locale the locale
-     * @return the list
-     */
-    @RequestMapping(value="/female/{locale}")
-    public @ResponseBody List<MemberGeneralVO> findfemaleMembers(@PathVariable("locale") final String locale){
-        return Member.findfemaleMembers(locale);
-    }
-
-    /**
-     * Find members by last name.
-     *
-     * @param locale the locale
-     * @return the list
-     */
-    @RequestMapping(value="/lastname/{locale}")
-    public @ResponseBody List<MemberGeneralVO> findMembersByLastName(@PathVariable("locale") final String locale){
-        return Member.findMembersByLastName(locale);
-    }
-
-    /**
-     * Find members by district.
-     *
-     * @param locale the locale
-     * @return the list
-     */
-    @RequestMapping(value="/district/{locale}")
-    public @ResponseBody List<MemberGeneralVO> findMembersByDistrict(@PathVariable("locale") final String locale){
-        return Member.findMembersByDistrict(locale);
-    }
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	@RequestMapping(value="/{report}")
+	public @ResponseBody List findReport(@PathVariable("report") final String report,
+			final HttpServletRequest request){
+		Map<String,String[]> requestMap=request.getParameterMap();
+		if(report.equals("MIS_REPORT_PARTYDISTRICT")){
+			return Query.findMISPartyDistrictReport(report,requestMap);
+		}else{
+			return Query.findReport(report,requestMap);
+		}
+	}	
 }
