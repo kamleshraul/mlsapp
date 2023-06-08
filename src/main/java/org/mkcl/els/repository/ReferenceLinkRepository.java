69cc7b2279188736c1b2307e51c955f0c9a926cb
shubhama <shubhama@348444cb-318d-0e48-9875-12270ded7a77>
2023-06-08 06:04:38 +0000

    Updated the exisitng func to fetch Ref link

--- src/main/java/org/mkcl/els/repository/ReferenceLinkRepository.java
+++ src/main/java/org/mkcl/els/repository/ReferenceLinkRepository.java
@@ -8,61 +8,71 @@ import java.util.List;
 
 import org.mkcl.els.common.util.FormaterUtil;
 import org.mkcl.els.common.vo.ReferenceLinkVO;
+import org.mkcl.els.domain.Query;
 import org.mkcl.els.domain.ReferenceLinks;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class ReferenceLinkRepository extends BaseRepository<ReferenceLinks, Serializable>{
 
-	public List<ReferenceLinkVO> findReferences(final Integer devicetype, final Integer housetype, final Integer documenttype){
+	public List<ReferenceLinkVO> findReferences(final Integer housetype  , final Integer devicetype, final Integer documenttype){
 	
 		/*List<ReferenceLinkVO> referenceLinkVOs=new ArrayList<ReferenceLinkVO>();*/
 		String query = "";
-		if(devicetype == 5){
-			query = "SELECT CONCAT('अतारांकित प्रश्नोत्तरांची ',rt.display_name,' (', st.session_type,') अधिवेशन ', formater(ss.session_year,'mr_IN')) AS NAME, rk.date, rk.link "
-					+" FROM reference_links rk" 
-					+" JOIN sessions ss ON (rk.session_id = ss.id)"
-					+" JOIN sessiontypes st ON (ss.sessiontype_id = st.id)"
-					+" JOIN referencetypes rt ON (rk.documenttype_id = rt.id)"
-					+" WHERE rk.devicetype_id = "+devicetype+" AND rk.housetype_id = "+housetype+" AND rk.documenttype_id = "+documenttype+" ORDER BY rk.id ASC";
+		if(devicetype.equals(5)){
+			
+			Query q = Query.findByName(Query.class, "REFERENCELINK_WS_QUERY_DEVICETYPE_5","mr_IN" );
+			if(q != null)
+			{
+			 query= q.getKeyField();
+			}
 		}
 		else {
-			query = "SELECT rk.name,rk.date,rk.link,rk.locale,rt.display_name,rk.date,ss.session_year FROM reference_links rk "
-					+"JOIN sessions ss ON (rk.session_id = ss.id)"
-					+" JOIN sessiontypes st ON (ss.sessiontype_id = st.id)"
-					+" JOIN referencetypes rt ON (rk.documenttype_id = rt.id)"
-					+"where rk.devicetype_id ='"+devicetype+"' and rk.housetype_id = '"+housetype+"' and rk.documenttype_id = '"+documenttype+"' ORDER BY rk.id ASC";	
+			
+			Query q = Query.findByName(Query.class, "REFERENCELINK_WS_QUERY_DEVICETYPE_OTHER","mr_IN" );
+			if(q != null)
+			{
+			 query= q.getKeyField();
+			}
 		}
 		
-		List records= this.em().createNativeQuery(query).getResultList();
-	
-		List<ReferenceLinkVO> referenceLinkVOs =new ArrayList<ReferenceLinkVO>();
+		if(!query.isEmpty() || query != "")
+		{
+			javax.persistence.Query q  =  this.em().createQuery(query); 
+			q.setParameter("deviceType", devicetype);
+			q.setParameter("houseType", housetype);
+			q.setParameter("documentType", documenttype);
+			List records= this.em().createNativeQuery(query).getResultList();
 		
-		Date dbFormat = null;
-		for(Object i:records){
-			Object[] o=(Object[]) i;
-			ReferenceLinkVO referenceInfo =new ReferenceLinkVO();
-			referenceInfo.setName(o[0]!=null?o[0].toString().trim():"-");
-			if(o[1].toString() != null){
-				try {
-					dbFormat = FormaterUtil.getDateFormatter("yyyy-MM-dd", "mr_IN").parse(o[1].toString());
-				} catch (ParseException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
+			List<ReferenceLinkVO> referenceLinkVOs =new ArrayList<ReferenceLinkVO>();
+			
+			Date dbFormat = null;
+			for(Object i:records){
+				Object[] o=(Object[]) i;
+				ReferenceLinkVO referenceInfo =new ReferenceLinkVO();
+				referenceInfo.setName(o[0]!=null?o[0].toString().trim():"-");
+				if(o[1].toString() != null){
+					try {
+						dbFormat = FormaterUtil.getDateFormatter("yyyy-MM-dd", "mr_IN").parse(o[1].toString());
+					} catch (ParseException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					String serverFormat=FormaterUtil.getDateFormatter("dd MMM yyyy","mr_IN").format(dbFormat);
+					referenceInfo.setDate(serverFormat);
 				}
-				String serverFormat=FormaterUtil.getDateFormatter("dd MMM yyyy","mr_IN").format(dbFormat);
-				referenceInfo.setDate(serverFormat);
+				else{
+					referenceInfo.setDate("-");
+				}
+				referenceInfo.setLink(o[2]!=null?o[2].toString().trim():"-");
+				referenceInfo.setLocale(o[3]!=null?o[3].toString().trim():"-");
+				referenceInfo.setLocalizedname(o[4]!=null?o[4].toString().trim():"-");
+				referenceInfo.setEnglishFormatDate(o[5]!=null?o[5].toString().trim():"-");
+				referenceLinkVOs.add(referenceInfo);
 			}
-			else{
-				referenceInfo.setDate("-");
-			}
-			referenceInfo.setLink(o[2]!=null?o[2].toString().trim():"-");
-			referenceInfo.setLocale(o[3]!=null?o[3].toString().trim():"-");
-			referenceInfo.setLocalizedname(o[4]!=null?o[4].toString().trim():"-");
-			referenceInfo.setEnglishFormatDate(o[5]!=null?o[5].toString().trim():"-");
-			referenceLinkVOs.add(referenceInfo);
+					
+			return referenceLinkVOs;
 		}
-				
-		return referenceLinkVOs;
+		return null;
 	}
 }
