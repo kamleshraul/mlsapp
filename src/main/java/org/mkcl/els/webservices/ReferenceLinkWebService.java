e71e0d9c708f2acefffa98a521609607192d95f1
shubhama <shubhama@348444cb-318d-0e48-9875-12270ded7a77>
2023-06-21 01:38:11 +0000

    Added func to fetch session and refernce links as per published

--- src/main/java/org/mkcl/els/webservices/ReferenceLinkWebService.java
+++ src/main/java/org/mkcl/els/webservices/ReferenceLinkWebService.java
@@ -6,6 +6,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.mkcl.els.common.vo.ReferenceLinkVO;
+import org.mkcl.els.common.vo.TemplateVO;
+import org.mkcl.els.common.vo.TestVO;
 import org.mkcl.els.domain.ReferenceLinks;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,7 +17,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 
 @Controller
-@RequestMapping("/ws/yaadi")
+@RequestMapping("/ws/referencelink")
 public class ReferenceLinkWebService {
 
 	@RequestMapping(value = "/{housetype}/{devicetype}/{documenttype}",method=RequestMethod.GET)
@@ -25,5 +27,31 @@ public class ReferenceLinkWebService {
     	response.setHeader("Access-Control-Allow-Origin", "*");
     	return ReferenceLinks.findReferences(housetype,devicetype,documenttype);
     }
+	
+	
+	@RequestMapping(value = "/housetype/{housetype}/devicetype/{devicetype}/{locale}",method=RequestMethod.GET)
+    public @ResponseBody List<TestVO> getSessionAndReferenceNumber(@PathVariable("devicetype") final Integer devicetype ,
+            @PathVariable("housetype")final Integer housetype ,
+            @PathVariable("locale")final String locale,
+           HttpServletRequest request, HttpServletResponse response){
+    	response.setHeader("Access-Control-Allow-Origin", "*");
+    	
+    	return ReferenceLinks.getSessionAndReferenceNumber(housetype,locale);
+    }
+	
+	
+	@RequestMapping(value = "/{sessionId}/{documentTypeId}",method=RequestMethod.GET)
+    public @ResponseBody List<TemplateVO> getYaadiLinkAndNumber(@PathVariable("sessionId") final Integer sessionId ,
+            @PathVariable("documentTypeId")final Integer documentTypeId ,
+           
+           HttpServletRequest request, HttpServletResponse response){
+    	response.setHeader("Access-Control-Allow-Origin", "*");
+    	
+    	return ReferenceLinks.getYaadiLinkAndNumber(sessionId, documentTypeId);
+    }
+	
+	
+	
+	
 	
 }
