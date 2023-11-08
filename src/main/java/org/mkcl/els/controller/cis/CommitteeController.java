4278d9b40258b19554676b995cdeb5b2c5c151b7
shubhama <shubhama@348444cb-318d-0e48-9875-12270ded7a77>
2023-11-08 03:46:01 +0000

    Added Api to create,get ,update committee from admin login

--- src/main/java/org/mkcl/els/controller/cis/CommitteeController.java
+++ src/main/java/org/mkcl/els/controller/cis/CommitteeController.java
@@ -1,12 +1,20 @@
 package org.mkcl.els.controller.cis;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.mkcl.els.common.exception.ELSException;
 import org.mkcl.els.common.util.ApplicationConstants;
@@ -15,11 +23,15 @@ import org.mkcl.els.common.vo.AuthUser;
 import org.mkcl.els.controller.GenericController;
 import org.mkcl.els.controller.question.QuestionController;
 import org.mkcl.els.domain.Committee;
+import org.mkcl.els.domain.CommitteeDesignation;
 import org.mkcl.els.domain.CommitteeMember;
 import org.mkcl.els.domain.CommitteeMemberAttendance;
 import org.mkcl.els.domain.CommitteeName;
 import org.mkcl.els.domain.CommitteeType;
 import org.mkcl.els.domain.CustomParameter;
+import org.mkcl.els.domain.Grid;
+import org.mkcl.els.domain.Member;
+import org.mkcl.els.domain.Session;
 import org.mkcl.els.domain.Status;
 import org.mkcl.els.domain.SubDepartment;
 import org.mkcl.els.domain.UserGroup;
@@ -28,6 +40,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -427,5 +440,422 @@ public class CommitteeController extends GenericController<Committee> {
 		
 		return userGroupTypes;
 	}
+	
+	
+	@RequestMapping(value ="/adminCommitteeCreation/module",method = RequestMethod.GET)
+	public String  getCommitteeCreationModule(final ModelMap map,HttpServletRequest request,HttpSession session ,final Locale locale) {
+		
+		
+		String urlPattern=request.getRequestURI().toString();
+		System.out.println(urlPattern);
+		
+		String[] SplittedURI = urlPattern.split("/");
+		map.addAttribute("urlPattern", SplittedURI[3]);
+		map.addAttribute("messagePattern", SplittedURI[2]);
+		
+//         String messagePattern=urlPattern.replaceAll("\\/",".");
+//         model.addAttribute("messagePattern", messagePattern);
+//         model.addAttribute("urlPattern", urlPattern);
+		return "committee/committeeCreation/module";
+	}
+	
+	@RequestMapping(value ="/adminCommitteeCreation/list",method = RequestMethod.GET)
+	public String  getCommitteeCreationList(final ModelMap map,HttpServletRequest request,final Locale locale) {
+		
+		
+		String urlPatternFromRequest=request.getRequestURI().toString();
+		System.out.println(urlPatternFromRequest);
+		
+		String[] SplittedURI = urlPatternFromRequest.split("/");
+		map.addAttribute("urlPattern", SplittedURI[3]);
+		map.addAttribute("messagePattern", SplittedURI[2]);
+		
+		// final String servletPath = request.getServletPath().replaceFirst("\\/","");
+	        String urlPattern=SplittedURI[3];
+	        String messagePattern=SplittedURI[2];
+	       // String newurlPattern=modifyURLPattern(urlPattern,request,map,locale.toString());
+	        Grid grid = null;
+	        try{
+	        	grid = Grid.findByDetailView(urlPattern, locale.toString());
+	        	map.addAttribute("gridId", grid.getId());
+	        }catch (ELSException e) {
+	        	logger.error(e.getMessage());
+				map.addAttribute("error", e.getParameter());			
+			}        
+//	        /******* Hook **********/
+//	        populateList(model, request, locale.toString(), this.getCurrentUser());
+//	        /***********************/
+//	        if (formtype != null) {
+//	            if (formtype.equals("g")) {
+//	                model.addAttribute("messagePattern", messagePattern);
+//	                model.addAttribute("urlPattern", urlPattern);
+//	                return "generic/list";
+//	            }
+//	        }
+	        //here making provisions for displaying error pages
+	        if(map.containsAttribute("errorcode")){
+	            return "generic/error";
+	        }else{
+	        	return "committee/committeeCreation/list";
+	        }
+	
+	}
+	
+	@RequestMapping(value ="/adminCommitteeCreation/new",method = RequestMethod.GET)
+	public String  getCommitteeCreationNew(final ModelMap model,HttpServletRequest request,final Locale locale) {
+		
+		List<CommitteeName> committeeNames = CommitteeName.findAll(CommitteeName.class, "id", ApplicationConstants.DESC, locale.toString());
+		model.addAttribute("committeeNames", committeeNames);
+		
+		List<Status> allCommitteStatus = Status.findAllByStartingWith(Status.class, "type", "committee", "id",ApplicationConstants.ASC, locale.toString());
+		model.addAttribute("allCommitteeStatus", allCommitteStatus);
+		
+		List<CommitteeDesignation> committeeDesignation = CommitteeDesignation.findAll(CommitteeDesignation.class, "id", ApplicationConstants.DESC, locale.toString());
+		model.addAttribute("committeeDesignation",committeeDesignation);
+		
+		
+		return "committee/committeeCreation/new";
+	}
+	
+	
+	@RequestMapping(value ="/adminCommitteeCreation/create",method = RequestMethod.POST)
+	public String  getCommitteeCreationCreate(final ModelMap model,HttpServletRequest request,final Locale locale) {
+		String retVal = "generic/error";
+		
+		
+		String strtotalRows = request.getParameter("totalRows");
+		String strcommitteeNameId = request.getParameter("committeeNameId");
+		String strStatusId = request.getParameter("statusId");
+		String strFormationDate = request.getParameter("formationDate");
+		String strfoundationDate = request.getParameter("foundationDate");
+		String strdissolutionDate = request.getParameter("dissolutionDate");
+		Long committeeId = null; // populate After Persisting
+		
+		List<Map<String,String>> committeeMemberDetails = new ArrayList<>();
+		
+		Integer totalRows = Integer.parseInt(strtotalRows);
+		
+		if(totalRows != null) {
+			CommitteeName cm = CommitteeName.findById(CommitteeName.class, Long.parseLong(strcommitteeNameId));
+			Status s  = Status.findById(Status.class, Long.parseLong(strStatusId));
+						
+			for (int i=0;i<totalRows;i++)
+			{
+				Map<String,String> ymap = new HashMap<String,String>();
+				String  priority = request.getParameter("committeeDetails["+i+"][priority]");
+				ymap.put("priority",priority);
+				
+				String  memberId = request.getParameter("committeeDetails["+i+"][memberId]");
+				ymap.put("memberId",memberId);
+				
+				String  designation = request.getParameter("committeeDetails["+i+"][designation]");
+				ymap.put("designation",designation);
+				
+				committeeMemberDetails.add(ymap);
+			}
+			
+			Committee newComittee = new Committee();
+			newComittee.setCommitteeName(cm);
+			newComittee.setStatus(s);
+			DateFormat formatter = new SimpleDateFormat("yyyy-MM-DD"); 
+			Date formationDate = null;
+			Date foundationDate = null;
+			Date dissolutionDate = null;
+			try {
+				 formationDate = (Date)formatter.parse(strFormationDate);
+				 foundationDate = (Date)formatter.parse(strfoundationDate);
+				 dissolutionDate = (Date)formatter.parse(strdissolutionDate);
+				 
+			} catch (ParseException e) {
+				System.out.println("Error At Converting String to Date Object in Committee Creation Class in [ getCommitteeCreationCreate ] Method");
+				e.printStackTrace();
+			}
+			
+			if(formationDate != null ) {
+				newComittee.setFormationDate(formationDate);			
+			}else {
+				newComittee.setFormationDate(new Date());
+			}
+			
+			 if (foundationDate != null) {
+				 newComittee.setCreationDate(foundationDate);
+			 }else {
+				 newComittee.setCreationDate(new Date());
+			 }
+			 
+			 if(dissolutionDate != null) {
+				 newComittee.setDissolutionDate(dissolutionDate);
+			 }
+		
+			List<CommitteeMember> ogCommitteeMember = new ArrayList<CommitteeMember>();
+			List<CommitteeMember> invitedCommitteeMember = new ArrayList<CommitteeMember>();
+			
+			if(committeeMemberDetails != null )
+			{
+				for(Map<String,String>  i : committeeMemberDetails)
+				{
+				CommitteeMember committeeMember = new CommitteeMember();
+					
+				 String strmemberId = 	i.get("memberId");
+				 Member member  = Member.findById(Member.class, Long.parseLong(strmemberId));				 
+				 committeeMember.setMember(member);				 
+				 committeeMember.setJoiningDate(new Date());
+				 
+				 String strpriority = i.get("priority");
+				 if(strpriority != null && !strpriority.equals(" - ")) {
+					 committeeMember.setPosition(Integer.parseInt(strpriority));
+				 }
+				 
+				 String strDesignation = i.get("designation");
+				 if(strDesignation != null) {
+					 CommitteeDesignation memberComitteeDesignation = member.findById(CommitteeDesignation.class, Long.parseLong(strDesignation));
+					 committeeMember.setDesignation(memberComitteeDesignation);
+				 }		
+				 
+				 if(committeeMember.getPosition() != null) {
+					 ogCommitteeMember.add(committeeMember);
+				 }else {
+					 invitedCommitteeMember.add(committeeMember);
+				 }
+				}
+			}
+			newComittee.setMembers(ogCommitteeMember);
+			newComittee.setInvitedMembers(invitedCommitteeMember);
+			newComittee.setStatus(s);
+			
+			int rowsAdded = ogCommitteeMember.size() + invitedCommitteeMember.size();
+			
+			
+			newComittee.setLocale(locale.toString());
+			Committee createdCommittee = (Committee) newComittee.persist();
+			committeeId = createdCommittee.getId();
+									
+		}
+		
+		
+			if(committeeId != null) {
+				System.out.println("Committee Added SuccessFully !!");
+				this.getCommitteeDetailsForEdit(committeeId, model, request, locale);
+				model.addAttribute("success","Added");
+				retVal = "committee/committeeCreation/edit";		
+				return retVal;	
+			}								
+		return retVal;
+	}
+	
+	@RequestMapping(value ="/adminCommitteeCreation/{id}/edit",method = RequestMethod.GET)
+	public String  getCommitteeCreationEdit(final ModelMap model,HttpServletRequest request,@PathVariable("id") final Long committeeId,final Locale locale) {
+		this.getCommitteeDetailsForEdit(committeeId,model, request, locale);
+		return "committee/committeeCreation/edit";
+	}
+	
+	private void getCommitteeDetailsForEdit( final Long committeeId,final ModelMap model,HttpServletRequest request,final Locale locale) {
+		
+		SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+		
+		Committee  selectedCommittee = Committee.findById(Committee.class, committeeId);
+		
+		model.addAttribute("selectedCommitteeId", selectedCommittee.getId());		
+		Status selectedCommitteStatus = selectedCommittee.getStatus();
+		model.addAttribute("selectedCommitteStatus",selectedCommitteStatus);
+		
+		
+		CustomParameter cp =CustomParameter.findByFieldName(CustomParameter.class,"name","COMMITTEE_STATUS_FOR_CREATION","");
+		
+		if(cp != null) {
+			
+			List<Status> allCommitteeStatus = new ArrayList<Status>();
+	 		String allfetchedStatus = cp.getValue();
+	 		String[] splitedFetchedStatus = allfetchedStatus.split(",");
+			
+	 		for(String s : splitedFetchedStatus) {
+	 			Status  status = Status.findByType(s, locale.toString());
+	 			if(status != null) {
+	 				allCommitteeStatus.add(status);
+	 			}
+	 		}
+	 		model.addAttribute("allCommitteeStatus", allCommitteeStatus);
+		}
+		
+		
+		List<CommitteeDesignation> allComitteeDesignation = CommitteeDesignation.findAll(CommitteeDesignation.class, "id", ApplicationConstants.ASC, locale.toString());
+		model.addAttribute("allComitteeDesignation", allComitteeDesignation);
+				
+		
+		List<CommitteeName> allCommitteeNames = CommitteeName.findAll(CommitteeName.class, "id", ApplicationConstants.DESC, locale.toString());
+		model.addAttribute("allCommitteeNames", allCommitteeNames);
+		
+		model.addAttribute("selectedCommitteeName", selectedCommittee.getCommitteeName().getName());
+		
+		String strSelectedCommitteFormationDate = null;
+		
+		if(selectedCommittee.getFormationDate() != null)
+		{
+			strSelectedCommitteFormationDate = formatter.format(selectedCommittee.getFormationDate());					
+		}
+		model.addAttribute("selectedCommitteFormationDate", strSelectedCommitteFormationDate);
+		
+		
+		String strSelectedCommitteeCreationDate = null;
+		if(selectedCommittee.getCreationDate() != null) {
+			strSelectedCommitteeCreationDate = formatter.format(selectedCommittee.getCreationDate());			
+		}
+		model.addAttribute("selectedCommitteeCreationDate", strSelectedCommitteeCreationDate);
+		 
+		String strSelectedCommitteeDissolutionDate = null;				
+		if(selectedCommittee.getDissolutionDate() != null) {
+			strSelectedCommitteeDissolutionDate = formatter.format(selectedCommittee.getDissolutionDate());			
+		}
+		model.addAttribute("selectedCommitteeDissolutionDate", strSelectedCommitteeDissolutionDate);
+		
+		List<CommitteeMember> allCommitteeMembers  = new ArrayList<CommitteeMember>();
+		allCommitteeMembers.addAll(selectedCommittee.getMembers());
+		allCommitteeMembers.addAll(selectedCommittee.getInvitedMembers());
+		Collections.sort(allCommitteeMembers,new Comparator<CommitteeMember>() {
+			public int compare(CommitteeMember cm1,CommitteeMember cm2) {
+				if(cm1.getPosition() == null) {
+					return 1;
+				}else if(cm2.getPosition() == null) {
+					return -1;
+				}else if(cm1.getDesignation().getId() == 52 ) {
+					return -1;
+				}else if(cm2.getDesignation().getId() == 52){
+					return -2;
+				}else {
+					return cm1.getPosition() - cm2.getPosition();
+				}
+			}});
+		
+		model.addAttribute("allCommitteeMembers", allCommitteeMembers);		
+		model.addAttribute("totalInvitedMembers", selectedCommittee.getInvitedMembers().size());
+		model.addAttribute("totalMembers", allCommitteeMembers.size());
+
+		
+	}
+	
+	
+	@SuppressWarnings("unused")
+	@RequestMapping(value ="/adminCommitteeCreation/edit",method = RequestMethod.POST)
+	private  String updateCommitteeDetailsForEdit( final ModelMap model,HttpServletRequest request,final Locale locale) {
+		String responseMessage = "error";
+		String strtotalRows = request.getParameter("totalRows");
+		String strCommitteeId = request.getParameter("committeeId");
+		String strcommitteeNameId = request.getParameter("committeeNameId");
+		String strStatusId = request.getParameter("statusId");
+		String strFormationDate = request.getParameter("formationDate");
+		String strfoundationDate = request.getParameter("foundationDate");
+		String strdissolutionDate = request.getParameter("dissolutionDate");
+		String retVal = "error";
+		
+		
+		Integer totalRows = Integer.parseInt(strtotalRows);
+		Committee committeeToBeupdated = Committee.findById(Committee.class, Long.parseLong(strCommitteeId));
+		List<Map<String,String>> committeeMemberDetails = new ArrayList<>();
+		
+		if(totalRows != null ) {
+			CommitteeName cm = CommitteeName.findById(CommitteeName.class, Long.parseLong(strcommitteeNameId));
+			Status s  = Status.findById(Status.class, Long.parseLong(strStatusId));
+						
+			for (int i=0;i<totalRows;i++){
+				Map<String,String> ymap = new HashMap<String,String>();
+				String  priority = request.getParameter("committeeDetails["+i+"][priority]");
+				ymap.put("priority",priority);
+				
+				String  memberId = request.getParameter("committeeDetails["+i+"][memberId]");
+				ymap.put("memberId",memberId);
+				
+				String  designation = request.getParameter("committeeDetails["+i+"][designation]");
+				ymap.put("designation",designation);
+				
+				committeeMemberDetails.add(ymap);
+			}
+			
+			
+			committeeToBeupdated.setCommitteeName(cm);
+			committeeToBeupdated.setStatus(s);
+			committeeToBeupdated.setLocale(locale.toString());
+			DateFormat formatter = new SimpleDateFormat("yyyy-MM-DD"); 
+			Date formationDate = null;
+			Date foundationDate = null;
+			Date dissolutionDate = null;
+			try {
+				 formationDate = (Date)formatter.parse(strFormationDate);
+				 foundationDate = (Date)formatter.parse(strFormationDate);
+				 dissolutionDate = (Date)formatter.parse(strdissolutionDate);
+				 
+			} catch (ParseException e) {
+				System.out.println("Error At Converting String to Date Object in Committee Creation Class in [ updateCommitteeDetailsForEdit ] Method");
+				e.printStackTrace();
+			}
+			
+			if(formationDate != null ) {
+				committeeToBeupdated.setFormationDate(formationDate);			
+			}else {
+				committeeToBeupdated.setFormationDate(new Date());
+			}
+			
+			 if (foundationDate != null) {
+				 committeeToBeupdated.setCreationDate(foundationDate);
+			 }else {
+				 committeeToBeupdated.setCreationDate(new Date());
+			 }
+			 
+			 if(dissolutionDate != null) {
+				 committeeToBeupdated.setDissolutionDate(dissolutionDate);
+			 }
+		
+			List<CommitteeMember> ogCommitteeMember = new ArrayList<CommitteeMember>();
+			List<CommitteeMember> invitedCommitteeMember = new ArrayList<CommitteeMember>();
+				
+			if(committeeMemberDetails != null ){
+				for(Map<String,String>  i : committeeMemberDetails)
+				{
+				CommitteeMember committeeMember = new CommitteeMember();
+					
+				 String strmemberId = 	i.get("memberId");
+				 Member member  = Member.findById(Member.class, Long.parseLong(strmemberId));				 
+				 committeeMember.setMember(member);				 
+				 committeeMember.setJoiningDate(new Date());
+				 
+				 String strpriority = i.get("priority");
+				 if(strpriority != null && !strpriority.equals(" - ")) {
+					 committeeMember.setPosition(Integer.parseInt(strpriority));
+				 }
+				 
+				 String strDesignation = i.get("designation");
+				 if(strDesignation != null) {
+					 CommitteeDesignation memberComitteeDesignation = member.findById(CommitteeDesignation.class, Long.parseLong(strDesignation));
+					 committeeMember.setDesignation(memberComitteeDesignation);
+				 }		
+				 
+				 if(committeeMember.getPosition() != null) {
+					 ogCommitteeMember.add(committeeMember);
+				 }else {
+					 invitedCommitteeMember.add(committeeMember);
+				 }
+				}
+			}else {
+				return retVal;
+			}
+			
+			
+			committeeToBeupdated.setMembers(ogCommitteeMember);
+			committeeToBeupdated.setInvitedMembers(invitedCommitteeMember);			
+			
+			int rowsAdded = ogCommitteeMember.size() + invitedCommitteeMember.size();			
+			
+			committeeToBeupdated.merge();			 
+			System.out.println("Committee Updated SuccessFully !!");
+			
+			model.addAttribute("success", "Updated");
+			this.getCommitteeCreationEdit(model, request, Long.parseLong(strCommitteeId), locale);
+			
+			retVal =  "Updated";
+			return "committee/committeeCreation/edit";
+		}
+		
+		return retVal;
+	}
+	
 
 }
